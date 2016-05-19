0f179c483604a7a5cdf393f7ed2a06f2157b945b
Oleksandr Chekmez <chekmez@nikama.com>
2016-05-19 11:24:00 +0300

    #168 add property types to segment parameters

---
+++ service_packs/2016_05_18_1_alter_SegmentParameter.sql
@@ -0,0 +1,10 @@
+IF NOT EXISTS(SELECT NULL
+              FROM information_schema.columns
+              WHERE table_name = 'SegmentParameter'
+                AND column_name = 'PropertyType')
+BEGIN
+   ALTER TABLE [dbo].[SegmentParameter] ADD [PropertyType] [int] NULL;
+   ALTER TABLE [dbo].[SegmentParameter] WITH CHECK ADD  CONSTRAINT [FK_SegmentParameter_PropertyTypes] FOREIGN KEY([PropertyType]) REFERENCES [dbo].[PropertyTypes] ([ID]);
+   ALTER TABLE [dbo].[MaterialClassProperty] CHECK CONSTRAINT [FK_MaterialClassProperty_PropertyTypes];
+END
+GO
---
+++ service_packs/2016_05_18_2_add_PropertyTypes.sql
@@ -0,0 +1,18 @@
+﻿BEGIN TRANSACTION;
+
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'STD',N'Стандарт');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'LEN',N'Длина');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'QMIN',N'Количество прутков (минимальное)');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'CONTR',N'КОНТРАКТ №');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'DIR',N'НАПРАВЛЕНИЕ');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'PROD',N'Продукция');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'CLASS',N'Класс');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'STCLASS',N'Марка стали');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'CHEM',N'Хим. Анализ');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'DIAM',N'Диаметр бунта'); 
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'ADR',N'Адрес поставщика');
+INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'ORDER',N'Заказ');
+
+COMMIT;
+GO
+
