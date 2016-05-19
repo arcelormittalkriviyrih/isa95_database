e4ade04b529e070c29dd7c3d72c33a46bae5e0d1
Oleksandr Chekmez <chekmez@nikama.com>
2016-05-19 11:22:28 +0300

    #164 add trigger to fill  MIME type

---
+++ service_packs/2016_05_13_2_alter_Files.sql
@@ -0,0 +1,38 @@
+﻿IF NOT EXISTS(SELECT NULL
+              FROM information_schema.columns
+              WHERE table_name = 'Files'
+                AND column_name = 'MIMEType')
+   ALTER TABLE [dbo].[Files] ADD [MIMEType] [nvarchar](255) NULL
+GO
+
+IF OBJECT_ID ('dbo.InsUpdFiles',N'TR') IS NOT NULL
+   DROP TRIGGER [dbo].[InsUpdFiles];
+GO
+
+CREATE TRIGGER [dbo].[InsUpdFiles] ON [dbo].[Files]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+
+   SET NOCOUNT ON;
+
+   DECLARE @ID int,
+           @FileName VARCHAR(255),
+           @FileExt  VARCHAR(10),
+           @MIMEType VARCHAR(255);
+
+   SELECT @ID=[ID],@FileName=REVERSE([FileName])
+   FROM INSERTED;
+
+   SET @FileExt = LOWER(REVERSE(SUBSTRING(@FileName,1,CHARINDEX('.',@FileName))));
+
+   SELECT @MIMEType = mt.MIMEType
+   FROM dbo.MIMEType mt
+   WHERE mt.Extension=@FileExt;
+
+   UPDATE [dbo].[Files]
+   SET MIMEType = @MIMEType
+   WHERE [ID]=@ID;
+
+END
+GO
