c62cd5f3e9f9cefcc184a4d6c1cfa8191a215b1e
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-22 07:02:38 +0300

    #108 adjust trigger on KEP logging

---
+++ procedures/ins_ErrorLog.prc
@@ -0,0 +1,17 @@
+﻿--------------------------------------------------------------
+-- Процедура ins_ErrorLog
+IF OBJECT_ID ('dbo.ins_ErrorLog',N'P') IS NOT NULL
+   DROP PROCEDURE dbo.ins_ErrorLog;
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ins_ErrorLog]
+AS
+BEGIN
+   INSERT INTO [dbo].[ErrorLog](error_details,error_message)
+   SELECT N'ERROR_NUMBER: ' + CAST(ERROR_NUMBER() AS NVARCHAR) + N', ERROR_SEVERITY: '+ IsNull(CAST(ERROR_SEVERITY() AS NVARCHAR),N'') + N', ERROR_STATE: '+ IsNull(CAST(ERROR_STATE() AS NVARCHAR),N'') + N', ERROR_PROCEDURE: '+ IsNull(ERROR_PROCEDURE(),N'') + N', ERROR_LINE '+ CAST(ERROR_LINE() AS NVARCHAR),
+          ERROR_MESSAGE();
+END;
+GO
--- service_packs/2016_06_17_3_add_trigger_InsKeplogger.sql
+++ service_packs/2016_06_17_3_add_trigger_InsKeplogger.sql
@@ -1,4 +1,23 @@
-﻿IF OBJECT_ID ('dbo.InsKeplogger',N'TR') IS NOT NULL
+﻿--------------------------------------------------------------
+-- Процедура ins_ErrorLog
+IF OBJECT_ID ('dbo.ins_ErrorLog',N'P') IS NOT NULL
+   DROP PROCEDURE dbo.ins_ErrorLog;
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ins_ErrorLog]
+AS
+BEGIN
+   INSERT INTO [dbo].[ErrorLog](error_details,error_message)
+   SELECT N'ERROR_NUMBER: ' + CAST(ERROR_NUMBER() AS NVARCHAR) + N', ERROR_SEVERITY: '+ IsNull(CAST(ERROR_SEVERITY() AS NVARCHAR),N'') + N', ERROR_STATE: '+ IsNull(CAST(ERROR_STATE() AS NVARCHAR),N'') + N', ERROR_PROCEDURE: '+ IsNull(ERROR_PROCEDURE(),N'') + N', ERROR_LINE '+ CAST(ERROR_LINE() AS NVARCHAR),
+          ERROR_MESSAGE();
+END;
+GO
+
+------------------------------------------------------------------
+IF OBJECT_ID ('dbo.InsKeplogger',N'TR') IS NOT NULL
    DROP TRIGGER [dbo].[InsKeplogger];
 GO
 
@@ -26,31 +45,35 @@ BEGIN
           @WEIGHT__FIX_TIMESTAMP=[WEIGHT__FIX_TIMESTAMP]
    FROM INSERTED;
 
-   IF @WEIGHT_OK_VALUE=0
-      RETURN;
+   BEGIN TRY
+      IF @WEIGHT_OK_VALUE=1
+         BEGIN
+            SELECT TOP 1 @JobOrderID=jo.[ID]
+            FROM [dbo].[JobOrder] jo
+                 INNER JOIN [dbo].[OpEquipmentRequirement] er ON (er.[JobOrderID]=jo.[ID] AND er.EquipmentID=@WEIGHT__FIX_NUMERICID)
+            WHERE jo.[WorkType]=N'INIT'
+            ORDER BY jo.[StartTime] DESC
 
-   SELECT TOP 1 @JobOrderID=jo.[ID]
-   FROM [dbo].[JobOrder] jo
-        INNER JOIN [dbo].[OpEquipmentRequirement] er ON (er.[JobOrderID]=jo.[ID] AND er.EquipmentID=@WEIGHT__FIX_NUMERICID)
-   WHERE jo.[WorkType]=N'INIT'
-   ORDER BY jo.[StartTime] DESC
+            SET @FactoryNumber=[dbo].[get_GenMaterialLotNumber](@WEIGHT__FIX_NUMERICID,NEXT VALUE FOR dbo.gen_MaterialLotNumber);
+            EXEC [dbo].[ins_MaterialLot] @FactoryNumber = @FactoryNumber,
+                                         @Status        = N'0',
+                                         @Quantity      = @WEIGHT__FIX_VALUE,
+                                         @MaterialLotID = @MaterialLotID OUTPUT;
 
-   SET @FactoryNumber=[dbo].[get_GenMaterialLotNumber](@WEIGHT__FIX_NUMERICID,NEXT VALUE FOR dbo.gen_MaterialLotNumber);
-   EXEC [dbo].[ins_MaterialLot] @FactoryNumber = @FactoryNumber,
-                                @Status        = N'0',
-                                @Quantity      = @WEIGHT__FIX_VALUE,
-                                @MaterialLotID = @MaterialLotID OUTPUT;
+            EXEC [dbo].[ins_MaterialLotPropertyByJobOrder] @MaterialLotID   = @MaterialLotID,
+                                                           @JobOrderID      = @JobOrderID,
+                                                           @MEASURE_TIME    = @WEIGHT__FIX_TIMESTAMP,
+                                                           @AUTO_MANU_VALUE = @AUTO_MANU_VALUE;
 
-   EXEC [dbo].[ins_MaterialLotPropertyByJobOrder] @MaterialLotID   = @MaterialLotID,
-                                                  @JobOrderID      = @JobOrderID,
-                                                  @MEASURE_TIME    = @WEIGHT__FIX_TIMESTAMP,
-                                                  @AUTO_MANU_VALUE = @AUTO_MANU_VALUE;
-
-   SET @PrinterID = [dbo].[get_EquipmentPropertyValue](@WEIGHT__FIX_NUMERICID,N'USED_PRINTER');
-   EXEC [dbo].[ins_JobOrderPrintLabel] @PrinterID     = @PrinterID,
-                                       @MaterialLotID = @MaterialLotID,
-                                       @Command       = N'Print';
-
+            SET @PrinterID = [dbo].[get_EquipmentPropertyValue](@WEIGHT__FIX_NUMERICID,N'USED_PRINTER');
+            EXEC [dbo].[ins_JobOrderPrintLabel] @PrinterID     = @PrinterID,
+                                                @MaterialLotID = @MaterialLotID,
+                                                @Command       = N'Print';
+         END;
+   END TRY;
+   BEGIN CATCH
+      EXEC [dbo].[ins_ErrorLog];
+   END CATCH;
 END
 GO
 
