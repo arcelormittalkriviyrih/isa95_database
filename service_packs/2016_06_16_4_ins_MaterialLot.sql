48c40962a324838b055c12447835f640ea75114f
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-24 12:30:41 +0300

    #add material lot proc

---
+++ service_packs/2016_06_16_4_ins_MaterialLot.sql
@@ -0,0 +1,24 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[ins_MaterialLot]
+@FactoryNumber   NVARCHAR(250),
+@Status          NVARCHAR(250),
+@Quantity        INT,
+@MaterialLotID   INT OUTPUT
+AS
+BEGIN
+
+   SET @MaterialLotID=NEXT VALUE FOR [dbo].[gen_MaterialLot];
+   INSERT INTO [dbo].[MaterialLot] ([ID],[FactoryNumber],[Status],[Quantity])
+   VALUES (@MaterialLotID,@FactoryNumber,@Status,@Quantity);
+
+END;
+
+GO
+
+
