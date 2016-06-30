f5f5d08bef13fbae2ee422f25dd523eb0dca4747
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-30 20:11:55 +0300

    #89 add material lot link types

---
+++ service_packs/2016_06_30_2_ins_MaterialLinkTypes_data.sql
@@ -0,0 +1,15 @@
+﻿BEGIN TRANSACTION;
+
+INSERT INTO [dbo].[MaterialLinkTypes] ([ID],[Description]) VALUES (0,N'печать');
+
+INSERT INTO [dbo].[MaterialLinkTypes] ([ID],[Description]) VALUES (1,N'перемаркировка');
+
+INSERT INTO [dbo].[MaterialLinkTypes] ([ID],[Description]) VALUES (2,N'сортировка');
+
+INSERT INTO [dbo].[MaterialLinkTypes] ([ID],[Description]) VALUES (3,N'отбраковка');
+
+INSERT INTO [dbo].[MaterialLinkTypes] ([ID],[Description]) VALUES (4,N'разделение пачки');
+
+COMMIT;
+GO
+
