3d7f46264c19ba81decb8c421d3a9b2ea357bd11
Oleksandr Chekmez <chekmez@nikama.com>
2016-06-30 20:14:54 +0300

    #89 add procedure for editing material lot

---
+++ service_packs/2016_06_30_4_add_ins_MaterialLotByFactoryNumber.sql
@@ -0,0 +1,128 @@
+﻿--------------------------------------------------------------
+-- Процедура для печати перемаркированой бирки ins_MaterialLotByFactoryNumber
+IF OBJECT_ID ('dbo.ins_MaterialLotByFactoryNumber',N'P') IS NOT NULL
+   DROP PROCEDURE dbo.ins_MaterialLotByFactoryNumber;
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ins_MaterialLotByFactoryNumber]
+@FactoryNumber   NVARCHAR(15),
+@EquipmentID     INT,
+@COMM_ORDER      NVARCHAR(50) = NULL,
+@PROD_ORDER      NVARCHAR(50) = NULL,
+@CONTRACT_NO     NVARCHAR(50) = NULL,
+@DIRECTION       NVARCHAR(50) = NULL,
+@SIZE            NVARCHAR(50) = NULL,
+@LENGTH          NVARCHAR(50) = NULL,
+@TOLERANCE       NVARCHAR(50) = NULL,
+@CLASS           NVARCHAR(50) = NULL,
+@STEEL_CLASS     NVARCHAR(50) = NULL,
+@MELT_NO         NVARCHAR(50) = NULL,
+@PART_NO         NVARCHAR(50) = NULL,
+@MIN_ROD         NVARCHAR(50) = NULL,
+@BUYER_ORDER_NO  NVARCHAR(50) = NULL,
+@BRIGADE_NO      NVARCHAR(50) = NULL,
+@PROD_DATE       NVARCHAR(50) = NULL,
+@UTVK            NVARCHAR(50) = NULL,
+@CHANGE_NO       NVARCHAR(50) = NULL,
+@MATERIAL_NO     NVARCHAR(50) = NULL,
+@BUNT_DIA        NVARCHAR(50) = NULL,
+@PRODUCT         NVARCHAR(50) = NULL,
+@STANDARD        NVARCHAR(50) = NULL,
+@CHEM_ANALYSIS   NVARCHAR(50) = NULL,
+@TEMPLATE        NVARCHAR(50) = NULL
+AS
+BEGIN
+
+   DECLARE @MaterialLotID       INT,
+           @MaterialLotCopyID   INT,
+           @Quantity            INT,
+           @PrinterID           [NVARCHAR](50),
+           @err_message         NVARCHAR(255);
+
+   SELECT @MaterialLotID=ml.[ID],
+          @Quantity=ml.[Quantity]
+   FROM (SELECT ml.[ID],
+                ml.[Quantity],
+                ROW_NUMBER() OVER (PARTITION BY ml.[FactoryNumber] ORDER BY ml.[CreateTime] DESC, ml.[ID] DESC) RowNumber
+         FROM [dbo].[MaterialLot] ml
+         WHERE ml.[FactoryNumber]=@FactoryNumber) ml
+   WHERE ml.RowNumber=1;
+
+   IF @MaterialLotID IS NULL
+      BEGIN
+         SET @err_message = N'By FactoryNumber [' + @FactoryNumber + N'] MaterialLot not found';
+         THROW 60010, @err_message, 1;
+      END;
+
+   EXEC [dbo].[ins_MaterialLot] @FactoryNumber = @FactoryNumber,
+                                @Status        = N'1',
+                                @Quantity      = @Quantity,
+                                @MaterialLotID = @MaterialLotCopyID OUTPUT;
+
+   INSERT INTO [dbo].[MaterialLotLinks] ([MaterialLot1],[MaterialLot2],[LinkType])
+   VALUES (@MaterialLotID,@MaterialLotCopyID,1);
+
+   DECLARE @tblParams TABLE(ID    NVARCHAR(50),
+                            Value NVARCHAR(50));
+
+   INSERT @tblParams
+   SELECT N'COMM_ORDER',@COMM_ORDER WHERE @COMM_ORDER IS NOT NULL
+   UNION ALL
+   SELECT N'PROD_ORDER',@PROD_ORDER WHERE @PROD_ORDER IS NOT NULL
+   UNION ALL
+   SELECT N'CONTRACT_NO',@CONTRACT_NO WHERE @CONTRACT_NO IS NOT NULL
+   UNION ALL
+   SELECT N'DIRECTION',@DIRECTION WHERE @DIRECTION IS NOT NULL
+   UNION ALL
+   SELECT N'SIZE',@SIZE WHERE @SIZE IS NOT NULL
+   UNION ALL
+   SELECT N'LENGTH',@LENGTH WHERE @LENGTH IS NOT NULL
+   UNION ALL
+   SELECT N'TOLERANCE',@TOLERANCE WHERE @TOLERANCE IS NOT NULL
+   UNION ALL
+   SELECT N'CLASS',@CLASS WHERE @CLASS IS NOT NULL
+   UNION ALL
+   SELECT N'STEEL_CLASS',@STEEL_CLASS WHERE @STEEL_CLASS IS NOT NULL
+   UNION ALL
+   SELECT N'MELT_NO',@MELT_NO WHERE @MELT_NO IS NOT NULL
+   UNION ALL
+   SELECT N'PART_NO',@PART_NO WHERE @PART_NO IS NOT NULL
+   UNION ALL
+   SELECT N'MIN_ROD',@MIN_ROD WHERE @MIN_ROD IS NOT NULL
+   UNION ALL
+   SELECT N'BUYER_ORDER_NO',@BUYER_ORDER_NO WHERE @BUYER_ORDER_NO IS NOT NULL
+   UNION ALL
+   SELECT N'BRIGADE_NO',@BRIGADE_NO WHERE @BRIGADE_NO IS NOT NULL
+   UNION ALL
+   SELECT N'PROD_DATE',@PROD_DATE WHERE @PROD_DATE IS NOT NULL
+   UNION ALL
+   SELECT N'UTVK',@UTVK WHERE @UTVK IS NOT NULL
+   UNION ALL
+   SELECT N'CHANGE_NO',@CHANGE_NO WHERE @CHANGE_NO IS NOT NULL
+   UNION ALL
+   SELECT N'MATERIAL_NO',@MATERIAL_NO WHERE @MATERIAL_NO IS NOT NULL
+   UNION ALL
+   SELECT N'BUNT_DIA',@BUNT_DIA WHERE @BUNT_DIA IS NOT NULL
+   UNION ALL
+   SELECT N'PRODUCT',@PRODUCT WHERE @PRODUCT IS NOT NULL
+   UNION ALL
+   SELECT N'STANDARD',@STANDARD WHERE @STANDARD IS NOT NULL
+   UNION ALL
+   SELECT N'CHEM_ANALYSIS',@CHEM_ANALYSIS WHERE @CHEM_ANALYSIS IS NOT NULL
+   UNION ALL
+   SELECT N'TEMPLATE',@TEMPLATE WHERE @TEMPLATE IS NOT NULL;
+
+   INSERT INTO [dbo].[MaterialLotProperty] ([Value],[MaterialLotID],[PropertyType])
+   SELECT t.value,@MaterialLotCopyID,pt.ID
+   FROM @tblParams t INNER JOIN [dbo].[PropertyTypes] pt ON (pt.value=t.ID);
+
+   SET @PrinterID = [dbo].[get_EquipmentPropertyValue](@EquipmentID,N'USED_PRINTER');
+   EXEC [dbo].[ins_JobOrderPrintLabel] @PrinterID     = @PrinterID,
+                                       @MaterialLotID = @MaterialLotCopyID,
+                                       @Command       = N'Print';
+
+END;
+GO
