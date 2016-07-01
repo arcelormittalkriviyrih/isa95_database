d85839dd22cda22a80c70d6a3b691b2034975184
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-01 08:06:15 +0300

    #149 fix path

--- jenkins_scripts/apply_service_packs_prod.bat
+++ jenkins_scripts/apply_service_packs_prod.bat
@@ -1,3 +1,3 @@
 echo %WORKSPACE%
 CD "%WORKSPACE%\service_packs"
-FOR %%i IN ("*.sql") DO sqlcmd -S KRR-SQL-PACLX02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%WORKSPACE%\jenkins scripts\apply_service_packs.prc"
+FOR %%i IN ("*.sql") DO sqlcmd -S KRR-SQL-PACLX02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%WORKSPACE%\jenkins_scripts\apply_service_packs.prc"
