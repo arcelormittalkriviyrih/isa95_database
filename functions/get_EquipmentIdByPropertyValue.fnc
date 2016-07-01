942a787cc1764c1befce085b37bd834c9aaee671
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-01 20:51:28 +0300

    #220 code refactoring - create function to get equipment property

---
+++ functions/get_EquipmentIdByPropertyValue.fnc
@@ -0,0 +1,23 @@
+﻿--------------------------------------------------------------
+-- Процедура вычитки поля ID из таблицы EquipmentProperty по значению поля Value
+IF OBJECT_ID ('dbo.get_EquipmentIdByPropertyValue', N'FN') IS NOT NULL
+   DROP FUNCTION [dbo].[get_EquipmentIdByPropertyValue];
+GO
+
+CREATE FUNCTION [dbo].[get_EquipmentIdByPropertyValue](@Value [nvarchar](50),
+                                                       @EquipmentClassPropertyValue [NVARCHAR](50))
+RETURNS INT
+AS
+BEGIN
+
+DECLARE @Id INT;
+
+SELECT @Id=[EquipmentID]
+FROM [dbo].[EquipmentProperty] ep
+WHERE ep.[Value]=@Value
+  AND ep.[ClassPropertyID]=[dbo].[get_EquipmentClassPropertyByValue](@EquipmentClassPropertyValue);
+
+RETURN @Id;
+
+END;
+GO
--- service_packs/2016_07_01_1_add_get_EquipmentIdByPropertyValue.sql
+++ service_packs/2016_07_01_1_add_get_EquipmentIdByPropertyValue.sql
@@ -1,4 +1,28 @@
 ﻿--------------------------------------------------------------
+-- Процедура вычитки поля ID из таблицы EquipmentProperty по значению поля Value
+IF OBJECT_ID ('dbo.get_EquipmentIdByPropertyValue', N'FN') IS NOT NULL
+   DROP FUNCTION [dbo].[get_EquipmentIdByPropertyValue];
+GO
+
+CREATE FUNCTION [dbo].[get_EquipmentIdByPropertyValue](@Value [nvarchar](50),
+                                                       @EquipmentClassPropertyValue [NVARCHAR](50))
+RETURNS INT
+AS
+BEGIN
+
+DECLARE @Id INT;
+
+SELECT @Id=[EquipmentID]
+FROM [dbo].[EquipmentProperty] ep
+WHERE ep.[Value]=@Value
+  AND ep.[ClassPropertyID]=[dbo].[get_EquipmentClassPropertyByValue](@EquipmentClassPropertyValue);
+
+RETURN @Id;
+
+END;
+GO
+
+--------------------------------------------------------------
 -- Процедура ins_JobOrderPrintLabel
 IF OBJECT_ID ('dbo.ins_JobOrderPrintLabel',N'P') IS NOT NULL
    DROP PROCEDURE dbo.ins_JobOrderPrintLabel;
