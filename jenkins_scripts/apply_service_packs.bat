b56a9ec28f999582c8f465f2f59b22ae13458329
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-01 07:51:57 +0300

    #149 fix service pack applier when update db from other dump

--- jenkins_scripts/apply_service_packs.bat
+++ jenkins_scripts/apply_service_packs.bat
@@ -1,2 +1,3 @@
 echo %WORKSPACE%
-FOR %%i IN ("%WORKSPACE%\service_packs\*.sql") DO sqlcmd -S krr-tst-pahwl02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i %WORKSPACE%\jenkins_scripts\apply_service_packs.prc 
+CD "%WORKSPACE%\service_packs"
+FOR %%i IN ("*.sql") DO sqlcmd -S krr-tst-pahwl02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i %WORKSPACE%\jenkins scripts\apply_service_packs.prc 
--- jenkins_scripts/apply_service_packs_prod.bat
+++ jenkins_scripts/apply_service_packs_prod.bat
@@ -1,2 +1,3 @@
 echo %WORKSPACE%
-FOR %%i IN ("%WORKSPACE%\service_packs\*.sql") DO sqlcmd -S KRR-SQL-PACLX02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i %WORKSPACE%\jenkins_scripts\apply_service_packs.prc 
+CD "%WORKSPACE%\service_packs"
+FOR %%i IN ("*.sql") DO sqlcmd -S KRR-SQL-PACLX02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i %WORKSPACE%\jenkins scripts\apply_service_packs.prc 
