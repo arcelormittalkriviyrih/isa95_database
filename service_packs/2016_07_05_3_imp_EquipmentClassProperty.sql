4e53242e0b404e4317db69ffd562f54fcd06951e
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-06 08:00:22 +0300

    #224 add properties to store current work definition and job order

---
+++ service_packs/2016_07_05_3_imp_EquipmentClassProperty.sql
@@ -0,0 +1,7 @@
+﻿IF NOT EXISTS (SELECT NULL FROM [dbo].[EquipmentClassProperty] WHERE [Value]=N'STANDARD_WORK_DEFINITION_ID')
+   INSERT INTO [dbo].[EquipmentClassProperty]([Description],[Value],[EquipmentClassID]) (SELECT N'Текущий WorkDefinition с режимом Standard',N'STANDARD_WORK_DEFINITION_ID',eqc.[ID] FROM [dbo].[EquipmentClass] eqc WHERE eqc.[Code]=N'SCALES')
+GO
+
+IF NOT EXISTS (SELECT NULL FROM [dbo].[EquipmentClassProperty] WHERE [Value]=N'STANDARD_JOB_ORDER_ID')
+   INSERT INTO [dbo].[EquipmentClassProperty]([Description],[Value],[EquipmentClassID]) (SELECT N'Текущий JobOrder с режимом Standard',N'STANDARD_JOB_ORDER_ID',eqc.[ID] FROM [dbo].[EquipmentClass] eqc WHERE eqc.[Code]=N'SCALES')
+GO
