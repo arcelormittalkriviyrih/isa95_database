6968ab771117b28fed69e80ab899e7550ec9301e
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-13 16:14:09 +0300

    #246 rename scale types

---
+++ service_packs/2016_07_13_4_rename_scale_type.sql
@@ -0,0 +1,19 @@
+﻿SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+BEGIN TRANSACTION;
+
+UPDATE [dbo].[EquipmentProperty]
+SET Value=CASE Value WHEN N'Карманные' THEN N'POCKET' 
+                     WHEN N'Ручные' THEN N'MANUAL' 
+                     WHEN N'Весы линии упаковки' THEN N'LINEPACK'
+                     WHEN N'Бунтовые' THEN N'BUNT'  
+          END
+WHERE [ClassPropertyID] IN (SELECT [ID] FROM [dbo].[EquipmentClassProperty] WHERE [Value]=N'SCALES_TYPE')
+  AND [Value] IN (N'Карманные',N'Ручные',N'Весы линии упаковки',N'Бунтовые');
+
+COMMIT;
+GO
+
