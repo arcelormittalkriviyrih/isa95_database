0d1e53b700f2b05a535ea7bc7423b4db6891cfc6
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-13 16:13:43 +0300

    #243 add PreviewID to Files

---
+++ service_packs/2016_07_13_3_add_PreviewID.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+-- Обновление структур таблиц
+IF NOT EXISTS (SELECT NULL
+               FROM information_schema.columns
+               WHERE table_name = 'Files'
+                 AND column_name = 'PreviewID')
+BEGIN
+   ALTER TABLE [dbo].[Files] ADD [PreviewID] int NULL;   
+   ALTER TABLE [dbo].[Files] WITH CHECK ADD  CONSTRAINT [FK_Files_PreviewID] FOREIGN KEY([PreviewID]) REFERENCES [dbo].[Files] ([ID]);   
+END
