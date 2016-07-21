24ab504b07a3eb98630f6bb92d9d156e04649dfe
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-21 07:37:06 +0300

    #260 set on delete cascade for property tables

---
+++ service_packs/2016_07_20_3_alter_on_delete_cascade.sql
@@ -0,0 +1,21 @@
+﻿
+ALTER TABLE [dbo].[EquipmentProperty]  DROP CONSTRAINT [FK_EquipmentProperty_Equipment]
+GO
+
+ALTER TABLE [dbo].[EquipmentProperty]  WITH CHECK ADD  CONSTRAINT [FK_EquipmentProperty_Equipment] FOREIGN KEY([EquipmentID])
+REFERENCES [dbo].[Equipment] ([ID]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[MaterialDefinitionProperty] DROP CONSTRAINT [FK_MaterialDefinitionProperty_MaterialDefinition]
+GO
+
+ALTER TABLE [dbo].[MaterialDefinitionProperty]  WITH CHECK ADD  CONSTRAINT [FK_MaterialDefinitionProperty_MaterialDefinition] FOREIGN KEY([MaterialDefinitionID])
+REFERENCES [dbo].[MaterialDefinition] ([ID]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[PersonProperty] DROP CONSTRAINT [FK_PersonProperty_Person]
+GO
+
+ALTER TABLE [dbo].[PersonProperty]  WITH CHECK ADD  CONSTRAINT [FK_PersonProperty_Person] FOREIGN KEY([PersonID])
+REFERENCES [dbo].[Person] ([ID]) ON DELETE CASCADE
+GO
