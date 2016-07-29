b126b32be87b4ee26b630dba278f40134ffd25a0
Oleksandr Chekmez <chekmez@nikama.com>
2016-07-29 12:04:48 +0300

    fix service packs to be able to run them multiple times

--- service_packs/2016_07_14_5_imp_ProperyTypes.sql
+++ service_packs/2016_07_14_5_imp_ProperyTypes.sql
@@ -4,8 +4,11 @@ SET QUOTED_IDENTIFIER ON;
 GO
 BEGIN TRANSACTION;
 
-INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'BINDING_DIA',N'Диаметр увязки');
-INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'BINDING_QTY',N'Количество увязок');
+IF NOT EXISTS (SELECT NULL FROM [dbo].[PropertyTypes] WHERE [Value]=N'BINDING_DIA')
+	INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'BINDING_DIA',N'Диаметр увязки');
+
+IF NOT EXISTS (SELECT NULL FROM [dbo].[PropertyTypes] WHERE [Value]=N'BINDING_QTY')
+	INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'BINDING_QTY',N'Количество увязок');
 
 COMMIT;
 GO
--- service_packs/2016_07_22_2_add_SAP_Properties.sql
+++ service_packs/2016_07_22_2_add_SAP_Properties.sql
@@ -4,10 +4,14 @@ SET QUOTED_IDENTIFIER ON;
 GO
 BEGIN TRANSACTION;
 
+IF NOT EXISTS (SELECT NULL FROM [dbo].[PropertyTypes] WHERE [Value]=N'SAP_SERVICE_URL')
+	INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'SAP_SERVICE_URL',N'SAP service URL');
 
-INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'SAP_SERVICE_URL',N'SAP service URL');
-INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'SAP_SERVICE_LOGIN',N'SAP service login');
-INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'SAP_SERVICE_PASS',N'SAP service password');
+IF NOT EXISTS (SELECT NULL FROM [dbo].[PropertyTypes] WHERE [Value]=N'SAP_SERVICE_LOGIN')
+	INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'SAP_SERVICE_LOGIN',N'SAP service login');
+
+IF NOT EXISTS (SELECT NULL FROM [dbo].[PropertyTypes] WHERE [Value]=N'SAP_SERVICE_PASS')
+	INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'SAP_SERVICE_PASS',N'SAP service password');
 
 COMMIT;
 GO
