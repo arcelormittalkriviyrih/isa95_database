7183687f76af1fc2247ade70c388599bd76b98e5
Oleksandr Chekmez <chekmez@nikama.com>
2016-08-02 14:45:18 +0300

    #285 add handling of duplicates from KEP

---
+++ service_packs/2016_08_02_2_alter_Kep_Weigth_fix.sql
@@ -0,0 +1,23 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS(SELECT NULL
+              FROM information_schema.columns
+              WHERE table_name = 'kep_weigth_fix'
+                AND column_name = 'IDENT')
+   ALTER TABLE [dbo].[kep_weigth_fix] ADD [IDENT] [INT]  NULL
+GO
+
+IF NOT EXISTS(SELECT NULL
+              FROM information_schema.columns
+              WHERE table_name = 'kep_weigth_fix_archive'
+                AND column_name = 'IDENT')
+   ALTER TABLE [dbo].kep_weigth_fix_archive ADD [IDENT] [INT]  NULL
+GO
+
+IF NOT EXISTS (SELECT NULL FROM [dbo].[PropertyTypes] WHERE [Value]=N'MATERIAL_LOT_IDENT')
+	INSERT INTO [dbo].[PropertyTypes]([Value],[Description]) VALUES (N'MATERIAL_LOT_IDENT',N'Material Lot Ident');
+GO	
--- service_packs/2016_08_02_3_ins_JobOrderPrintLabelByScalesNo.sql
+++ service_packs/2016_08_02_3_ins_JobOrderPrintLabelByScalesNo.sql
@@ -1,4 +1,63 @@
 ﻿--------------------------------------------------------------
+-- Процедура ins_MaterialLotPropertyByWorkDefinition
+IF OBJECT_ID ('dbo.ins_MaterialLotPropertyByWorkDefinition',N'P') IS NOT NULL
+   DROP PROCEDURE dbo.ins_MaterialLotPropertyByWorkDefinition;
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ins_MaterialLotPropertyByWorkDefinition]
+@WorkDefinitionID INT,
+@MaterialLotID    INT,
+@MEASURE_TIME     NVARCHAR(50),
+@AUTO_MANU_VALUE  NVARCHAR(50),
+@MILL_ID          NVARCHAR(50) = NULL,
+@NEMERA           NVARCHAR(50) = NULL,
+@IDENT			  NVARCHAR(50) = NULL
+AS
+BEGIN
+
+   DECLARE @LEAVE_NO NVARCHAR(50);
+   DECLARE @tblParams TABLE(ID    NVARCHAR(50),
+                            Value NVARCHAR(50));
+
+   SET @LEAVE_NO =
+	   (
+		  SELECT sp.[Value]
+		  FROM [dbo].[ParameterSpecification] sp
+			  INNER JOIN [dbo].[PropertyTypes] pt ON(pt.[ID] = sp.[PropertyType])
+		  WHERE(sp.WorkDefinitionID = @WorkDefinitionID)
+			  AND pt.[Value] = N'COMM_ORDER'
+	   );
+
+   INSERT @tblParams
+   SELECT pt.[Value],sp.[Value]
+   FROM [dbo].[ParameterSpecification] sp
+        INNER JOIN [dbo].[PropertyTypes] pt ON (pt.[ID]=sp.[PropertyType])
+   WHERE (sp.WorkDefinitionID=@WorkDefinitionID)
+   UNION ALL
+   SELECT N'MEASURE_TIME',@MEASURE_TIME WHERE @MEASURE_TIME IS NOT NULL
+   UNION ALL
+   SELECT N'AUTO_MANU_VALUE',@AUTO_MANU_VALUE WHERE @AUTO_MANU_VALUE IS NOT NULL
+   UNION ALL
+   SELECT N'MILL_ID',@MILL_ID WHERE @MILL_ID IS NOT NULL
+   UNION ALL
+   SELECT N'NEMERA',@NEMERA WHERE @NEMERA IS NOT NULL
+   UNION ALL
+   SELECT N'LEAVE_NO',CAST(CAST(@LEAVE_NO AS NUMERIC(11,0))-5000000000 as nvarchar(50)) WHERE @LEAVE_NO IS NOT NULL
+   UNION ALL
+   SELECT N'MATERIAL_LOT_IDENT',@IDENT WHERE @IDENT IS NOT NULL;
+
+   INSERT INTO [dbo].[MaterialLotProperty] ([Value],[MaterialLotID],[PropertyType])
+   SELECT t.value,@MaterialLotID,pt.ID
+   FROM @tblParams t INNER JOIN [dbo].[PropertyTypes] pt ON (pt.value=t.ID);
+
+END;
+GO
+
+
+--------------------------------------------------------------
 -- Процедура ins_JobOrderPrintLabelByScalesNo
 
 
