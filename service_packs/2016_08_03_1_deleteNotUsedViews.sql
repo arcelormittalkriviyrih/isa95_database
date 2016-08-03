3d3a02f9632eda9214c49d8b3abffa49b1172c21
Oleksandr Chekmez <chekmez@nikama.com>
2016-08-03 10:35:43 +0300

    delete not used views

---
+++ service_packs/2016_08_03_1_deleteNotUsedViews.sql
@@ -0,0 +1,16 @@
+﻿IF OBJECT_ID ('dbo.v_ProductionResponse', 'V') IS NOT NULL
+	DROP VIEW dbo.v_ProductionResponse;
+GO
+
+IF OBJECT_ID ('dbo.v_ProductionParameter_Files', 'V') IS NOT NULL
+	DROP VIEW dbo.v_ProductionParameter_Files;
+GO
+
+IF OBJECT_ID ('dbo.v_Property', N'V') IS NOT NULL
+   DROP VIEW dbo.v_Property;
+GO
+
+IF OBJECT_ID ('dbo.v_MaterialLot_Request', N'V') IS NOT NULL
+   DROP VIEW dbo.v_MaterialLot_Request;
+GO
+
