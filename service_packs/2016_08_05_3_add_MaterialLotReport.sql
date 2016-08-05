1d1c07b773c641d9cf8a97f9b2a2fb432a1635a1
Oleksandr Chekmez <chekmez@nikama.com>
2016-08-05 15:09:14 +0300

    add material lot view for reports

---
+++ service_packs/2016_08_05_3_add_MaterialLotReport.sql
@@ -0,0 +1,71 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID ('dbo.v_MaterialLotReport', N'V') IS NOT NULL
+   DROP VIEW dbo.v_MaterialLotReport;
+GO
+/*
+   View: v_MaterialLotReport
+    Возвращает список бирок с информацией для отчетов.
+*/
+CREATE VIEW [dbo].[v_MaterialLotReport]
+AS
+     WITH MaterialLotProperties
+          AS (SELECT mlp.[ID],
+                     mlp.[MaterialLotID],
+                     pt.[Value] PropertyType,
+                     mlp.[Value]
+              FROM [dbo].[MaterialLotProperty] mlp
+                   INNER JOIN [dbo].[PropertyTypes] pt ON(pt.[ID] = mlp.[PropertyType]))
+          SELECT ml.ID,
+                 ml.FactoryNumber,
+                 ml.CreateTime,
+                 ml.Quantity,
+          (
+              SELECT [Value]
+              FROM MaterialLotProperties
+              WHERE MaterialLotID = ml.Id
+                    AND PropertyType = 'CHANGE_NO'
+          ) CHANGE_NO,
+          (
+              SELECT [Value]
+              FROM MaterialLotProperties
+              WHERE MaterialLotID = ml.Id
+                    AND PropertyType = 'BRIGADE_NO'
+          ) BRIGADE_NO,
+          (
+              SELECT [Value]
+              FROM MaterialLotProperties
+              WHERE MaterialLotID = ml.Id
+                    AND PropertyType = 'MELT_NO'
+          ) MELT_NO,
+          (
+              SELECT [Value]
+              FROM MaterialLotProperties
+              WHERE MaterialLotID = ml.Id
+                    AND PropertyType = 'COMM_ORDER'
+          ) COMM_ORDER,
+		(
+              SELECT [Value]
+              FROM MaterialLotProperties
+              WHERE MaterialLotID = ml.Id
+                    AND PropertyType = 'PART_NO'
+          ) PART_NO,  
+		(
+              SELECT [Value]
+              FROM MaterialLotProperties
+              WHERE MaterialLotID = ml.Id
+                    AND PropertyType = 'AUTO_MANU_VALUE'
+          ) AUTO_MANU_VALUE     
+          FROM (SELECT ml.[ID],
+                                  ml.[FactoryNumber],
+                                  ml.[Status],
+                                  ml.[Quantity],
+						    ml.CreateTime,
+                                  ROW_NUMBER() OVER (PARTITION BY ml.[FactoryNumber] ORDER BY ml.[CreateTime] DESC, ml.[ID] DESC) RowNumber
+                           FROM [dbo].[MaterialLot] ml) ml
+                     WHERE ml.RowNumber=1;
+GO
