c79e55316741fa17781f002cc348adca1111debd
Oleksandr Chekmez <chekmez@nikama.com>
2016-08-08 11:18:57 +0300

    #117 add get_EquipmentIDByScalesNo

--- service_packs/2016_08_08_2_add_comments.sql
+++ service_packs/2016_08_08_2_add_comments.sql
@@ -621,6 +621,42 @@ AS
 GO 
 --------------------------------------------------------------
 
+--------------------------------------------------------------
+IF OBJECT_ID ('dbo.get_EquipmentIDByScalesNo', N'FN') IS NOT NULL
+   DROP FUNCTION dbo.get_EquipmentIDByScalesNo;
+GO
+/*
+   Function: get_EquipmentIDByScalesNo
+
+   Функция вычитки поля ID из таблицы Equipment по EquipmentClassProperty N'SCALES_NO'
+
+   Parameters:
+
+      Value - Идентификатор весов.
+     
+   Returns:
+	  
+	  ID весов.
+
+*/
+CREATE FUNCTION dbo.get_EquipmentIDByScalesNo(@Value [nvarchar](50))
+RETURNS INT
+AS
+BEGIN
+
+DECLARE @EquipmentID INT;
+
+SELECT @EquipmentID=eqp.EquipmentID
+FROM dbo.EquipmentProperty eqp
+     INNER JOIN dbo.EquipmentClassProperty ecp ON (ecp.ID=eqp.ClassPropertyID AND ecp.value=N'SCALES_NO')
+WHERE eqp.value=@Value;
+
+RETURN @EquipmentID;
+
+END;
+GO
+
+
 IF OBJECT_ID('dbo.get_RoundedWeightByEquipment', N'FN') IS NOT NULL
     DROP FUNCTION dbo.get_RoundedWeightByEquipment;
 GO
