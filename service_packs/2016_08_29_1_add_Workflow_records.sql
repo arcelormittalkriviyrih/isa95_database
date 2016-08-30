1a895308be256c8c2783c27c258e2ce4563b72a0
Oleksandr Chekmez <chekmez@nikama.com>
2016-08-30 08:34:26 +0300

    105 Add workflow data

---
+++ service_packs/2016_08_29_1_add_Workflow_records.sql
@@ -0,0 +1,60 @@
+﻿SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+BEGIN TRANSACTION;
+
+insert into [dbo].[WorkflowSpecification] ([Description], Version)
+values (N'Test',N'Test');
+
+insert into [dbo].[WorkflowSpecificationNodeType] ([Description])
+values (N'Round');
+insert into [dbo].[WorkflowSpecificationNodeType] ([Description])
+values (N'Square');
+insert into [dbo].[WorkflowSpecificationNodeType] ([Description])
+values (N'Triangle');
+
+insert into [dbo].[WorkflowSpecificationNode](NodeType, [Description], WorkflowSpecification)
+select nt.ID, ROW_NUMBER() OVER (ORDER BY ID), (SELECT ws.ID FROM [dbo].[WorkflowSpecification] ws WHERE ws.[Description]=N'Test')
+from [dbo].[WorkflowSpecificationNodeType] nt;
+
+insert into [dbo].[WorkflowSpecificationConnectionType] ([Description])
+values (N'Straight');
+
+insert into [dbo].[WorkflowSpecificationConnection]([Description], ConnectionType, FromNodeID, ToNodeID, WorkflowSpecification)
+select [Description], ID, 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'1'), 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'2'), 
+       (SELECT ws.ID FROM [dbo].[WorkflowSpecification] ws WHERE ws.[Description]=N'Test')
+from [dbo].[WorkflowSpecificationConnectionType];
+
+insert into [dbo].[WorkflowSpecificationConnection]([Description], ConnectionType, FromNodeID, ToNodeID, WorkflowSpecification)
+select N'Opposite', ID, 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'2'), 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'1'), 
+       (SELECT ws.ID FROM [dbo].[WorkflowSpecification] ws WHERE ws.[Description]=N'Test')
+from [dbo].[WorkflowSpecificationConnectionType];
+
+insert into [dbo].[WorkflowSpecificationConnection]([Description], ConnectionType, FromNodeID, ToNodeID, WorkflowSpecification)
+select N'Self', ID, 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'2'), 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'2'), 
+       (SELECT ws.ID FROM [dbo].[WorkflowSpecification] ws WHERE ws.[Description]=N'Test')
+from [dbo].[WorkflowSpecificationConnectionType];
+
+insert into [dbo].[WorkflowSpecificationConnection]([Description], ConnectionType, FromNodeID, ToNodeID, WorkflowSpecification)
+select N'Single', ID, 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'2'), 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'3'), 
+       (SELECT ws.ID FROM [dbo].[WorkflowSpecification] ws WHERE ws.[Description]=N'Test')
+from [dbo].[WorkflowSpecificationConnectionType];
+
+insert into [dbo].[WorkflowSpecificationConnection]([Description], ConnectionType, FromNodeID, ToNodeID, WorkflowSpecification)
+select N'Back', ID, 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'3'), 
+       (SELECT ID FROM [dbo].[WorkflowSpecificationNode] WHERE [Description]=N'1'), 
+       (SELECT ws.ID FROM [dbo].[WorkflowSpecification] ws WHERE ws.[Description]=N'Test')
+from [dbo].[WorkflowSpecificationConnectionType];
+
+COMMIT;
+GO
