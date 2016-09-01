6dd8b75344603724442cf02f1abcd3912e9afe91
Timur.Kapalov <tbkapalov@arcelormittal.com>
2016-09-01 13:17:56 +0300

    alter table MaterialDefinition add column MaterialDefinition

---
+++ service_packs/2016_09_01_3_alter_MaterialDefinition.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (select * from INFORMATION_SCHEMA.COLUMNS 
+where TABLE_NAME = 'MaterialDefinition' and COLUMN_NAME='MaterialDefinitionID')
+BEGIN
+ALTER TABLE [dbo].[MaterialDefinition] ADD [MaterialDefinitionID] [int] NULL
+END
+
+
+GO
