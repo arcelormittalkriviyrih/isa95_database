4301a1794d30e1a73581e2bedbf3fedf5e9cc06e
Oleksandr Chekmez <chekmez@nikama.com>
2016-09-02 13:46:09 +0300

    #296 optimize view

--- service_packs/2016_09_02_2_edit_v_MaterialLotReport.sql
+++ service_packs/2016_09_02_2_edit_v_MaterialLotReport.sql
@@ -1,4 +1,39 @@
-﻿SET ANSI_NULLS ON;
+﻿--------------------------------------------------------------
+IF OBJECT_ID ('dbo.get_PropertyTypeIdByValue', N'FN') IS NOT NULL
+   DROP FUNCTION [dbo].[get_PropertyTypeIdByValue];
+GO
+/*
+   Function: get_PropertyTypeIdByValue
+
+   Функция вычитки поля ID из таблицы PropertyTypes по значению поля Value
+
+   Parameters:
+
+      Value                       - Value свойства,
+
+   Returns:
+
+      ID свойства.
+
+*/
+CREATE FUNCTION [dbo].[get_PropertyTypeIdByValue](@Value [nvarchar](50)
+                                                 )
+RETURNS INT
+AS
+BEGIN
+
+DECLARE @Id INT;
+
+SELECT @Id=[ID]
+FROM [dbo].[PropertyTypes] ep
+WHERE ep.[Value]=@Value;
+
+RETURN @Id;
+
+END;
+GO
+
+SET ANSI_NULLS ON;
 GO
 
 SET QUOTED_IDENTIFIER ON;
