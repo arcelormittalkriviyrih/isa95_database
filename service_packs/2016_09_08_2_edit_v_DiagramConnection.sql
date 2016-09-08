4cdb365e09a2086d7dc5c5000a5c02189b999454
Oleksandr Chekmez <chekmez@nikama.com>
2016-09-08 11:05:46 +0300

    #105 fix connections view

---
+++ service_packs/2016_09_08_2_edit_v_DiagramConnection.sql
@@ -0,0 +1,19 @@
+﻿IF OBJECT_ID ('dbo.v_DiagramConnection', N'V') IS NOT NULL
+   DROP VIEW dbo.v_DiagramConnection;
+GO
+/*
+   View: v_DiagramConnection
+    Возвращает список диаграмм
+*/
+CREATE VIEW dbo.v_DiagramConnection
+AS
+SELECT d.ID,
+       d.Description,
+       d.FromNodeID,
+       d.ToNodeID,
+       d.WorkflowSpecification DiagramID,
+       p.Value json       
+FROM [dbo].[WorkflowSpecificationConnection] d
+     LEFT OUTER JOIN [dbo].[WorkflowSpecificationProperty] p ON (p.WorkflowSpecificationConnection=d.ID)
+     LEFT OUTER JOIN [dbo].[PropertyTypes] pt ON (pt.[ID]=p.[PropertyType] AND pt.[Value]=N'JSON')
+GO
