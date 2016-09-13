1b392e4572b7c052c92397a80716ea1ca6d86158
Oleksandr Chekmez <chekmez@nikama.com>
2016-09-13 11:59:02 +0300

    #301 add index

---
+++ service_packs/2016_09_13_1_add_i1_KEP_controller_diag.sql
@@ -0,0 +1,7 @@
+﻿IF EXISTS (SELECT NULL FROM sys.indexes WHERE name='i1_KEP_controller_diag' AND object_id = OBJECT_ID('[dbo].[KEP_controller_diag]'))
+   DROP INDEX [i1_KEP_controller_diag] ON [dbo].[KEP_controller_diag]
+GO
+
+CREATE INDEX [i1_KEP_controller_diag] ON [dbo].[KEP_controller_diag] ([Controller_ID],[ID])
+GO
+
