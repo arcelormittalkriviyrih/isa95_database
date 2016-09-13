560b16198e55cae202315f691b3dad4d85bc8298
Oleksandr Chekmez <chekmez@nikama.com>
2016-09-13 12:02:10 +0300

    add KEY_MANU to KEP_Test

---
+++ service_packs/2016_09_13_2_add_KEY_MANU.sql
@@ -0,0 +1,13 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT NULL
+               FROM information_schema.columns
+               WHERE table_name = 'KEP_test'
+                 AND column_name = 'KEY_MANU')
+   ALTER TABLE [dbo].[KEP_test] ADD [KEY_MANU] [BIT]  NULL
+GO
+
