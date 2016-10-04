af016d9202f5e5044d7fa0b8d280cd23bf70a95d
Ilchenko Vladyslav <vladislav.ilchenko@arcelormittal.com>
2016-10-04 13:28:57 +0300

    Insert Equipment for weight analysts

---
+++ service_packs/2016_10_04_1_ins_EquipmentClass_Scales.sql
@@ -0,0 +1,20 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+
+INSERT INTO [dbo].[EquipmentClass]
+           ([Description]
+           ,[ParentID]
+           ,[ID])
+     VALUES
+           (N'Весы железнодорожные',2 ,5000)
+
+INSERT INTO [dbo].[EquipmentClass]
+           ([Description]
+           ,[ParentID]
+           ,[ID])
+     VALUES
+           (N'Весы автомобильные',2 ,5001)
+GO
+
+
---
+++ service_packs/2016_10_04_2_ins_Equipment_Scales.sql
@@ -0,0 +1,549 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5000
+           , N'ж/д весы "пост Бл-1"'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5001
+           , N'ж/д весы Блуминг (20 туп.)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5002
+           , N'ж/д весы ст. Шихтовая'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5003
+           , N'ж/д весы жидкого чугуна ДЦ-1'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5004
+           , N'ж/д весы ст. Промышленно-Сырьевая'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5005
+           , N'ж/д весы "пост 10км"'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5006
+           , N'ж/д весы ст. Промышленная/Прокатная-1'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5007
+           , N'ж/д весы СГП СПЦ-1 (1 туп.)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5008
+           , N'ж/д весы СГП СПЦ-1 (2 туп.)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5009
+           , N'ж/д весы СГП СПЦ-1 (4 туп.)'   
+           , 5000)
+
+--*------------------------------------------*--
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5010
+           , N'ж/д весы ст. Промышленная (м/лом)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5011
+           , N'ж/д весы ст. Копровая-1'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5012
+           , N' ж/д весы ст. Прокатная-1'
+		   , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5013
+           , N'ж/д весы ст. Прокатная-2 (новые)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5014
+           , N'ж/д весы ст. Копровая-2'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5015
+           , N'ж/д весы ст. Промышленная ГОКа'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5016
+           , N'ж/д весы ст. Складская'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5017
+           , N'ж/д весы Прокатная-2 "старые"'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5018
+           , N'СХЧ ДЦ №1, пост  Шлаковый (путь №2)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5019
+           , N'ж/д весы КХП'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5020
+           , N'ж/д весы ж.ч. ДЦ-2 (брутто)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5021
+           , N'ж/д весы ж.ч. ДЦ-2 (тара)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5022
+           , N'ж/д весы ст. Новобункерная'   
+           , 5000)
+------------------------------------------------------------
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5023
+           , N'ж/д весы полигона'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5024
+           , N'ж/д весы ст. Бункерная'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5025
+           , N'ж/д весы ст. Новодоменная'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5026
+           , N'ж/д весы станции Кирово ШУ'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5027
+           , N'СХЧ ДЦ №1, пост  Шлаковый'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5028
+           , N'ж/д весы для взвешивания шлака ДЦ-1 (2 ед.)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5029
+           , N'ж/д весы для взвешивания заготовки ОНРС'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5030
+           , N'ж/д весы ст. Восточно-Сортировочная (7 путь)'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5031
+           , N'ж/д весы в Николаевском морпорту НИКМЕТ'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5032
+           , N'ж/д весы ст. Аглофабрика ГД (3 путь)'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5033
+           , N'ж/д весы КЦ ШО-1'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5034
+           , N'ж/д весы КЦ ШО-1'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5035
+           , N'весы мульдовые Мартеновского цеха'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5036
+           , N'весы мульдовые Мартеновского цеха'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5037
+           , N'ж/д весы ж.ч. Мартена'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5038
+           , N'ж/д весы ж.ч. Мартена'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5039
+           , N'ж/д весы Копровой-4'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5040
+           , N'ж/д весы КЦ МО-1 миксер 1'   
+           , 5000)
+--------------------------------------------------
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5041
+           , N'ж/д весы КЦ МО-1 миксер 3'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5042
+           , N'ж/д весы ст. Восточно-Разгрузочная'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5043
+           , N'ж/д весы КЦ МО-2 миксер 2'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5044
+           , N'ж/д весы КЦ МО-2 миксер 4'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5045
+           , N'ж/д весы КЦ ШО-2'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5046
+           , N'СГП СПЦ-2 (17 туп.)'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5047
+           , N'СГП СПЦ-2 (19 туп.)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5048
+           , N'ж/д весы ст. Карьерная ГД'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5049
+           , N'ж/д весы ст. Карьерная ГД'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5050
+           , N'ж/д весы РОФ-2 (32 путь)'   
+           , 5000)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5051
+           , N'ж/д весы РОФ-2 (34 путь)'   
+           , 5000)
+
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5052
+           , N'автовесы готовой продукции'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5053
+           , N'весы склада ферросплавов СХиПП'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5054
+           , N'автобелазы'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5055
+           , N'автовесы Полигона'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5056
+           , N'автовесы КХП'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5057
+           , N'весы автомобильные СХиПП (склад сыпучих)'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5058
+           , N'весы автомобильные СХиПП (пилорама)'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5059
+           , N'автовесы граншлака ДЦ-2'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5060
+           , N'автовесы (брутто), участок №3 копрового цеха'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5061
+           , N'автовесы (тара), участок №3 копрового цеха'   
+           , 5001)
+INSERT INTO [dbo].[Equipment]
+           ([ID]
+           ,[Description]
+           ,[EquipmentClassID])
+     VALUES
+           ( 5062
+           , N'автовесы СХиПП ГД'   
+           , 5001)
+GO
