de53c67e5f3a2dc2d7d0cb1e4311bdaf66045b1e
Ilchenko Vladyslav <vladislav.ilchenko@arcelormittal.com>
2016-10-05 16:42:38 +0300

    added view AcceptanceMaterial

---
+++ service_packs/2016_10_05_1_add_v_AcceptanceMaterial.sql
@@ -0,0 +1,17 @@
+SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID ('dbo.v_AcceptanceMaterial', N'V') IS NOT NULL
+   DROP VIEW dbo.v_MaterialLot;
+GO
+
+
+CREATE VIEW [dbo].[v_AcceptanceMaterial]
+AS
+SELECT     dbo.MaterialLot.FactoryNumber, dbo.MaterialDefinition.Description, dbo.MaterialLot.Status, dbo.MaterialLot.Location
+FROM         dbo.MaterialDefinition INNER JOIN
+                      dbo.MaterialLot ON dbo.MaterialDefinition.ID = dbo.MaterialLot.MaterialDefinitionID
+GO
