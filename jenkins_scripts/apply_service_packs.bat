ed688d2efd918e530f8d30fa7e95ff78e6714a46
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-06 11:43:39 +0300

    remove not used scripts

--- jenkins_scripts/apply_service_packs.bat
+++
@@ -1,3 +0,0 @@
-echo %WORKSPACE%
-CD "%WORKSPACE%\service_packs"
-FOR %%i IN ("*.sql") DO sqlcmd -S krr-tst-pahwl02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%WORKSPACE%\jenkins_scripts\apply_service_packs.prc"
--- jenkins_scripts/apply_service_packs_prod.bat
+++
@@ -1,3 +0,0 @@
-echo %WORKSPACE%
-CD "%WORKSPACE%\service_packs"
-FOR %%i IN ("*.sql") DO sqlcmd -S KRR-SQL-PACLX02 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%WORKSPACE%\jenkins_scripts\apply_service_packs.prc"
