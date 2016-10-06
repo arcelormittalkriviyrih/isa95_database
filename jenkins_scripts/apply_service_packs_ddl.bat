d98fd36a7847dbfbd3cbe733c67dc767fb0c7f04
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-06 09:56:26 +0300

    try runas

--- jenkins_scripts/apply_service_packs_ddl.bat
+++ jenkins_scripts/apply_service_packs_ddl.bat
@@ -1,3 +1,3 @@
 echo %WORKSPACE%
 CD "%WORKSPACE%\service_packs\DDL"
-FOR %%i IN ("*.sql") DO sqlcmd -S %SERVER_NAME% -d KRR-PA-ISA95_PRODUCTION -U %DDL_USER% -P %DDL_PASSWORD% -v FileName="%%i" -i "%WORKSPACE%\jenkins_scripts\apply_service_packs.prc"
+FOR %%i IN ("*.sql") DO runas %DDL_USER%\%DDL_PASSWORD% sqlcmd -S %SERVER_NAME% -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%WORKSPACE%\jenkins_scripts\apply_service_packs.prc"
