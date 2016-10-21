8d15777a0e191a7bc7f97d9edb1a49ffdfb5bbb1
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-21 16:38:22 +0300

    merge all logs in one file

--- jenkins_scripts/apply_service_packs_dml.bat
+++ jenkins_scripts/apply_service_packs_dml.bat
@@ -1,2 +1,4 @@
 CD "%1\service_packs\DML"
 FOR %%i IN ("*.sql") DO sqlcmd -S %2 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%1\jenkins_scripts\apply_service_packs.prc" -o "%1\jenkins_scripts\dml_%%i.log"
+copy /Y /b dml_*.log dml.log
+del dml_*.log
