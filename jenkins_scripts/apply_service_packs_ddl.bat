f1528a57e286730302f10399fc2f35a6e05178c6
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-21 16:43:25 +0300

    use full path

--- jenkins_scripts/apply_service_packs_ddl.bat
+++ jenkins_scripts/apply_service_packs_ddl.bat
@@ -1,4 +1,4 @@
 CD "%1\service_packs\DDL"
 FOR %%i IN ("*.sql") DO sqlcmd -S %2 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%1\jenkins_scripts\apply_service_packs.prc" -o "%1\jenkins_scripts\ddl_%%i.log"
-copy /Y /b ddl_*.log ddl.log
-del ddl_*.log
+copy /Y /b "%1\jenkins_scripts\ddl_*.log" "%1\jenkins_scripts\ddl.log"
+del "%1\jenkins_scripts\ddl_*.log"
