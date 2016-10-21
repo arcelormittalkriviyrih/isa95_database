2473e9d164007a83bdb99d2e5b72d2551a8d254b
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-21 16:17:40 +0300

    fix problem with lost logs

--- jenkins_scripts/apply_service_packs_dml.bat
+++ jenkins_scripts/apply_service_packs_dml.bat
@@ -1,2 +1,2 @@
 CD "%1\service_packs\DML"
-FOR %%i IN ("*.sql") DO sqlcmd -S %2 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%1\jenkins_scripts\apply_service_packs.prc" -o "%1\jenkins_scripts\dml.log"
+FOR %%i IN ("*.sql") DO sqlcmd -S %2 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%1\jenkins_scripts\apply_service_packs.prc" -o "%1\jenkins_scripts\dml_%%i.log"
