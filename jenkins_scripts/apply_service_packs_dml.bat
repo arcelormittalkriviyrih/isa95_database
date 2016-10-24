964e77172e46f75302b73d370c190f0e893f32de
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-24 11:51:41 +0300

    fix problem when build was considered as succeded when there are failed scripts in the middle

--- jenkins_scripts/apply_service_packs_dml.bat
+++ jenkins_scripts/apply_service_packs_dml.bat
@@ -1,4 +1,7 @@
 CD "%1\service_packs\DML"
-FOR %%i IN ("*.sql") DO sqlcmd -S %2 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%1\jenkins_scripts\apply_service_packs.prc" -o "%1\jenkins_scripts\dml_%%i.log"
-copy /Y /b "%1\jenkins_scripts\dml_*.log" "%1\jenkins_scripts\dml.log"
+del "%1\jenkins_scripts\failed_dmls.log"
+SET SQLCMD_ERROR_LEVEL=0;
+FOR %%i IN ("*.sql") DO (sqlcmd -S %2 -d KRR-PA-ISA95_PRODUCTION -v FileName="%%i" -i "%1\jenkins_scripts\apply_service_packs.prc" -o "%1\jenkins_scripts\dml_%%i.log" || (echo Failed script: %%i >> "%1\jenkins_scripts\failed_dmls.log" & SET SQLCMD_ERROR_LEVEL=1))
+copy /Y /b "%1\jenkins_scripts\dml_*.log" "%1\jenkins_scripts\output_dml.log"
 del "%1\jenkins_scripts\dml_*.log"
+IF %SQLCMD_ERROR_LEVEL% EQU 1 exit /b 1
