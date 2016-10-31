0e0681fca67a0209d8980fda6af9eea05347cd1e
Oleksandr Chekmez <chekmez@nikama.com>
2016-10-31 11:42:06 +0200

    #336 get SAP linked server from SIDE

---
+++ service_packs/DML/2016_10_31_1_add_SAP_LINKED_SERVER.sql
@@ -0,0 +1,46 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+DECLARE @EquipmentClassID int;
+
+SELECT @EquipmentClassID = ID
+FROM dbo.EquipmentClass
+WHERE Code = N'SIDE';
+
+IF NOT EXISTS
+(
+	SELECT NULL
+	FROM dbo.EquipmentClassProperty
+	WHERE [Value] = N'SAP_LINKED_SERVER'
+)
+BEGIN
+	INSERT INTO dbo.EquipmentClassProperty( Description, [Value], EquipmentClassID )
+	VALUES( N'SAP', N'SAP_LINKED_SERVER', @EquipmentClassID );
+END;
+
+IF NOT EXISTS (SELECT NULL from EquipmentProperty where ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'SAP_LINKED_SERVER') and EquipmentID in (SELECT EquipmentID
+	   FROM EquipmentProperty
+	   WHERE ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'SIDE_ID' ) and Value=N'MS_250_1_LEFT'))
+    INSERT INTO dbo.EquipmentProperty( [Value], EquipmentID, ClassPropertyID )
+	   SELECT N'[KRR-SQL23-ZPP]', EquipmentID, dbo.get_EquipmentClassPropertyByValue( N'SAP_LINKED_SERVER' )
+	   FROM EquipmentProperty
+	   WHERE ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'SIDE_ID' ) and Value=N'MS_250_1_LEFT';
+
+GO
+
+IF NOT EXISTS (SELECT NULL from EquipmentProperty where ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'SAP_LINKED_SERVER') and EquipmentID in (SELECT EquipmentID
+	   FROM EquipmentProperty
+	   WHERE ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'SIDE_ID' ) and Value=N'MS_250_1_RIGHT'))
+    INSERT INTO dbo.EquipmentProperty( [Value], EquipmentID, ClassPropertyID )
+	   SELECT N'[KRR-SQL23-ZPP]', EquipmentID, dbo.get_EquipmentClassPropertyByValue( N'SAP_LINKED_SERVER' )
+	   FROM EquipmentProperty
+	   WHERE ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'SIDE_ID' ) and Value=N'MS_250_1_RIGHT';
+
+GO
+
+delete from WorkDefinition where WORKType='SAPExport';
+
+GO
