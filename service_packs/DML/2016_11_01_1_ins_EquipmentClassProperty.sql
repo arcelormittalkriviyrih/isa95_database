5c2de92b696a2ed6d21003ed9c77add59d99d499
EvgeniyShvets <Evgeniy.Shvets@arcelormittal.com>
2016-11-01 16:22:19 +0200

    Insert into EquipmentClassProperty

---
+++ service_packs/DML/2016_11_01_1_ins_EquipmentClassProperty.sql
@@ -0,0 +1,18 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+insert into [KRR-PA-ISA95_PRODUCTION].[dbo].[EquipmentClassProperty]
+  (       [Description]
+      ,[Value]
+      ,[EquipmentClassID])
+         values(
+         N'Идентификатор ЖД весов',
+         N'WEIGHBRIDGES_ID',
+         11
+         )
+
+
+GO
