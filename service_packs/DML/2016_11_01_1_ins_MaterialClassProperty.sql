89d5c588fc7ad3262c665d9f9b4a4f072960e2b2
EvgeniyShvets <Evgeniy.Shvets@arcelormittal.com>
2016-11-03 14:15:34 +0200

    Insert MaterialClassProperty

---
+++ service_packs/DML/2016_11_01_1_ins_MaterialClassProperty.sql
@@ -0,0 +1,19 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+INSERT INTO [dbo].[MaterialClassProperty]   ([Description],[Value],[MaterialClassID])  VALUES ( N'Вид лома', N'Scrap type',9 )
+INSERT INTO [dbo].[MaterialClassProperty]   ([Description],[Value],[MaterialClassID])  VALUES ( N'Вес нетто Дебет', N'Netto debit',9 )
+INSERT INTO [dbo].[MaterialClassProperty]   ([Description],[Value],[MaterialClassID])  VALUES ( N'Вес нетто Кредит', N'Netto credit',9 )
+INSERT INTO [dbo].[MaterialClassProperty]   ([Description],[Value],[MaterialClassID])  VALUES ( N'Вес брутто', N'Brutto',9 )
+INSERT INTO [dbo].[MaterialClassProperty]   ([Description],[Value],[MaterialClassID])  VALUES ( N'Отправитель', N'Sender',9 )
+INSERT INTO [dbo].[MaterialClassProperty]   ([Description],[Value],[MaterialClassID])  VALUES ( N'Получатель', N'Receiver',9 )
+
+    
+Update [dbo].[MaterialClass] SET [Description]=N'Лом', [Code]=N'Scrap'
+WHERE ID=9
+
+
+GO
