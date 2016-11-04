96e13392e7d9ec2c1e0be5d3fa7f2620d16f2c28
EvgeniyShvets <Evgeniy.Shvets@arcelormittal.com>
2016-11-04 13:53:28 +0200

    Fix Insert to MaterialLot FixProc TakeWeight

---
+++ service_packs/DDL/2016_11_04_2_Fix_Proc_ins_KP4_WorkPerformance.sql
@@ -0,0 +1,52 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+IF OBJECT_ID ('dbo.ins_KP4_WorkPerformance',N'P') IS NOT NULL
+  DROP PROCEDURE dbo.ins_KP4_WorkPerformance;
+GO
+
+/****** Object:  StoredProcedure [dbo].[upd_KP4_WorkPerformance]    Script Date: 02.11.2016 14:41:01 ******/
+
+GO
+-- exec in  ins_KP4_TakeWeight
+-- =============================================
+CREATE PROCEDURE [dbo].[ins_KP4_WorkPerformance]
+   @WorkPerformanceID int OUTPUT,
+   @Description    nvarchar(50),
+   @WeightBridgeID int
+ 
+AS
+BEGIN
+ 
+   if (select count(ID) from [dbo].[WorkPerformance] where [WorkState] = 'Active' and [WorkType] = 'WSH_'+cast(@WeightBridgeID as nvarchar)) > 0
+   RAISERROR ('WeightSheet has already been created',16,1);
+			   
+   SET @WorkPerformanceID=NEXT VALUE FOR [dbo].[gen_WorkPerformance];
+   INSERT INTO [dbo].[WorkPerformance]        (ID   ,[Description]  ,[StartTime]    ,[WorkState]    ,WorkType)
+   VALUES  (@WorkPerformanceID, 
+            @Description, 
+			CURRENT_TIMESTAMP, 
+			N'Active', 
+			'WSH_'+cast(@WeightBridgeID as nvarchar))
+
+   INSERT INTO [dbo].[MaterialLot] ([Description],[FactoryNumber], [Status],[Quantity])
+   VALUES (N'KP4-WorkPerformance', cast(@WorkPerformanceID as nvarchar), 0, 0);
+
+
+   INSERT INTO [dbo].[JobOrder] ([ID], [WorkType], [StartTime], [DispatchStatus], [Command])
+   VALUES   ((NEXT VALUE FOR [dbo].[gen_JobOrder]),  
+            N'KP4_WeightSheet', 
+			CURRENT_TIMESTAMP,
+			N'Active', 
+			'insert WorkPerformance ' + cast(@WorkPerformanceID as nvarchar));
+
+
+END
+
+GO
+
+
---
+++ service_packs/DDL/2016_11_04_4_Fix_ins_KP4_TakeWeight.sql
@@ -0,0 +1,70 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID ('dbo.ins_KP4_TakeWeight',N'P') IS NOT NULL
+  DROP PROCEDURE dbo.ins_KP4_TakeWeight;
+GO
+
+SET ANSI_NULLS ON
+GO
+
+
+
+
+-- Call from program Odata (button "Take weight")
+-- =============================================
+CREATE PROCEDURE [dbo].[ins_KP4_TakeWeight]
+   @WorkPerformanceID int,                 --  ID WeightSheet 
+   @WeightBridgeID    int,                 --  ID Equipment
+   @WeightSheetNumber nvarchar(50),        --  Number WeightSheet 
+   @WagonNumber    nvarchar(50),           --  Number wagon
+   @WorkResponseDescription  nvarchar(50), --  Number waybill
+   @Value      real,		               --  Weight
+   @WeightMode nvarchar(20),               --  Mode operation brutto\tara
+   @MaterialDefinitionID int,              -- Code scrap (CSH)
+   @ReceiverID int,            
+   @SenderID int           
+
+
+AS
+BEGIN
+
+ IF @WeightSheetNumber IS NULL or @WeightBridgeID is NULL
+	  RAISERROR ('Description param required',16,1);
+
+ 
+  BEGIN TRANSACTION insKP4_TakeWeight;
+
+  BEGIN TRY 
+
+     IF @WorkPerformanceID=0   --  First weighting, create  WeightSheet
+     BEGIN
+		EXEC [dbo].[ins_KP4_WorkPerformance] @WorkPerformanceID OUTPUT, @WeightSheetNumber, @WeightBridgeID
+	 END
+
+     IF @WeightMode='Weighting'   ---  operation brutto
+     BEGIN
+		  EXEC [dbo].[ins_KP4_WeightBrutto]  @WorkPerformanceID, @WagonNumber, @WorkResponseDescription,  @Value, @WeightMode, @MaterialDefinitionID,  @ReceiverID,  @SenderID, @WeightBridgeID
+	 END
+
+     IF @WeightMode='Taring'      ---  operation tara
+     BEGIN
+		  EXEC [dbo].[ins_KP4_WeightTara]    @WorkPerformanceID, @WagonNumber, @WorkResponseDescription,  @Value, @WeightMode
+	 END
+
+
+  COMMIT TRANSACTION  insKP4_TakeWeight; 
+  END TRY
+
+
+  BEGIN CATCH
+      ROLLBACK TRANSACTION insKP4_TakeWeight;
+	  RAISERROR ('Error transaction TakeWeight',16,1);
+	  select 'rollback'                                   ---!!!!!!!!! change!!!
+  END CATCH
+
+
+END
+GO
