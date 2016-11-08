b8bfe2d31bcad20c93b4a11e17755ad0c1a34e62
EvgeniyShvets <Evgeniy.Shvets@arcelormittal.com>
2016-11-08 10:14:14 +0200

    Movement Script From DDL To DML

---
+++ service_packs/DML/2016_11_08_1_UpdateEquipment.sql
@@ -0,0 +1,18 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+UPDATE [dbo].[Equipment]
+   SET 
+      [EquipmentLevel] = 'Area'
+     ,[Equipment] = 116
+ WHERE  [ID] = 298 or [ID] = 303 
+
+
+DELETE FROM [dbo].[Equipment]
+      WHERE id = 126 or id = 127
+
+Update [dbo].[Equipment]
+SET [Equipment] = 115
+Where [ID]=149 
+
+GO
