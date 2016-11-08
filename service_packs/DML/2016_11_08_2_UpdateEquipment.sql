408054904824f93389e9501b5a60b70613e13104
Timur.Kapalov <tbkapalov@arcelormittal.com>
2016-11-08 10:24:44 +0200

    fix script "UpdateEquipment"

---
+++ service_packs/DML/2016_11_08_2_UpdateEquipment.sql
@@ -0,0 +1,22 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+UPDATE [dbo].[Equipment]
+	SET 
+      [EquipmentLevel] = N'Area'
+     ,[Equipment] = 116
+WHERE [ID] in (298, 303) 
+
+
+DELETE FROM [dbo].[Equipment]
+      WHERE id in (126, 127)
+
+
+UPDATE [dbo].[Equipment]
+	SET [Equipment] = 115
+WHERE [ID]=149 
+
+GO
