9e487758d73ced58ab2eb25dd0f0b1eb14ed25fb
Timur.Kapalov <tbkapalov@arcelormittal.com>
2016-11-08 13:44:57 +0200

    alter views "v_KP4_Wagon", "v_AvailableWeighbridgesInfo", "v_KP4_PackagingUnitsProperty"

---
+++ service_packs/DDL/2016_11_08_2_Create_View_v_KP4_PackagingUnitsProperty.sql
@@ -0,0 +1,33 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID ('dbo.v_KP4_PackagingUnitsProperty',N'V') IS NOT NULL
+  DROP VIEW dbo.v_KP4_PackagingUnitsProperty;
+GO
+
+SET ANSI_PADDING OFF
+GO
+ 
+
+CREATE VIEW [dbo].[v_KP4_PackagingUnitsProperty]
+AS
+
+select top (100) percent
+	pu.ID,
+	pu.[Description]	as [Wagon],
+	pup.[Description]	as [Parameter],
+	pup.Value
+from 
+	dbo.PackagingUnits pu,
+	dbo.PackagingUnitsProperty pup
+where 
+	pu.ID = pup.PackagingUnitsID
+and pup.PackagingDefinitionPropertyID in (2)
+order by pup.ID
+
+GO
+
+
