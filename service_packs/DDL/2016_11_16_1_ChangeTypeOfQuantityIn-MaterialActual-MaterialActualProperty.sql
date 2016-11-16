185d41ce1a87db5ffa122a3a28f6b4587ee06042
Dmitry Fil <monkdaf@gmail.com>
2016-11-16 11:35:00 +0200

    change data type from INT to REAL in MaterialActual and MaterialActualProperty Rev.2

---
+++ service_packs/DDL/2016_11_16_1_ChangeTypeOfQuantityIn-MaterialActual-MaterialActualProperty.sql
@@ -0,0 +1,11 @@
+﻿--- use DB as default one
+USE [KRR-PA-ISA95_PRODUCTION]
+GO
+
+-- change data type from INT to REAL
+ALTER TABLE [dbo].[MaterialActual] ALTER COLUMN Quantity real
+GO
+
+-- change data type from INT to REAL
+ALTER TABLE [dbo].[MaterialActualProperty] ALTER COLUMN Quantity real
+GO
