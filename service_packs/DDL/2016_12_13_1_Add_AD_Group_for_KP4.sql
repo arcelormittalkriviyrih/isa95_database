768257232a782af35a9613b838c275a1373fbfec
EvgeniyShvets <Evgeniy.Shvets@arcelormittal.com>
2016-12-13 14:35:23 +0200

    Add New ADirectory Groups to KP4 Project.

---
+++ service_packs/DDL/2016_12_13_1_Add_AD_Group_for_KP4.sql
@@ -0,0 +1,15 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+
+
+ALTER TABLE [KRR-PA-ISA95_PRODUCTION].[dbo].[KPPRoles]
+DROP CONSTRAINT RoleName
+
+INSERT INTO [dbo].[KPPRoles] ([RoleName],[ADRoleName]) VALUES (  'WeightAnalytics','EUROPE\KRR-LG-PA-Weight_Analytics-Stakeholders')
+INSERT INTO [dbo].[KPPRoles] ([RoleName],[ADRoleName]) VALUES (  'WeightAnalytics','EUROPE\KRR-LG-PA-Developers_DB')
+
+GO
