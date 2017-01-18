bcb0afbb8a4eb7c60b0b7ea7cb3f50d3789c3931
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-01-18 11:57:28 +0200

    fix if overwrite file

--- jenkins_scripts/copy_inretnal_scripts.bat
+++ jenkins_scripts/copy_inretnal_scripts.bat
@@ -1,7 +1,7 @@
 @echo on
 cd "%WORKSPACE%\service_packs_internal"
 if ERRORLEVEL 1 exit 1
-echo Y | XCOPY "*" "..\service_packs" /s /i
+XCOPY "*" "..\service_packs" /s /i /y
 if ERRORLEVEL 1 exit 1
 del ..\service_packs\.git
 if ERRORLEVEL 1 exit 1
