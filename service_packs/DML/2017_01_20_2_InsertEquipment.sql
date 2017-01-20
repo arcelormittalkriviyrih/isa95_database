131521985e4ff9f9dc4cd86b4b4d3c3909a5b808
iagamezo <ivan.gamezo@arcelormittal.com>
2017-01-20 09:29:59 +0200

    Delete existing BF

---
+++ service_packs/DML/2017_01_20_2_InsertEquipment.sql
@@ -0,0 +1,13 @@
+﻿SET NOCOUNT ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+  delete [KRR-PA-ISA95_PRODUCTION].[dbo].[Equipment]
+  where id in('356','357','358','359')
+
+GO
+
+
+SET QUOTED_IDENTIFIER OFF
+GO
