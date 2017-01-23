629f14f21af10cdb11d62aa9d270199c2c70f619
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-01-23 12:26:06 +0200

    fix transfer KEP_Analytics_Weight_XXX

--- service_packs/DDL/2016_12_16_1_alter_schema.sql
+++ service_packs/DDL/2016_12_16_1_alter_schema.sql
@@ -1,3 +1,6 @@
-﻿
-ALTER SCHEMA AMKR_WEIGHING TRANSFER OBJECT::dbo.KEP_Analytics_Weight;  
-ALTER SCHEMA AMKR_WEIGHING TRANSFER OBJECT::dbo.KEP_Analytics_Weight_archive; 
+﻿IF (OBJECT_ID ('dbo.KEP_Analytics_Weight',N'U') IS NOT NULL and OBJECT_ID ('AMKR_WEIGHING.KEP_Analytics_Weight',N'U') IS NULL)
+	ALTER SCHEMA AMKR_WEIGHING TRANSFER OBJECT::dbo.KEP_Analytics_Weight;  
+go
+IF (OBJECT_ID ('dbo.KEP_Analytics_Weight_archive',N'U') IS NOT NULL and OBJECT_ID ('AMKR_WEIGHING.KEP_Analytics_Weight_archive',N'U') IS NULL)
+	ALTER SCHEMA AMKR_WEIGHING TRANSFER OBJECT::dbo.KEP_Analytics_Weight_archive; 
+go
