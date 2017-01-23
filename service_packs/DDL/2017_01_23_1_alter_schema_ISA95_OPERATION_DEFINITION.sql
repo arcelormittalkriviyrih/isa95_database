5a6d88f7686942a2b7361363b366098121e4aaeb
Sergey Arhipov <Sergey.Arhipov@arcelormittal.com>
2017-01-23 12:07:51 +0200

    fix transfer WITH SCHEMABINDING

--- service_packs/DDL/2017_01_23_1_alter_schema_ISA95_OPERATION_DEFINITION.sql
+++ service_packs/DDL/2017_01_23_1_alter_schema_ISA95_OPERATION_DEFINITION.sql
@@ -1,40 +1,56 @@
+--| drop SCHEMABINDING for ParameterSpecification
+IF OBJECT_ID ('dbo.v_ParameterSpecification_Order',N'V') IS NOT NULL
+  DROP VIEW dbo.[v_ParameterSpecification_Order];
+GO
 
-IF OBJECT_ID ('dbo.OperationsDefinition',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsDefinition; 
+begin try
+    BEGIN TRANSACTION
+		IF OBJECT_ID ('dbo.OperationsDefinition',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsDefinition; 
+		IF OBJECT_ID ('dbo.OperationsMaterialBill',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsMaterialBill; 
+		IF OBJECT_ID ('dbo.OperationsMaterialBillItem',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsMaterialBillItem; 
+		IF OBJECT_ID ('dbo.OperationsSegment',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsSegment; 
+		IF OBJECT_ID ('dbo.MaterialSpecification',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.MaterialSpecification; 
+		IF OBJECT_ID ('dbo.MaterialSpecificationProperty',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.MaterialSpecificationProperty;
+		IF OBJECT_ID ('dbo.EquipmentSpecification',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.EquipmentSpecification; 
+		IF OBJECT_ID ('dbo.EquipmentSpecificationProperty',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.EquipmentSpecificationProperty; 
+		IF OBJECT_ID ('dbo.ParameterSpecification',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.ParameterSpecification; 
+		IF OBJECT_ID ('dbo.PersonnelSpecification',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PersonnelSpecification; 
+		IF OBJECT_ID ('dbo.PersonnelSpecificationProperty',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PersonnelSpecificationProperty;
+		IF OBJECT_ID ('dbo.PhysicalAssetSpecification',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PhysicalAssetSpecification; 
+		IF OBJECT_ID ('dbo.PhysicalAssetSpecificationProperty',N'U') IS NOT NULL
+			ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PhysicalAssetSpecificationProperty; 
+    COMMIT TRANSACTION
+--	ROLLBACK TRANSACTION
+end try
+begin catch
+	print  
+    'Msg '+cast(ERROR_NUMBER() as nvarchar(50))+', '+
+    'Level '+cast(ERROR_SEVERITY() as nvarchar(50))+', '+
+    'State '+cast(ERROR_STATE() as nvarchar(50))+', '+
+    'Line '+cast(ERROR_LINE() as nvarchar(50))+char(10)+
+    ERROR_MESSAGE();
+	ROLLBACK TRANSACTION
+end catch
 go
-IF OBJECT_ID ('dbo.OperationsMaterialBill',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsMaterialBill; 
-go
-IF OBJECT_ID ('dbo.OperationsMaterialBillItem',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsMaterialBillItem; 
-go
-IF OBJECT_ID ('dbo.OperationsSegment',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.OperationsSegment; 
-go
-IF OBJECT_ID ('dbo.MaterialSpecification',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.MaterialSpecification; 
-go
-IF OBJECT_ID ('dbo.MaterialSpecificationProperty',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.MaterialSpecificationProperty;
-go
-IF OBJECT_ID ('dbo.EquipmentSpecification',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.EquipmentSpecification; 
-go
-IF OBJECT_ID ('dbo.EquipmentSpecificationProperty',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.EquipmentSpecificationProperty; 
-go
-IF OBJECT_ID ('dbo.ParameterSpecification',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.ParameterSpecification; 
-go
-IF OBJECT_ID ('dbo.PersonnelSpecification',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PersonnelSpecification; 
-go
-IF OBJECT_ID ('dbo.PersonnelSpecificationProperty',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PersonnelSpecificationProperty;
-go
-IF OBJECT_ID ('dbo.PhysicalAssetSpecification',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PhysicalAssetSpecification; 
-go
-IF OBJECT_ID ('dbo.PhysicalAssetSpecificationProperty',N'U') IS NOT NULL
-	ALTER SCHEMA [ISA95_OPERATION_DEFINITION] TRANSFER OBJECT::dbo.PhysicalAssetSpecificationProperty; 
+
+--| restore view SCHEMABINDING
+CREATE VIEW [dbo].[v_ParameterSpecification_Order] WITH SCHEMABINDING
+AS
+SELECT sp.ID, sp.[Value], sp.[Description], sp.[WorkDefinitionID], oes.[EquipmentID], sp.PropertyType
+FROM [ISA95_OPERATION_DEFINITION].[ParameterSpecification] sp
+     INNER JOIN [dbo].[OpEquipmentSpecification] oes ON (oes.[WorkDefinition]=sp.[WorkDefinitionID])
+     INNER JOIN [dbo].[WorkDefinition] wd ON (wd.[ID]=sp.[WorkDefinitionID] AND wd.[WorkType]=N'Standard')
+     INNER JOIN[dbo].[PropertyTypes] pt ON (pt.ID=sp.PropertyType AND pt.Value=N'COMM_ORDER')
 go
