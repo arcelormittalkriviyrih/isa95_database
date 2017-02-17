b9082164808926b499685b80144494b4b854a30b
Oleksandr Chekmez <chekmez@nikama.com>
2017-02-17 13:27:14 +0200

    #353 optimize Stat and Graphics views

--- service_packs/DDL/2017_02_17_1_alter_KEP_logger_archive.sql
+++ service_packs/DDL/2017_02_17_1_alter_KEP_logger_archive.sql
@@ -1,6 +1,21 @@
 ﻿SET NUMERIC_ROUNDABORT OFF;
 GO
+
 SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+
+IF NOT EXISTS(SELECT NULL
+              FROM information_schema.columns
+              WHERE table_name = 'KEP_logger_archive'
+                AND column_name = 'WEIGHT_TIMESTAMP')
+   ALTER TABLE dbo.KEP_logger_archive ADD [WEIGHT_TIMESTAMP] AS ToDateTimeOffset([TIMESTAMP],0)
+GO
+
+IF EXISTS (SELECT NULL FROM sys.indexes WHERE name='i1_KEP_Logger_Archive' AND object_id = OBJECT_ID('[dbo].[KEP_logger_archive]'))
+   DROP INDEX [i1_KEP_Logger_Archive] ON [dbo].[KEP_logger_archive]
+GO
+
+CREATE INDEX i1_KEP_Logger_Archive ON [dbo].[KEP_logger_archive] ([WEIGHT_TIMESTAMP])
 GO
 
 IF OBJECT_ID ('dbo.v_ScalesWeightHistory',N'V') IS NOT NULL
