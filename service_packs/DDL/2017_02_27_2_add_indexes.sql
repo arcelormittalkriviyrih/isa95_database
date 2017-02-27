1dd54c3fd1678bf97f0f08c0d985bf1b68797034
Oleksandr Chekmez <chekmez@nikama.com>
2017-02-27 10:12:19 +0200

    #356 add indexes to optimize queries

---
+++ service_packs/DDL/2017_02_27_2_add_indexes.sql
@@ -0,0 +1,24 @@
+﻿SET NUMERIC_ROUNDABORT OFF;
+GO
+
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+
+IF EXISTS (SELECT NULL FROM sys.indexes WHERE name='i3_JobOrder' AND object_id = OBJECT_ID('[dbo].[JobOrder]'))
+   DROP INDEX [i3_JobOrder] ON [dbo].[JobOrder]
+GO
+
+CREATE NONCLUSTERED INDEX i3_JobOrder
+ON [dbo].[JobOrder] ([WorkType],[DispatchStatus])
+INCLUDE ([StartTime],[Command])
+GO
+
+
+IF EXISTS (SELECT NULL FROM sys.indexes WHERE name='i1_Parameter' AND object_id = OBJECT_ID('[dbo].[Parameter]'))
+   DROP INDEX [i1_Parameter] ON [dbo].[Parameter]
+GO
+
+CREATE NONCLUSTERED INDEX i1_Parameter
+ON [dbo].[Parameter] ([PropertyType])
+INCLUDE ([Value],[JobOrder])
+GO
