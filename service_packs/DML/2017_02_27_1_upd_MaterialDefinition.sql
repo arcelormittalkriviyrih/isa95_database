f46640eee7dcf5050e028ae3d38b98fb92fa389a
monkdaf <monkdaf@gmail.com>
2017-02-27 16:49:01 +0200

    update description in MaterialDefinition

---
+++ service_packs/DML/2017_02_27_1_upd_MaterialDefinition.sql
@@ -0,0 +1,21 @@
+﻿USE [KRR-PA-ISA95_PRODUCTION]
+GO
+SET NOCOUNT ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+UPDATE [dbo].[MaterialDefinition] 
+  SET [Description]=N'Кислород', [MaterialClassID]=10000
+  Where ID=10000
+GO
+
+UPDATE [dbo].[MaterialDefinition] 
+  SET [Description]=N'Азот', [MaterialClassID]=10000
+  Where ID=10001
+GO
+
+UPDATE [dbo].[MaterialDefinition] 
+  SET [Description]=N'Сжатый воздух', [MaterialClassID]=10000
+  Where ID=10002
+GO
