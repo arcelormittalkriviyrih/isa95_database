c0c9e62ebe5888b1f2ecc22689fc0ea662b9ac39
Oleksandr Chekmez <chekmez@nikama.com>
2017-03-09 10:28:00 +0200

    fix error check

---
+++ service_packs/DDL/2017_03_09_02_fix_error_check.sql
@@ -0,0 +1,361 @@
+﻿SET NUMERIC_ROUNDABORT OFF;
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+
+IF OBJECT_ID ('dbo.v_DiagInfo',N'V') IS NOT NULL
+   DROP VIEW dbo.[v_DiagInfo];
+GO
+/*
+   View: [v_DiagInfo]
+    Возвращает диагностические данные по оборудованию
+*/
+CREATE VIEW [dbo].[v_DiagInfo]
+AS
+WITH Kep_Data
+          AS (SELECT *
+              FROM
+              (
+                  SELECT ROW_NUMBER() OVER(PARTITION BY kl.[NUMBER_POCKET] ORDER BY kl.[TIMESTAMP] DESC) RowNumber,
+                         kl.[WEIGHT_CURRENT],
+                         kl.[WEIGHT_STAB],
+                         kl.[WEIGHT_ZERO],
+                         kl.[COUNT_BAR],
+                         kl.[REM_BAR],
+                         kl.[AUTO_MANU],
+                         kl.[POCKET_LOC],
+                         kl.[PACK_SANDWICH],
+                         kl.[NUMBER_POCKET],
+					kl.[ALARM]
+                  FROM dbo.KEP_logger kl
+                  WHERE kl.[TIMESTAMP] >= DATEADD(hour, -1, GETDATE())
+              ) ww
+              WHERE ww.RowNumber = 1)
+SELECT equip.ID,
+       equip.[Description] EquipmentName,
+	  side.ID SideID,
+	  CASE kd.ALARM
+	  WHEN 0 THEN N'OK'
+	  WHEN 1 THEN N'Error'
+	  WHEN NULL THEN NULL
+	  END EquipmentStatus
+FROM dbo.Equipment side
+     INNER JOIN dbo.EquipmentClass ec ON (ec.ID = side.EquipmentClassID and ec.Code=N'SIDE')	
+     INNER JOIN dbo.Equipment equip ON (equip.Equipment = side.ID)
+	INNER JOIN dbo.EquipmentClass ecs ON (ecs.ID = equip.EquipmentClassID and ecs.Code=N'SCALES')	
+     INNER JOIN dbo.EquipmentProperty eqp ON(eqp.EquipmentID = equip.ID)
+     INNER JOIN dbo.EquipmentClassProperty ecp ON(ecp.ID = eqp.ClassPropertyID AND ecp.value = N'SCALES_NO')
+     LEFT OUTER JOIN Kep_Data kd ON(ISNUMERIC(eqp.value) = 1 AND kd.[NUMBER_POCKET] = CAST(eqp.value AS INT))     
+UNION
+SELECT abs(Checksum(NewID())),
+	  N'PLC',
+	  side.ID,
+	  IIF(diag.Controller_Last_Connect IS NULL, NULL, IIF(DATEDIFF(second, diag.Controller_Last_Connect,CURRENT_TIMESTAMP)>15,N'Error',N'OK'))	  
+FROM dbo.Equipment side
+     INNER JOIN dbo.EquipmentClass ec ON (ec.ID = side.EquipmentClassID and ec.Code=N'SIDE')
+     INNER JOIN dbo.EquipmentProperty ep ON (ep.EquipmentID = side.ID and ep.ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'CONTROLLER_ID' ))
+	LEFT OUTER JOIN dbo.kep_controller_diag diag ON (cast(diag.Controller_ID as nvarchar) = ep.[Value])
+UNION
+SELECT abs(Checksum(NewID())),
+ 	  N'UPS',
+	  side.ID,
+	  CASE diag.UPS_FAIL
+	  WHEN 0 THEN N'OK'
+	  WHEN 1 THEN N'Error'
+	  WHEN NULL THEN NULL
+	  END
+FROM dbo.Equipment side
+     INNER JOIN dbo.EquipmentClass ec ON (ec.ID = side.EquipmentClassID and ec.Code=N'SIDE')
+     INNER JOIN dbo.EquipmentProperty ep ON (ep.EquipmentID = side.ID and ep.ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'CONTROLLER_ID' ))
+	LEFT OUTER JOIN dbo.kep_controller_diag diag ON (cast(diag.Controller_ID as nvarchar) = ep.[Value])
+UNION
+SELECT equip.ID,
+	  equip.[Description],
+	  side.ID,
+	  dbo.get_EquipmentPropertyValue(equip.ID,N'PRINTER_STATUS')	  
+FROM dbo.Equipment side
+     INNER JOIN dbo.EquipmentClass ec ON (ec.ID = side.EquipmentClassID and ec.Code=N'SIDE')	
+     INNER JOIN dbo.Equipment equip ON (equip.Equipment = side.ID)
+	INNER JOIN dbo.EquipmentClass ecs ON (ecs.ID = equip.EquipmentClassID and ecs.Code=N'PRINTER')	;
+
+GO
+
+
+IF OBJECT_ID ('dbo.v_MarkerErrors', N'V') IS NOT NULL
+   DROP VIEW dbo.v_MarkerErrors;
+GO
+/*
+   View: v_MarkerErrors
+    Возвращает список ошибок в системе для маркировщицы.
+*/
+CREATE VIEW [dbo].[v_MarkerErrors]
+AS
+     SELECT ABS(CHECKSUM(NEWID())) AS ID,
+            N'Нет связи с САП' AS ErrorMessage
+     WHERE EXISTS
+     (
+         SELECT NULL
+         FROM [dbo].[MaterialLot] AS ml
+         WHERE DATEDIFF(second, ml.CreateTime, CURRENT_TIMESTAMP) > 30
+               AND ml.[Quantity] != 0
+               AND EXISTS
+         (
+             SELECT NULL
+             FROM JobOrder AS o,
+                  [dbo].[Parameter] AS p,
+                  [dbo].[PropertyTypes] AS pt
+             WHERE o.ID = p.JobOrder
+                   AND p.PropertyType = pt.ID
+                   AND pt.[Value] = N'MaterialLotID'
+                   AND o.WorkType = N'SAPExport'
+                   AND o.DispatchStatus = N'TODO'
+                   AND p.[Value] = ml.[ID]
+         )
+               AND EXISTS
+         (
+             SELECT NULL
+             FROM dbo.Person p,
+                  dbo.PersonProperty pp,
+                  dbo.PersonnelClassProperty pcp,
+                  dbo.Equipment side,
+                  dbo.EquipmentClassProperty ecp,
+                  dbo.EquipmentProperty ep
+             WHERE pp.PersonID = p.ID
+                   AND pcp.Value = 'WORK_WITH'
+                   AND pcp.ID = pp.ClassPropertyID
+                   AND p.ID = dbo.get_CurrentPerson()
+                   AND ep.Value = pp.Value
+                   AND ecp.Value = 'SIDE_ID'
+                   AND ep.EquipmentID = side.ID
+                   AND ep.ClassPropertyID = ecp.ID
+                   AND side.id = dbo.get_SideIdByMaterialLotID(ml.[ID])
+         )
+     )
+UNION
+SELECT ABS(CHECKSUM(NEWID())) AS ID,
+	N'Свойство "'+epr.Description+N'" не задано для весов "'+e.Description +N'" ('+workshop.Description+N'\'+mill.Description+N'\'+side.[Description]+N')'
+     FROM dbo.Person p,
+          dbo.PersonProperty pp,
+          dbo.PersonnelClassProperty pcp,
+          dbo.Equipment e,
+          dbo.Equipment side,
+          dbo.EquipmentClassProperty ecp,
+          dbo.EquipmentProperty ep,
+          dbo.EquipmentClass ec,
+		  dbo.EquipmentClassProperty epr,
+          dbo.Equipment mill,
+		  dbo.Equipment workshop
+     WHERE pp.PersonID = p.ID
+           AND pcp.Value = 'WORK_WITH'
+           AND pcp.ID = pp.ClassPropertyID
+           AND p.ID = dbo.get_CurrentPerson()
+           AND ep.Value = pp.Value
+           AND ecp.Value = 'SIDE_ID'
+           AND ep.EquipmentID = side.ID
+           AND ep.ClassPropertyID = ecp.ID
+           AND side.ID = e.Equipment
+           AND ec.Code = N'SCALES'
+           AND ec.ID = e.EquipmentClassID
+		   AND mill.id = side.Equipment
+		   AND workshop.id=mill.Equipment
+		   AND epr.Value in (N'USED_PRINTER',
+								N'SCALES_TYPE',
+								N'SCALES_NO',
+								N'OPC_DEVICE_NAME')
+		   AND NOT EXISTS (select * from EquipmentProperty epv 
+								where epv.ClassPropertyID=epr.ID 
+									and epv.EquipmentID=e.ID)
+UNION
+SELECT ABS(CHECKSUM(NEWID())) AS ID,
+            N'Команды не отправляются в ПЛК' AS ErrorMessage
+     WHERE EXISTS
+     (
+SELECT NULL
+     FROM dbo.Person p,
+          dbo.PersonProperty pp,
+          dbo.PersonnelClassProperty pcp,
+          dbo.Equipment e,
+          dbo.Equipment side,
+          dbo.EquipmentClassProperty ecp,
+          dbo.EquipmentProperty ep,
+          dbo.EquipmentClass ec,
+		  dbo.EquipmentClassProperty epr,
+		  dbo.EquipmentProperty epv,
+		  dbo.JobOrder jo
+     WHERE pp.PersonID = p.ID
+           AND pcp.Value = 'WORK_WITH'
+           AND pcp.ID = pp.ClassPropertyID
+           AND p.ID = dbo.get_CurrentPerson()
+           AND ep.Value = pp.Value
+           AND ecp.Value = 'SIDE_ID'
+           AND ep.EquipmentID = side.ID
+           AND ep.ClassPropertyID = ecp.ID
+           AND side.ID = e.Equipment
+           AND ec.Code = N'SCALES'
+           AND ec.ID = e.EquipmentClassID		
+		   AND epr.Value = N'OPC_DEVICE_NAME'
+		   AND epv.ClassPropertyID=epr.ID 
+		   and epv.EquipmentID=e.ID
+		   AND jo.WorkType = N'KEPCommands'
+		   AND jo.DispatchStatus=N'ToSend'   
+		   AND epv.Value=  left(Command, len(Command)- charindex('.', reverse(Command)))
+		   and DATEDIFF(second, jo.StartTime, CURRENT_TIMESTAMP) > 30 )
+UNION
+SELECT ABS(CHECKSUM(NEWID())) AS ID,
+	N'Пропало питание на ПЛК для '+workshop.Description+N'\'+mill.Description+N'\'+side.[Description]
+     FROM dbo.Person p,
+          dbo.PersonProperty pp,
+          dbo.PersonnelClassProperty pcp,
+          dbo.Equipment side,
+          dbo.EquipmentClassProperty ecp,
+          dbo.EquipmentProperty ep,          
+          dbo.Equipment mill,
+		  dbo.Equipment workshop,
+		  dbo.EquipmentProperty esp,
+		  dbo.kep_controller_diag diag
+     WHERE pp.PersonID = p.ID
+           AND pcp.Value = 'WORK_WITH'
+           AND pcp.ID = pp.ClassPropertyID
+           AND p.ID = dbo.get_CurrentPerson()
+           AND ep.Value = pp.Value
+           AND ecp.Value = 'SIDE_ID'
+           AND ep.EquipmentID = side.ID
+           AND ep.ClassPropertyID = ecp.ID
+           AND mill.id = side.Equipment
+		   AND workshop.id=mill.Equipment		   
+		   AND esp.EquipmentID = side.ID 
+		   and esp.ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'CONTROLLER_ID' )
+		   AND cast(diag.Controller_ID as nvarchar) = esp.[Value]
+		   AND diag.UPS_FAIL=1
+UNION
+SELECT ABS(CHECKSUM(NEWID())) AS ID,
+	N'Нет связи с ПЛК для '+workshop.Description+N'\'+mill.Description+N'\'+side.[Description]
+     FROM dbo.Equipment side
+		  INNER JOIN dbo.EquipmentProperty ep ON (ep.EquipmentID = side.ID)
+		  INNER JOIN dbo.EquipmentClassProperty ecp ON (ep.ClassPropertyID = ecp.ID AND ecp.Value = 'SIDE_ID')
+		  INNER JOIN dbo.PersonProperty pp ON (ep.Value = pp.Value)			
+          INNER JOIN dbo.Person p ON (pp.PersonID = p.ID)
+		  INNER JOIN dbo.PersonnelClassProperty pcp ON (pcp.ID = pp.ClassPropertyID AND pcp.Value = 'WORK_WITH')
+		  INNER JOIN dbo.Equipment mill ON (mill.id = side.Equipment)
+		  INNER JOIN dbo.Equipment workshop ON (workshop.id=mill.Equipment)
+		  INNER JOIN dbo.EquipmentProperty esp ON (esp.EquipmentID = side.ID AND esp.ClassPropertyID=dbo.get_EquipmentClassPropertyByValue( N'CONTROLLER_ID' ))
+		  LEFT OUTER JOIN dbo.kep_controller_diag diag ON (cast(diag.Controller_ID as nvarchar) = esp.[Value])
+     WHERE p.ID = dbo.get_CurrentPerson()           
+           AND (DATEDIFF(second, diag.Controller_Last_Connect,CURRENT_TIMESTAMP)>15 OR diag.Controller_Last_Connect IS NULL)
+UNION
+SELECT ABS(CHECKSUM(NEWID())) AS ID,
+	N'Свойство "'+epr.Description+N'" не задано для стороны стана "'+side.Description +N'" ('+workshop.Description+N'\'+mill.Description+N')'
+     FROM dbo.Person p,
+          dbo.PersonProperty pp,
+          dbo.PersonnelClassProperty pcp,
+          dbo.Equipment side,
+          dbo.EquipmentClassProperty ecp,
+          dbo.EquipmentProperty ep,
+          dbo.EquipmentClassProperty epr,
+          dbo.Equipment mill,
+		  dbo.Equipment workshop
+     WHERE pp.PersonID = p.ID
+           AND pcp.Value = 'WORK_WITH'
+           AND pcp.ID = pp.ClassPropertyID
+           AND p.ID = dbo.get_CurrentPerson()
+           AND ep.Value = pp.Value
+           AND ecp.Value = 'SIDE_ID'
+           AND ep.EquipmentID = side.ID
+           AND ep.ClassPropertyID = ecp.ID
+           AND mill.id = side.Equipment
+		   AND workshop.id=mill.Equipment
+		   AND epr.Value in (N'CONTROLLER_ID')
+		   AND NOT EXISTS (select * from EquipmentProperty epv 
+								where epv.ClassPropertyID=epr.ID 
+									and epv.EquipmentID=side.ID);
+
+GO
+
+
+IF OBJECT_ID ('dbo.v_ScalesDetailInfo',N'V') IS NOT NULL
+   DROP VIEW dbo.[v_ScalesDetailInfo];
+GO
+/*
+   View: v_ScalesDetailInfo
+    Возвращает данные из контроллера а также расчитывает количество прудков для весов.
+	Используется на экране маркиорвщицы.
+*/
+CREATE VIEW [dbo].[v_ScalesDetailInfo]
+AS
+     WITH BarWeight
+          AS (SELECT CAST(par.[Value] AS FLOAT) PropertyValue,
+                     pr.[Value] PropertyType,
+                     ep.EquipmentId
+              FROM [PropertyTypes] pr,
+                   [dbo].[Parameter] par,
+                   dbo.[EquipmentProperty] ep
+              WHERE pr.[Value] IN(N'BAR_WEIGHT', N'MIN_WEIGHT', N'MAX_WEIGHT', N'BAR_QUANTITY')
+              AND pr.ID = par.PropertyType
+              AND ep.[ClassPropertyID] = dbo.get_EquipmentClassPropertyByValue(N'JOB_ORDER_ID')
+          AND cast(ep.[Value] as nvarchar) = par.JobOrder),
+          Kep_Data
+          AS (SELECT *
+              FROM
+              (
+                  SELECT ROW_NUMBER() OVER(PARTITION BY kl.[NUMBER_POCKET] ORDER BY kl.[TIMESTAMP] DESC) RowNumber,
+                         kl.[WEIGHT_CURRENT],
+                         kl.[WEIGHT_STAB],
+                         kl.[WEIGHT_ZERO],
+                         kl.[COUNT_BAR],
+                         kl.[REM_BAR],
+                         kl.[AUTO_MANU],
+                         kl.[POCKET_LOC],
+                         kl.[PACK_SANDWICH],
+                         kl.[NUMBER_POCKET],
+                         kl.[ALARM],
+                         kl.[KEY_MANU],
+						 kl.[EN_BUTTON_TARA],
+						 kl.[WEIGHT_OK],
+						 kl.[PEREBOR]
+                  FROM dbo.KEP_logger kl
+                  WHERE kl.[TIMESTAMP] >= DATEADD(hour, -1, GETDATE())
+              ) ww
+              WHERE ww.RowNumber = 1)
+          SELECT eq.ID AS ID,
+                 eq.[Description] AS ScalesName,
+                 dbo.get_RoundedWeightByEquipment(kd.WEIGHT_CURRENT, eq.ID) WEIGHT_CURRENT,
+                 kd.WEIGHT_STAB,
+                 kd.WEIGHT_ZERO,
+                 kd.AUTO_MANU,
+                 kd.POCKET_LOC,
+                 kd.PACK_SANDWICH,
+                 kd.ALARM,
+                 kd.KEY_MANU,
+				 kd.EN_BUTTON_TARA,
+                 CAST(FLOOR(dbo.get_RoundedWeightByEquipment(kd.WEIGHT_CURRENT, eq.ID) / bw.PropertyValue) AS   INT) RodsQuantity,
+                 kd.REM_BAR RodsLeft,
+                 ISNULL(CAST(bminW.PropertyValue AS INT), 0) MinWeight,
+                 ISNULL(CAST(bmaxW.PropertyValue AS INT), 0) MaxWeight,
+                 ISNULL(dbo.get_RoundedWeight(bmaxW.PropertyValue * 1.2, 'UP', 100), dbo.get_EquipmentPropertyValue(eq.ID, N'MAX_WEIGHT')) MaxPossibleWeight,
+                 ISNULL(dbo.get_EquipmentPropertyValue(eq.ID, N'SCALES_TYPE'), N'POCKET') SCALES_TYPE,
+                 dbo.get_EquipmentPropertyValue(eq.ID, N'PACK_RULE') PACK_RULE,
+                 CAST(bQty.PropertyValue AS INT)		BAR_QUANTITY,
+                 ISNULL(dbo.get_EquipmentPropertyValue(eq.ID, N'TAKE_WEIGHT_LOCKED'), '0') TAKE_WEIGHT_LOCKED,
+				 kd.[WEIGHT_OK],
+				 kd.[PEREBOR],
+				 (SELECT 1 WHERE EXISTS (SELECT NULL FROM dbo.JobOrder jo WHERE jo.WorkType = N'KEPCommands'
+																		   AND jo.DispatchStatus=N'ToSend'   
+																		   AND CAST(epv.Value as nvarchar)=  left(Command, len(Command)- charindex('.', reverse(Command)))
+																		   and DATEDIFF(second, jo.StartTime, CURRENT_TIMESTAMP) > 15 )
+						  OR EXISTS (SELECT * from dbo.kep_controller_diag diag where DATEDIFF(second, diag.Controller_Last_Connect,CURRENT_TIMESTAMP)>15 
+																			AND (cast(diag.Controller_ID as nvarchar) = CAST(epv2.[Value] as nvarchar)))) Send_OPC_Error
+          FROM dbo.Equipment eq
+               INNER JOIN dbo.EquipmentProperty eqp ON(eqp.EquipmentID = eq.ID)
+               INNER JOIN dbo.EquipmentClassProperty ecp ON(ecp.ID = eqp.ClassPropertyID
+                                                            AND ecp.value = N'SCALES_NO')
+			   INNER JOIN dbo.EquipmentProperty epv ON (epv.EquipmentID=eq.ID)
+			   INNER JOIN dbo.EquipmentClassProperty epr ON (epv.ClassPropertyID=epr.ID and CAST(epr.Value as nvarchar) = N'OPC_DEVICE_NAME')
+			   INNER JOIN dbo.Equipment side ON (side.ID = eq.Equipment)
+			   INNER JOIN dbo.EquipmentProperty epv2 ON (epv2.EquipmentID=side.ID)
+			   INNER JOIN dbo.EquipmentClassProperty epr2 ON (epv2.ClassPropertyID=epr2.ID and CAST(epr2.Value as nvarchar) = N'CONTROLLER_ID')
+               LEFT OUTER JOIN Kep_Data kd ON(ISNUMERIC(eqp.value) = 1 AND kd.[NUMBER_POCKET] = CAST(eqp.value AS INT))
+               LEFT OUTER JOIN BarWeight bw ON bw.EquipmentId = eq.ID AND bw.PropertyType = N'BAR_WEIGHT'
+               LEFT OUTER JOIN BarWeight bminW ON bminW.EquipmentId = eq.ID AND bminW.PropertyType = N'MIN_WEIGHT'
+               LEFT OUTER JOIN BarWeight bmaxW ON bmaxW.EquipmentId = eq.ID AND bmaxW.PropertyType = N'MAX_WEIGHT'
+               LEFT OUTER JOIN BarWeight bQty ON bQty.EquipmentId = eq.ID AND bQty.PropertyType = N'BAR_QUANTITY';
+GO
