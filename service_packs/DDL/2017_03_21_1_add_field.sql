2ff2c5eee2ff2ce790e7680e00459ae05b85b055
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-03-21 14:30:37 +0200

    Alter tables PackagingUnitsProperty and JobResponse

---
+++ service_packs/DDL/2017_03_21_1_add_field.sql
@@ -0,0 +1,16 @@
+﻿IF COLUMNPROPERTY(OBJECT_ID('PackagingUnitsProperty','U'),'ValueTime','ColumnId') IS NULL
+ALTER TABLE [dbo].[PackagingUnitsProperty]
+add  	[ValueTime] datetime NULL
+
+
+IF COLUMNPROPERTY(OBJECT_ID('JobResponse','U'),'PackagingUnitsID','ColumnId') IS NULL
+BEGIN
+  ALTER TABLE [dbo].[JobResponse]
+  ADD [PackagingUnitsID] [int] NULL
+
+  ALTER TABLE [dbo].[JobResponse]  WITH CHECK ADD  CONSTRAINT [FK_JobResponse_PackagingUnits] FOREIGN KEY([PackagingUnitsID])
+  REFERENCES [dbo].[PackagingUnits] ([ID])
+
+  ALTER TABLE [dbo].[JobResponse] CHECK CONSTRAINT [FK_JobResponse_PackagingUnits]
+END
+
