4e2104d15181a1e6212cb2e5723637abf8241b5b
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-05 13:56:45 +0300

    Create tables PackagingUnitsDocs, WeightingOperations.

---
+++ service_packs/DDL/2017_04_05_4_Create_table_PackagingUnitsDocs.sql
@@ -0,0 +1,53 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_PackagingUnitsDocs')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_PackagingUnitsDocs] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+END
+GO
+
+IF OBJECT_ID ('dbo.PackagingUnitsDocs',N'U') IS NOT NULL
+  DROP TABLE [dbo].[PackagingUnitsDocs];
+GO
+
+/*
+   Table: PackagingUnitsDocs
+    Вагоны, содержащиеся в документах
+*/
+
+CREATE TABLE [dbo].[PackagingUnitsDocs](
+	[ID] [int] NOT NULL,
+	[Description] [nvarchar](50) NULL,
+	[DocumentationsID] [int] NULL,
+	[PackagingUnitsID] [int] NULL,
+	[Status] [nvarchar](50) NULL,
+	[StartTime] [datetimeoffset](7) NULL,
+ CONSTRAINT [PK_PackagingUnitsDocs] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[PackagingUnitsDocs] ADD  CONSTRAINT [DEF_ID_PackagingUnitsDocs]  DEFAULT (NEXT VALUE FOR [dbo].[gen_PackagingUnitsDocs]) FOR [ID]
+GO
+
+ALTER TABLE [dbo].[PackagingUnitsDocs]  WITH CHECK ADD  CONSTRAINT [FK_PackagingUnitsDocs_Documentations] FOREIGN KEY([DocumentationsID])
+REFERENCES [dbo].[Documentations] ([ID])
+GO
+
+ALTER TABLE [dbo].[PackagingUnitsDocs] CHECK CONSTRAINT [FK_PackagingUnitsDocs_Documentations]
+GO
+
+ALTER TABLE [dbo].[PackagingUnitsDocs]  WITH CHECK ADD  CONSTRAINT [FK_PackagingUnitsDocs_PackagingUnits] FOREIGN KEY(PackagingUnitsID)
+REFERENCES [dbo].[PackagingUnits] ([ID])
+GO
+
+ALTER TABLE [dbo].[PackagingUnitsDocs] CHECK CONSTRAINT [FK_PackagingUnitsDocs_PackagingUnits]
+GO
+
