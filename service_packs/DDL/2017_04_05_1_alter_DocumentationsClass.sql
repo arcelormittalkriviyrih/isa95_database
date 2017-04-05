b7a6eb5ddafc6f679111ed2f79443a0693a9fbd7
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-05 10:47:21 +0300

    Alter tables Documentations, DocumentationsProperty, DocumentationsClass

---
+++ service_packs/DDL/2017_04_05_1_alter_DocumentationsClass.sql
@@ -0,0 +1,18 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_DocumentationsClass')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_DocumentationsClass] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+ 
+ 
+ ALTER TABLE [dbo].[DocumentationsClass] 
+ ADD CONSTRAINT DEF_ID_DocumentationsClass  DEFAULT (NEXT VALUE FOR [dbo].[gen_DocumentationsClass]) FOR [ID]
+
+END
+
+
+
---
+++ service_packs/DDL/2017_04_05_2_alter_Documentations.sql
@@ -0,0 +1,47 @@
+﻿
+IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_Documentations')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_Documentations] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+ 
+ 
+ ALTER TABLE [dbo].[Documentations] 
+ ADD CONSTRAINT DEF_ID_Documentations DEFAULT (NEXT VALUE FOR [dbo].[gen_Documentations]) FOR [ID]
+END
+
+
+IF COLUMNPROPERTY(OBJECT_ID('Documentations','U'),'DocumentationsClassID','ColumnId') IS NULL
+ALTER TABLE [dbo].Documentations
+ADD  DocumentationsClassID int NULL
+
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_DocumentationsID_DocumentationsClass]') AND parent_object_id = OBJECT_ID(N'[dbo].[Documentations]'))
+ALTER TABLE [dbo].[Documentations]  WITH CHECK ADD  CONSTRAINT [FK_DocumentationsID_DocumentationsClass] FOREIGN KEY([DocumentationsClassID])
+REFERENCES [dbo].[DocumentationsClass] ([ID])
+
+IF COLUMNPROPERTY(OBJECT_ID('Documentations','U'),'StartTime','ColumnId') IS NULL
+ALTER TABLE [dbo].Documentations
+ADD  [StartTime] [datetimeoffset](7) NULL
+
+
+IF COLUMNPROPERTY(OBJECT_ID('Documentations','U'),'EndTime','ColumnId') IS NULL
+ALTER TABLE [dbo].Documentations
+ADD  [EndTime] [datetimeoffset](7) NULL
+
+
+IF COLUMNPROPERTY(OBJECT_ID('Documentations','U'),'DocumentationsClassID','ColumnId') IS NOT NULL
+BEGIN
+   ALTER TABLE [dbo].Documentations
+   DROP CONSTRAINT [FK_Documentations_DocumentationsDefinition]
+
+   ALTER TABLE [dbo].Documentations
+   DROP COLUMN  DocumentationsDefinitionID 
+END
+
+
+
