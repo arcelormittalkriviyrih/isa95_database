82d92a4b0f839e81aac0255f79e2471308e25856
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-05 10:47:21 +0300

    Alter tables Documentations, DocumentationsProperty, DocumentationsClass

---
+++ service_packs/DDL/2017_04_05_3_alter_DocumentationsProperty.sql
@@ -0,0 +1,32 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_DocumentationsProperty')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_DocumentationsProperty] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+ 
+ 
+ ALTER TABLE [dbo].[DocumentationsProperty] 
+ ADD CONSTRAINT DEF_ID_DocumentationsProperty DEFAULT (NEXT VALUE FOR [dbo].[gen_DocumentationsProperty]) FOR [ID]
+END
+
+
+
+
+IF COLUMNPROPERTY(OBJECT_ID('DocumentationsProperty','U'),'DocumentationsDefinitionPropertyID','ColumnId') IS NOT NULL
+BEGIN
+   ALTER TABLE [dbo].DocumentationsProperty
+   DROP CONSTRAINT [FK_DocumentationsProperty_DocumentationsDefinitionProperty]
+
+   ALTER TABLE [dbo].DocumentationsProperty
+   DROP COLUMN  [DocumentationsDefinitionPropertyID] 
+END
+
+
+
+IF COLUMNPROPERTY(OBJECT_ID('DocumentationsProperty','U'),'ValueTime','ColumnId') IS NULL
+ALTER TABLE [dbo].DocumentationsProperty
+add  [ValueTime] [datetimeoffset](7) NULL
