dda346028794961b2a0614a125af434c91f82d34
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-05 16:12:42 +0300

    insert into table DocumentationsClass

---
+++ service_packs/DML/2017_04_05_7_ins_DocumentationsClass.sql
@@ -0,0 +1,26 @@
+﻿
+insert into [dbo].[DocumentationsClass]
+	([Description], [ParentID])
+values
+	(N'Путевая', null),
+	(N'Отвесная', null)
+
+
+insert into [dbo].[DocumentationsClass]
+	([Description], [ParentID])
+select 
+	 T1.[Description]
+	,T2.ID
+from(
+	select N'Тарирование'	[Description]
+	union all
+	select N'Загрузка'		[Description]
+	union all
+	select N'Отгрузка'		[Description]
+	union all
+	select N'Контроль брутто' [Description]) as T1
+join(
+	select [ID]
+	from [dbo].[DocumentationsClass]
+	where [Description] = N'Отвесная') as T2
+on 1=1
