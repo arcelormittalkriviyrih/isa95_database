0f048c4267a97177ca842995dfdd5783c30fbd33
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-06 14:34:19 +0300

    alter table DocumentationsProperty

---
+++ service_packs/DDL/2017_04_06_4_alter_DocumentationsProperty.sql
@@ -0,0 +1,7 @@
+﻿
+IF COLUMNPROPERTY(OBJECT_ID('DocumentationsProperty','U'),'DocumentationsProperty','ColumnId') IS NOT NULL
+BEGIN
+	ALTER TABLE [dbo].[DocumentationsProperty]
+	ALTER COLUMN [DocumentationsProperty] int NULL
+END
+
