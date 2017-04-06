c82ba1fd9a91a1f4cb6ebc1d4191a716bd381ce8
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-06 09:11:22 +0300

    alter table DocumentationsClassProperty, insert into DocumentationsClassProperty;

---
+++ service_packs/DDL/2017_04_06_1_alter_DocumentationsClassProperty.sql
@@ -0,0 +1,23 @@
+﻿
+IF COLUMNPROPERTY(OBJECT_ID('DocumentationsClassProperty','U'),'DocumentationsClassProperty','ColumnId') IS NOT NULL
+BEGIN
+	ALTER TABLE [dbo].[DocumentationsClassProperty]
+	ALTER COLUMN [DocumentationsClassProperty] int NULL
+END
+
+
+IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_DocumentationsClassProperty')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_DocumentationsClassProperty] 
+ AS [int]
+ START WITH 1
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+
+ 
+ ALTER TABLE [dbo].[DocumentationsClassProperty] 
+ ADD CONSTRAINT DEF_ID_DocumentationsClassProperty  DEFAULT (NEXT VALUE FOR [dbo].[gen_DocumentationsClassProperty]) FOR [ID]
+
+END
