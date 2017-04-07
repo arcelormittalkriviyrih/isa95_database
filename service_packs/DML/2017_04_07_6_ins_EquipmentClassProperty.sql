8ec5a4c99dcab7576f9f230011279adc8ff4d9f5
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-07 16:15:11 +0300

    inser into [EquipmentClassProperty], [EquipmentProperty]

---
+++ service_packs/DML/2017_04_07_6_ins_EquipmentClassProperty.sql
@@ -0,0 +1,14 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+/*add new class properties*/
+insert into [dbo].[EquipmentClassProperty]
+([Description],[Value],[EquipmentClassID])
+values
+(N'ЖД станция',N'RAILWAY STATION',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха'))
+GO
---
+++ service_packs/DML/2017_04_07_7_ins_EquipmentProperty.sql
@@ -0,0 +1,26 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+/*add properties for some Process Cell*/
+insert into [dbo].[EquipmentProperty]
+([Description],[Value],[EquipmentID],[ClassPropertyID])
+select
+	 ECP.[Description]
+	,'true' as [Value]
+	,E.ID
+	,ECP.[ID]
+from [dbo].[EquipmentClassProperty] ECP
+join [dbo].[Equipment] as E
+on 1=1
+join [dbo].[Equipment] as EE
+on E.[Equipment] = EE.ID
+where ECP.[Description] in (N'ЖД станция')
+and  E.EquipmentLevel = 'Process Cell'
+and EE.Description = N'ЖДЦ'
+
+GO
