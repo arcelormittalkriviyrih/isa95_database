d5e2b0c79a87c3f358be774530a5a9016c98b2c9
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-07 16:21:35 +0300

    create view v_WGT_RailwayStations

---
+++ service_packs/DDL/2017_04_07_7_create_view_WGT_RailwayStations.sql
@@ -0,0 +1,26 @@
+﻿
+
+IF OBJECT_ID ('dbo.v_WGT_RailwayStations',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_RailwayStations];
+GO
+
+/*
+   View: v_WGT_RailwayStations
+    ЖД станции
+*/
+create view dbo.v_WGT_RailwayStations
+as
+SELECT
+	 E.[ID]
+	,E.[Description]
+	,E.[EquipmentLevel]
+	,ECP.[Value]		as PropertyDescription
+	,EP.[Value]			as PropertyValue
+FROM [dbo].[Equipment] E
+left join [dbo].[EquipmentProperty] EP
+on EP.[EquipmentID] = E.ID
+inner join [dbo].[EquipmentClassProperty] ECP
+on EP.ClassPropertyID = ECP.ID
+where ECP.[Value] in (N'RAILWAY STATION') and EP.Value = 'true'
+
+GO
