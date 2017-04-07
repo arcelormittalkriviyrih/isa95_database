f7bd102a9ffc0d0ecf7892c269f4b82950f3dc34
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-07 10:59:05 +0300

    insert into Equipment, EquipmentClassProperty, EquipmentProperty

--- service_packs/DML/2017_04_06_4_ins_KPPRoles.sql
+++ service_packs/DML/2017_04_06_4_ins_KPPRoles.sql
@@ -1,4 +1,4 @@
 ﻿/* add new role */
 
 insert into [KPPRoles] ([RoleName], [ADRoleName])
-values (N'Consigner', N'EUROPE\KRR-LG-PA-Developers_DB')
+values (N'Consigners', N'EUROPE\KRR-LG-PA-Developers_DB')
---
+++ service_packs/DML/2017_04_07_3_ins_EquipmentClassProperty.sql
@@ -0,0 +1,6 @@
+﻿/*add new class properties*/
+insert into [dbo].[EquipmentClassProperty]
+([Description],[Value],[EquipmentClassID])
+values
+(N'Грузоотправитель',N'CONSIGNER',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха')),
+(N'Грузополучатель' ,N'CONSIGNEE',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха'))
