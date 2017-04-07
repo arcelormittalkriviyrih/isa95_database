e4a64455d44cb9b6658e70f4a6189c2cfb64fcdd
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-07 14:12:43 +0300

    create view v_WGT_Consigners

---
+++ service_packs/DDL/2017_04_07_5_create_view_WGT_Consigners.sql
@@ -0,0 +1,31 @@
+﻿
+
+IF OBJECT_ID ('dbo.v_WGT_Consigners',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_Consigners];
+GO
+
+/*
+   View: v_WGT_Consigners
+    Отправители и получатели грузов
+*/
+create view dbo.v_WGT_Consigners
+as
+SELECT
+	 E.[ID]
+	,E.[Description]
+	,E.[EquipmentLevel]
+	,EE.[ID]			as ParentID
+	,EE.[Description]	as ParentDescription
+	,ECP.[Value]		as PropertyDescription
+	,EP.[Value]			as PropertyValue
+FROM [dbo].[Equipment] E
+left join [dbo].[Equipment] EE
+on EE.ID = E.Equipment
+left join [dbo].[EquipmentProperty] EP
+on EP.[EquipmentID] = EE.ID
+inner join [dbo].[EquipmentClassProperty] ECP
+on EP.ClassPropertyID = ECP.ID
+where ECP.[Value] in (N'CONSIGNEE', N'CONSIGNER') and EP.Value = 'true'
+--order by EE.[Description], E.[Description]
+
+GO
