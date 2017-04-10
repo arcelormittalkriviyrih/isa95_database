864ef364faf9ed81a23090f2cca040e916fdacb7
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-10 13:53:38 +0300

    add sequence for table PackagingClass

---
+++ service_packs/DDL/2017_04_10_3_alter_PackagingClass.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_PackagingClass')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_PackagingClass] 
+ AS [int]
+ START WITH 2
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+ 
+ 
+ ALTER TABLE [dbo].PackagingClass 
+ ADD CONSTRAINT DEF_ID_PackagingClass  DEFAULT (NEXT VALUE FOR [dbo].[gen_PackagingClass]) FOR [ID]
+
+END
