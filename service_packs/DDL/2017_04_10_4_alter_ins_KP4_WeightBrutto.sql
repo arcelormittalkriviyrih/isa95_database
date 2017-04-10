a64e2c4d7e5e4aa69b9aa3d4592b196edf8af317
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-10 15:01:33 +0300

    Modify tables,views, procedures for delete PackagingDefinition

---
+++ service_packs/DDL/2017_04_10_4_alter_ins_KP4_WeightBrutto.sql
@@ -0,0 +1,136 @@
+﻿
+
+-------TRANSACTION insKP4_TakeWeight
+ALTER PROCEDURE [dbo].[ins_KP4_WeightBrutto]
+   @WorkPerformanceID int,					-- ID WeightSheet 
+   @WagonNumber   nvarchar(50),             -- Number wagon
+   @WorkResponseDescription  nvarchar(50),  -- Number waybill
+   @Value    real,					        -- Weight 
+   @WeightMode nvarchar(20),                -- Mode operation brutto\tara
+   @MaterialDefinitionID int,                -- Code scrap (CSH)
+   @ReceiverID int,            
+   @SenderID int, 
+   @WeightBridgeID    int                    --  ID Equipment
+    
+AS
+BEGIN
+ DECLARE @ID_JobResponse int,
+         @ID_WorkResponse int, @fl_Dubl bit, @WeightFirst real,
+		 @ID_JobOrder int,
+		 @ID_OpMaterialActual int,
+		 @DT datetime
+
+     SET @DT=getdate()
+
+     --------- Dubl weighting wagon
+	 if exists(SELECT * FROM [dbo].[WorkResponse] WHERE [Description]=@WorkResponseDescription AND  [WorkType]=@WeightMode AND [WorkPerfomence]=@WorkPerformanceID)
+	 BEGIN
+
+	     SELECT @ID_WorkResponse=ID  
+		 FROM   [dbo].[WorkResponse] 
+		 WHERE  [Description]=@WorkResponseDescription AND  [WorkType]=@WeightMode AND [WorkPerfomence]=@WorkPerformanceID
+
+		 SET @WeightFirst =	Isnull(  (SELECT Cast(map.Value as Float)
+		                              FROM  (SELECT max(ID) as jrID FROM [dbo].[JobResponse] WHERE WorkResponse=@ID_WorkResponse ) jr
+                                        left join [OpMaterialActual] oma on oma.JobResponseID=jr.jrID AND  oma.MaterialClassID=9
+                                        left join (SELECT * FROM [MaterialActualProperty] WHERE [Description]=N'Вес брутто') map 
+										on map.OpMaterialActual=oma.ID 
+                                       ), 0)
+		 SET @fl_Dubl=1;
+	 END
+
+	 ELSE   --------- First weighting wagon
+	 BEGIN
+         SET @ID_WorkResponse=NEXT VALUE FOR [dbo].[gen_WorkResponse];
+
+		 SET @WeightFirst =	Isnull((SELECT Cast(pap.Value as Float) 
+		                            FROM   dbo.PackagingUnitsProperty pap right join dbo.PackagingUnits pu
+									       on pu.ID=pap.PackagingUnitsID and pap.Description=N'Вес тары' ---   PackagingDefinitionPropertyID=2
+	                                WHERE  pu.[Description]=@WagonNumber),   50);
+		 SET @fl_Dubl=0;
+
+	 END
+
+     SET @ID_JobOrder=NEXT     VALUE FOR [dbo].[gen_JobOrder];
+     INSERT INTO [dbo].[JobOrder] ([ID], [WorkType], [StartTime], [DispatchStatus], [Command])
+     VALUES (@ID_JobOrder, @WeightMode, @DT,'Done', 'ins_KP4_WeightBrutto');
+
+     ----  New waybill for wagon
+	 IF @fl_Dubl=0
+         INSERT INTO [dbo].[WorkResponse]     ([ID], [Description],  [WorkType], [StartTime], [EndTime], [WorkPerfomence] ) 
+         VALUES    (@ID_WorkResponse, @WorkResponseDescription,  @WeightMode, @DT, @DT, @WorkPerformanceID )
+
+     ---- New operation weighting
+     SET @ID_JobResponse=NEXT  VALUE FOR [dbo].[gen_JobResponse];
+     INSERT INTO [dbo].[JobResponse]      ([ID], [Description], [WorkType], [JobOrderID],  [StartTime], [EndTime], [WorkResponse] )
+     VALUES 	   (@ID_JobResponse, N'Operation weighting', @WeightMode, @ID_JobOrder, @DT, @DT, @ID_WorkResponse )
+
+     ---- Code material scrap (CSH)
+     INSERT INTO [dbo].[OpMaterialActual]                         --  ID autoincrement
+        ([MaterialDefinitionID] ,[MaterialLotID]  ,[Description]  ,[JobResponseID]    ,[MaterialClassID]) 
+     SELECT top 1  @MaterialDefinitionID, isnull(lo.ID,3236),  cl.[Description],  @ID_JobResponse, cl.ID 
+	 FROM  [dbo].[MaterialClass] cl left join 
+	       [dbo].[MaterialLot]   lo  on lo.FactoryNumber = cast(@WorkPerformanceID as nvarchar) 
+	 WHERE cl.ID=9 
+
+     SELECT @ID_OpMaterialActual=@@IDENTITY
+
+     INSERT INTO [dbo].[MaterialActualProperty]   ([Description],[OpMaterialActual],[Value])
+	 SELECT mcp.[Description], oma.ID,
+	        (CASE WHEN mcp.Value=N'Scrap type'   THEN   @MaterialDefinitionID
+			      WHEN mcp.Value=N'Netto debit'  THEN   @Value-@WeightFirst
+			      WHEN mcp.Value=N'Netto credit' THEN   ( @Value-@WeightFirst) * (-1)
+                  WHEN mcp.Value=N'Brutto'   THEN   @Value
+                  WHEN mcp.Value=N'Sender'   THEN   @SenderID
+			      WHEN mcp.Value=N'Receiver' THEN   @ReceiverID
+				  ELSE 'error' END) value
+	 FROM MaterialClassProperty mcp,
+	      OpMaterialActual oma   
+	 WHERE mcp.MaterialClassID=9  and   oma.ID=@ID_OpMaterialActual
+
+     -- Mode new-operation for wagon  
+     INSERT INTO [dbo].[OpPackagingActual]     ([Description]       ,[PackagingUnitsID]          ,[JobResponseID])
+	 SELECT top 1 
+			@WeightMode, 
+			ID, 
+			@ID_JobResponse
+	 FROM   [dbo].[PackagingUnits]
+	 WHERE  [Description]=@WagonNumber
+
+
+
+    ----------------  Equipment
+    INSERT INTO [dbo].[OpEquipmentActual]      ([EquipmentID], [Description], [JobResponseID], [EquipmentClassID], Quantity)
+    SELECT ID,[Description], @ID_JobResponse, [EquipmentClassID], 0
+    FROM Equipment 	WHERE ID=@WeightBridgeID
+
+    ----- Sender AND REciever
+    --INSERT INTO [dbo].[OpEquipmentActual]      ([EquipmentID], [Description], [JobResponseID], [EquipmentClassID], Quantity)
+    --SELECT ID,[Description], @ID_JobResponse, [EquipmentClassID], 0
+    --FROM Equipment 	WHERE ID=@ReceiverID
+
+    --INSERT INTO [dbo].[OpEquipmentActual]      ([EquipmentID], [Description], [JobResponseID], [EquipmentClassID], Quantity)
+    --SELECT ID,[Description], @ID_JobResponse, [EquipmentClassID], 0
+    --FROM Equipment 	WHERE ID=@SenderID
+
+	
+	/*== test OpPersonnelActual ==*/
+	
+	insert into [dbo].[OpPersonnelActual] (
+		 [PersonID]
+		,[Description]
+		,[Quantity]
+		,[JobResponseID]
+		,[PersonnelClassID])
+	select top 1 
+		p.ID,
+		p.PersonName,
+		1,
+		@ID_JobResponse		[JobResponseID],
+		p.PersonnelClassID
+	from [dbo].[Person] p
+	where ID = [dbo].[get_CurrentPerson]()
+
+
+END
+
---
+++ service_packs/DDL/2017_04_10_6_alter_v_KP4_Wagon.sql
@@ -0,0 +1,58 @@
+﻿
+ALTER VIEW [dbo].[v_KP4_Wagon]
+AS
+SELECT     TOP (100) PERCENT 
+	dbo.JobResponse.ID, 
+	dbo.WorkPerformance.ID				AS WorkPerformanceID, 
+	dbo.WorkPerformance.Description		AS WeightSheetNumber, 
+	dbo.WorkResponse.ID					AS WorkResponseID, 
+	dbo.PackagingUnits.ID				AS PackagingUnitsID, 
+	dbo.PackagingUnits.Description		AS WagonNumber, 
+	dbo.WorkResponse.Description		AS WaybillNumber, 
+	dbo.JobResponse.WorkType, 
+	dbo.MaterialDefinition.ID			AS MaterialDefinitionID, 
+	dbo.MaterialDefinition.Description	AS Group_Lom, 
+	om.ID				AS OpMaterialActualID, 
+	---om.MaterialDefinitionID AS CSH, 
+	map.CSH AS CSH, 
+	(case
+		when dbo.JobResponse.WorkType = 'Taring'
+		then dbo.OpPackagingActualProperty.Value
+		when dbo.JobResponse.WorkType = 'Weighting'
+		then (SELECT Value FROM   dbo.PackagingUnitsProperty WHERE	 PackagingUnitsID = dbo.PackagingUnits.ID and [Description]=N'Вес тары' )
+	end)									AS Tare, 
+	map.brutto		AS Brutto, 
+	map.netto       AS Netto, 
+	----dbo.OpPackagingActual.ID			AS OpPackagingActualID,
+	dbo.JobResponse.StartTime			AS WeightingTime,
+	cast(ROW_NUMBER() over (partition by dbo.WorkPerformance.ID, dbo.WorkResponse.ID order by dbo.JobResponse.ID) as int)	AS WeightingIndex,
+	cast(DENSE_RANK() over (partition by dbo.WorkPerformance.ID order by dbo.WorkResponse.ID) as int)						AS WagonIndex,
+	--map.sndr as sender,
+	--map.rcvr as reciever,
+	(select top 1 [Description] from dbo.Equipment where ID = map.sndr) [Sender],
+	(select top 1 [Description] from dbo.Equipment where ID = map.rcvr) [Receiver],
+	opea.Description					as [Person]
+
+FROM
+	dbo.JobResponse INNER JOIN
+	dbo.WorkResponse ON dbo.JobResponse.WorkResponse = dbo.WorkResponse.ID INNER JOIN
+	dbo.WorkPerformance ON dbo.WorkResponse.WorkPerfomence = dbo.WorkPerformance.ID LEFT OUTER JOIN
+	dbo.OpPackagingActual ON dbo.JobResponse.ID = dbo.OpPackagingActual.JobResponseID LEFT OUTER JOIN
+	dbo.OpPackagingActualProperty ON dbo.OpPackagingActual.ID = dbo.OpPackagingActualProperty.OpPackagingActualID LEFT OUTER JOIN
+	dbo.PackagingUnits ON dbo.OpPackagingActual.PackagingUnitsID = dbo.PackagingUnits.ID LEFT OUTER JOIN
+	dbo.OpMaterialActual om ON dbo.JobResponse.ID = om.JobResponseID  AND om.MaterialClassID=9 LEFT OUTER JOIN
+	--dbo.OpEquipmentActual oe1 ON dbo.JobResponse.ID = oe1.JobResponseID  AND oe1.EquipmentClassID=15 LEFT OUTER JOIN
+	--dbo.OpEquipmentActual oe2 ON dbo.JobResponse.ID = oe2.JobResponseID  AND oe2.EquipmentClassID=16 LEFT OUTER JOIN
+	--dbo.MaterialActualProperty  brutto ON brutto.OpMaterialActual=om.ID  AND  brutto.[Description]=N'Вес брутто' LEFT OUTER JOIN
+	--dbo.MaterialActualProperty  netto  ON netto.OpMaterialActual=om.ID AND  netto.[Description]=N'Вес нетто Дебет'  LEFT OUTER JOIN
+	--dbo.MaterialActualProperty  sender ON brutto.OpMaterialActual=om.ID  AND  brutto.[Description]=N'Вес брутто' LEFT OUTER JOIN
+	--dbo.MaterialActualProperty  reciever  ON netto.OpMaterialActual=om.ID AND  netto.[Description]=N'Вес нетто Дебет' 
+	 dbo.MaterialDefinition ON dbo.MaterialDefinition.ID= om.MaterialDefinitionID  LEFT OUTER JOIN
+    (   SELECT   OpMaterialActual, CAST(MAX([Вид лома]) as int) CSH, 
+               CAST(MAX([Вес брутто]) as real)  brutto,  CAST(MAX([Вес нетто Дебет]) as real) netto,
+		       CAST(MAX([Получатель]) as int) rcvr,  CAST(MAX([Отправитель]) as int)  sndr
+        FROM  dbo.MaterialActualProperty 
+        PIVOT (MAX(Value) FOR  [Description] in  ([Вид лома], [Вес брутто], [Вес нетто Дебет], [Получатель], [Отправитель]) ) AS pvt   
+        GROUP BY OpMaterialActual ) map  
+	 ON map.OpMaterialActual=om.ID  LEFT OUTER JOIN
+	 [dbo].[OpPersonnelActual] opea ON dbo.JobResponse.ID = opea.JobResponseID 
