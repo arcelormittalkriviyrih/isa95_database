710e47b6f6dc74dbd60052eec22b1fd41347ce1b
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-11 13:27:49 +0300

    insert into PackagingClassProperty

---
+++ service_packs/DML/2017_04_11_4_PackagingClassProperty.sql
@@ -0,0 +1,14 @@
+﻿
+INSERT INTO [dbo].[PackagingClassProperty]
+           ([Description],[Value], PackagingClassProperty, [PackagingClassID])
+SELECT  N'Wagon number template',
+      case 
+	  when [Description]=N'лафет-короб' then N'XXX-XXX'
+	  when [Description]=N'полувагон' then N'XXXXX'
+	  when [Description]=N'спецвагон' then N'XXXXXXXX'
+	  when [Description]=N'цистерна' then N'XXXXXXXX'
+	  end
+      , 1,[ID]
+  FROM [KRR-PA-ISA95_PRODUCTION].[dbo].[PackagingClass] where Description not like N'%ж\д вагоны%'
+
+
