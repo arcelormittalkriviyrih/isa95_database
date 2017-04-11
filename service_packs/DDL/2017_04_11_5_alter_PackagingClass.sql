1fe48173aaea00ec8f4df035b3778ab27cd4377b
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-11 14:28:28 +0300

    Update PackagingClass

---
+++ service_packs/DDL/2017_04_11_5_alter_PackagingClass.sql
@@ -0,0 +1,7 @@
+﻿
+IF COLUMNPROPERTY(OBJECT_ID('PackagingClass','U'),'ParentID','ColumnId') IS NOT NULL
+BEGIN
+   ALTER TABLE [dbo].[PackagingClass]
+	ALTER COLUMN [ParentID] [int]  NULL
+END
+
---
+++ service_packs/DML/2017_04_11_6_PackagingClass.sql
@@ -0,0 +1,2 @@
+﻿UPDATE  [dbo].[PackagingClass] SET  [ParentID]=NULL
+ WHERE [Description]  like N'%ж\д вагоны%'
