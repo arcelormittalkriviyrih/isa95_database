ecab32f39dc7cf7aacb6b80cd25de6c254b90b8a
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-11 13:11:11 +0300

    restore working

--- service_packs/DML/2017_04_11_3_PackagingClassProperty.sql
+++ service_packs/DML/2017_04_11_3_PackagingClassProperty.sql
@@ -1,7 +1,2 @@
-﻿INSERT INTO [dbo].[PackagingClassProperty]
-           ([ID] ,[Description],[Value],PackagingClassProperty,[PackagingClassID])
-     VALUES
-           (2,N'Wagon number template', N'XXX-XXX',1, 2),
-           (3,N'Wagon number template', N'XXXXX',1, 3),
-           (4,N'Wagon number template', N'XXXXXXXX',1, 4),
-           (5,N'Wagon number template', N'XXXXXXXX',1, 5)
+﻿UPDATE [dbo].[PackagingClassProperty]
+           SET  PackagingClassProperty=1
