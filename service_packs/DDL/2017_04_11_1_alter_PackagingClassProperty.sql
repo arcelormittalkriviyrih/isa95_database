c205415810f57f4f4166f2b66b8c434023021518
evpavlovskaya <elena.pavlovskaya@arcelormittal.com>
2017-04-11 12:21:38 +0300

    alter tables PackagingUnitsProperty and PackagingClass

---
+++ service_packs/DDL/2017_04_11_1_alter_PackagingClassProperty.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.sequences WHERE name = N'gen_PackagingClassProperty')
+BEGIN
+CREATE SEQUENCE [dbo].[gen_PackagingClassProperty] 
+ AS [int]
+ START WITH 2
+ INCREMENT BY 1
+ MINVALUE -2147483648
+ MAXVALUE 2147483647
+ NO CACHE 
+ 
+ 
+ ALTER TABLE [dbo].PackagingClassProperty 
+ ADD CONSTRAINT DEF_ID_PackagingClassProperty  DEFAULT (NEXT VALUE FOR [dbo].[gen_PackagingClassProperty]) FOR [ID]
+
+END
---
+++ service_packs/DML/2017_04_11_1_UpdatePackagingUnitsProperty.sql
@@ -0,0 +1,10 @@
+﻿  Update [PackagingUnitsProperty]
+   SET [ValueTime]=pp.Value
+  FROM [PackagingUnitsProperty]  left join [PackagingUnitsProperty] pp
+  on [PackagingUnitsProperty].PackagingUnitsID=pp.PackagingUnitsID and   pp.Description=N'Время тарирования'
+  WHERE [PackagingUnitsProperty].Description=N'Вес тары';
+
+ Update [PackagingUnitsProperty] set [PackagingUnitsProperty]=0;
+ 
+ DELETE  FROM [dbo].[PackagingUnitsProperty] WHERE [Description]=N'Тара';
+ DELETE  FROM [dbo].[PackagingUnitsProperty] WHERE [Description]=N'Время тарирования';
