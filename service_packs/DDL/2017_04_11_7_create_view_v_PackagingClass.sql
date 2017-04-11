03a37cdecf1f65afbec911e955f4f355a5317eb7
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-11 15:38:10 +0300

    create_view_v_PackagingClass

---
+++ service_packs/DDL/2017_04_11_7_create_view_v_PackagingClass.sql
@@ -0,0 +1,23 @@
+﻿
+
+IF OBJECT_ID ('dbo.v_PackagingClass',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_PackagingClass];
+GO
+
+/*
+   View: v_PackagingClass
+   Виды вагонов
+*/
+create view [dbo].[v_PackagingClass]
+as
+select
+	 PC.[ID]
+	,PC.[Description]
+	,PC.[ParentID]
+	,PC1.[Description] as [ParentDescription]
+from [dbo].[PackagingClass] PC
+inner join [dbo].[PackagingClass] PC1
+on PC.ParentID = PC1.ID
+--order by PC.[ParentID], PC.[ID]
+
+GO
