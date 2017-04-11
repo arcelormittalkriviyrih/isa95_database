63d12a07f3f82c74933f161e6ebf02ff972d0983
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-11 15:25:12 +0300

    insert into Equipment

---
+++ service_packs/DDL/2017_04_10_1_alter_view_WGT_Consigners.sql
@@ -0,0 +1,27 @@
+﻿
+
+IF OBJECT_ID ('dbo.v_WGT_Consigners',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_Consigners];
+GO
+
+/*
+   View: v_WGT_Consigners
+    Отправители и получатели грузов
+*/
+alter view dbo.v_WGT_Consigners
+as
+select
+	 E.ID
+	,E.[Description]
+	,E.[EquipmentLevel]
+	,ECP.[Value]		as PropertyDescription
+	,EP.[Value]			as PropertyValue
+from [dbo].[Equipment] E
+left join [dbo].[EquipmentProperty] EP
+on EP.[EquipmentID] = E.ID
+inner join [dbo].[EquipmentClassProperty] ECP
+on EP.ClassPropertyID = ECP.ID
+where ECP.[Value] in (N'CONSIGNEE', N'CONSIGNER') and EP.Value = 'true'
+--order by E.[Description]
+
+GO
--- service_packs/DML/2017_04_11_4_PackagingClassProperty.sql
+++ service_packs/DML/2017_04_11_4_PackagingClassProperty.sql
@@ -3,12 +3,12 @@ INSERT INTO [dbo].[PackagingClassProperty]
            ([Description],[Value], PackagingClassProperty, [PackagingClassID])
 SELECT  N'Wagon number template',
       case 
-	  when [Description]=N'лафет-короб' then N'XXX-XXX'
-	  when [Description]=N'полувагон' then N'XXXXX'
-	  when [Description]=N'спецвагон' then N'XXXXXXXX'
-	  when [Description]=N'цистерна' then N'XXXXXXXX'
+	  when [Description]=N'Лафет-короб' then N'XXX-XXX'
+	  when [Description]=N'Полувагон' then N'XXXXX'
+	  when [Description]=N'Спецвагон' then N'XXXXXXXX'
+	  when [Description]=N'Цистерна' then N'XXXXXXXX'
 	  end
       , 1,[ID]
-  FROM [KRR-PA-ISA95_PRODUCTION].[dbo].[PackagingClass] where Description not like N'%ж\д вагоны%'
+  FROM [KRR-PA-ISA95_PRODUCTION].[dbo].[PackagingClass] where Description not like N'%ЖД вагоны%'
 
 
