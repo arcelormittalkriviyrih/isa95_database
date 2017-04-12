d59f12677391ab3d292e01a55b658bf0fa985d6f
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-12 15:12:49 +0300

    fix and add properties for wagons in table [PackagingClassProperty]

---
+++ service_packs/DDL/2017_04_12_1_alter_PackagingClassProperty.sql
@@ -0,0 +1,8 @@
+﻿/*set [PackagingClassProperty] column to NULLABLE*/
+IF COLUMNPROPERTY(OBJECT_ID('PackagingClassProperty','U'),'PackagingClassProperty','ColumnId') IS NOT NULL
+BEGIN
+   ALTER TABLE [dbo].[PackagingClassProperty]
+	ALTER COLUMN [PackagingClassProperty] [int]  NULL
+END
+
+GO
