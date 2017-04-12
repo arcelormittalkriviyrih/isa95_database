138f78025a76817d8ccd741c1bba9c8af0d10a21
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-04-12 15:12:49 +0300

    fix and add properties for wagons in table [PackagingClassProperty]

---
+++ service_packs/DML/2017_04_12_2_upd_PackagingClassProperty.sql
@@ -0,0 +1,26 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+/*reset PackagingClassProperty*/
+update [dbo].[PackagingClassProperty]
+set [PackagingClassProperty] = null
+
+/*fix Wagon number templates*/
+update [dbo].[PackagingClassProperty]
+set Value =	case PC.Description
+						when N'Лафет-короб'		then N'^[0-9]{1,3}-[0-9]{1,3}$'
+						when N'Спецвагон'		then N'^[0-9]{3,6}$'
+						when N'Полувагон'		then N'^[0-9]{8}$'
+						when N'Цистерна'		then N'^[0-9]{8}$'
+					end
+from [dbo].[PackagingClassProperty] PCP
+join [dbo].[PackagingClass] PC
+on PCP.[PackagingClassID] = PC.[ID]
+where PCP.[Description] = N'Wagon number template'
+
+GO
---
+++ service_packs/DML/2017_04_12_3_ins_PackagingClassProperty.sql
@@ -0,0 +1,31 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+/*add new properties for wagons*/
+insert into [dbo].[PackagingClassProperty]
+(Description, [Value], [ValueUnitofMeasure], [PackagingClassID])
+select	
+	 T1.[Description]
+	,T2.[Value]
+	,'ton' as [ValueUnitofMeasure]
+	,T2.[ID]
+from
+(select N'Грузоподъемность' [Description]) as T1
+join
+(
+SELECT [ID]
+      ,[Description]
+      ,[ParentID]
+	  ,50 as [Value]
+FROM [KRR-PA-ISA95_PRODUCTION].[dbo].[PackagingClass]
+) as T2
+on 1=1
+where T2.Description in (N'Лафет-короб', N'Полувагон', N'Спецвагон', N'Цистерна')
+
+
+GO
