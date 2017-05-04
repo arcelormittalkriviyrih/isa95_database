5ed42de5276462069f0d12a65192a2586aeea383
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-04 11:24:53 +0300

    drop column in PackagingUnits table

---
+++ service_packs/DDL/2017_05_04_3_alter_PackagingUnits.sql
@@ -0,0 +1,5 @@
+﻿   IF COLUMNPROPERTY(OBJECT_ID('PackagingUnits','U'),'PackagingDefinitionID','ColumnId') IS NOT NULL  
+   BEGIN  
+  	  ALTER TABLE dbo.PackagingUnits  
+  	  DROP COLUMN PackagingDefinitionID
+   END
