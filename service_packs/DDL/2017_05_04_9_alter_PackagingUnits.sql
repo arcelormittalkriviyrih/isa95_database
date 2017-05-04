8382234435f87777a8bf7c7c6b0fdca268c706a1
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-04 16:07:54 +0300

    drop constraints on PackagingUnits table; set column Location in PackagingUnits table to nullable

---
+++ service_packs/DDL/2017_05_04_9_alter_PackagingUnits.sql
@@ -0,0 +1,17 @@
+﻿
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_PackagingUnits_PackagingClass]') AND parent_object_id = OBJECT_ID(N'[dbo].[PackagingUnits]'))
+ALTER TABLE [dbo].[PackagingUnits] 
+DROP CONSTRAINT [FK_PackagingUnits_PackagingClass] 
+
+IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_PackagingUnits_Location]') AND parent_object_id = OBJECT_ID(N'[dbo].[PackagingUnits]'))
+ALTER TABLE [dbo].[PackagingUnits] 
+DROP CONSTRAINT [FK_PackagingUnits_Location] 
+
+IF COLUMNPROPERTY(OBJECT_ID('PackagingUnits','U'),'Location','ColumnId') IS NOT NULL
+BEGIN
+   ALTER TABLE [dbo].[PackagingUnits]
+	ALTER COLUMN [Location] [int] NULL
+END
+
+GO
+
