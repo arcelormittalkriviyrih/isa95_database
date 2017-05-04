85450a66cf32c6c0e00485a9788d020802950ad6
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-04 14:47:50 +0300

    update table PackagingClass; create procedure ins_PackagingUnits

---
+++ service_packs/DDL/2017_05_04_8_create_proc_ins_PackagingUnits.sql
@@ -0,0 +1,54 @@
+﻿
+IF OBJECT_ID ('dbo.ins_PackagingUnits',N'P') IS NOT NULL
+  DROP PROCEDURE dbo.ins_PackagingUnits;
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+
+CREATE PROCEDURE [dbo].[ins_PackagingUnits] 
+   @WagonNumber		nvarchar(20),		-- Wagon Number
+   @PackagingClass	int					-- Wagon Type
+
+AS
+BEGIN
+
+IF (isnull(@WagonNumber, '') = '')
+	THROW 60001, N'WagonNumber param required', 1;
+IF @PackagingClass IS NULL
+	THROW 60001, N'PackagingClass param required', 1;
+IF not exists (select ID from [PackagingUnitsClass] where ID = @PackagingClass)
+	THROW 60001, N'PackagingClassID doesnt exist', 1;
+
+
+declare @PackagingUnitsID int
+
+SELECT @PackagingUnitsID = [ID]
+FROM [PackagingUnits] 
+WHERE [Description] = @WagonNumber and [PackagingClassID] = @PackagingClass 
+
+if (@PackagingUnitsID is null)
+BEGIN
+
+	SELECT @PackagingUnitsID = NEXT VALUE FOR [dbo].[gen_PackagingUnits];
+
+	INSERT INTO [dbo].[PackagingUnits](
+		 [ID]
+		,[Description]
+		,[PackagingClassID]) 
+	VALUES(
+		 @PackagingUnitsID
+		,@WagonNumber
+		,@PackagingClass)
+
+END
+
+select @PackagingUnitsID 
+
+END
+
+
+GO
---
+++ service_packs/DML/2017_04_11_5_PackagingClass.sql
@@ -0,0 +1,6 @@
+﻿
+UPDATE  [dbo].[PackagingClass] 
+SET  [Description]=N'ЖД вагоны'
+WHERE [Description] like N'%ж\д вагоны%'
+
+GO
