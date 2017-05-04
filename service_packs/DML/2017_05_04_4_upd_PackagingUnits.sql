0cc14a7665848f833a535aa6ece60b2b2b3f3a2f
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-04 10:42:08 +0300

    set PackagingUnitsProperty column in table PackagingUnitsProperty to nullable; remove root elements in PackagingUnitsProperty and PackagingUnits tables; set PackagingClassID for PackagingUnits table.

---
+++ service_packs/DML/2017_05_04_4_upd_PackagingUnits.sql
@@ -0,0 +1,13 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+update [dbo].[PackagingUnits]
+set [PackagingClassID] = (select top 1 ID from [PackagingClass] where [Description] = N'Лафет-короб')
+where [Description] like '%-%'
+
+GO
