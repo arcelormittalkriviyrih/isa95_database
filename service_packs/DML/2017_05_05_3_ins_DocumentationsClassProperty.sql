da9498005f00aa87239b7177f99e3f8dcebafedf
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-05 13:26:47 +0300

    insert waybill's properties into table DocumentationsClassProperty

---
+++ service_packs/DML/2017_05_05_3_ins_DocumentationsClassProperty.sql
@@ -0,0 +1,24 @@
+﻿/* Properties of Waybills */
+insert into [dbo].[DocumentationsClassProperty]
+		( [Description],	[DocumentationsClassID])
+select T1.[Description], T2.[DocumentationsClassID]
+from(
+select N'Время прибытия (станция отправления)'				[Description]
+union all
+select N'Время подачи под погрузку (станция отправления)'	[Description]
+union all
+select N'Время окончания выгрузки (станция отправления)'	[Description]
+union all
+select N'Время прибытия (станция получения)'				[Description]
+union all
+select N'Время подачи под погрузку (станция получения)'		[Description]
+union all
+select N'Время окончания выгрузки (станция получения)'		[Description]
+) as T1
+join	
+(	select ID					[DocumentationsClassID]
+	from [dbo].[DocumentationsClass] 
+	where [Description] = N'Путевая') as T2
+on 1=1
+
+GO
