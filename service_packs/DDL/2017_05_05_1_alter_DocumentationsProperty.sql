3ba635c12f3caf7a081d66a971ce361fbe4e41c5
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-05 13:02:38 +0300

    add column DocumentationsClassPropertyID into table DocumentationsProperty

---
+++ service_packs/DDL/2017_05_05_1_alter_DocumentationsProperty.sql
@@ -0,0 +1,14 @@
+﻿
+IF COLUMNPROPERTY(OBJECT_ID('DocumentationsProperty','U'),'DocumentationsClassPropertyID','ColumnId') IS NULL
+BEGIN
+   ALTER TABLE [dbo].[DocumentationsProperty]
+	ADD [DocumentationsClassPropertyID] [int] NULL
+END
+
+IF NOT EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'[dbo].[FK_DocumentationsClassPropertyID_DocumentationsClassProperty]') AND parent_object_id = OBJECT_ID(N'[dbo].[DocumentationsProperty]'))
+ALTER TABLE [dbo].[DocumentationsProperty]  WITH CHECK 
+ADD CONSTRAINT [FK_DocumentationsClassPropertyID_DocumentationsClassProperty] FOREIGN KEY([DocumentationsClassPropertyID])
+REFERENCES [dbo].[DocumentationsClassProperty] ([ID])
+
+GO
+
