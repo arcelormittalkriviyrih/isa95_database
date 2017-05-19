755eb51cc2d9ec7bbc76c984455cb7306ca26849
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-19 12:15:50 +0300

    create view v_WGT_WaybillExistCheck

---
+++ service_packs/DDL/2017_05_19_1_create_view_v_WGT_WaybillExistCheck.sql
@@ -0,0 +1,30 @@
+﻿
+IF OBJECT_ID ('dbo.v_WGT_WaybillExistCheck',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_WaybillExistCheck];
+GO
+
+/*
+   View: v_WGT_WaybillExistCheck
+   Возвращает повторные путевые за последний год
+*/
+CREATE view [dbo].[v_WGT_WaybillExistCheck]
+as
+
+select --top 1
+	 [DocumentationsID] as [ID]
+	,[WaybillNumber]
+	,[SenderShop]
+	,D.[EndTime]
+from (
+	select 
+		 [DocumentationsID]
+		,[Description2]
+		,[Value]
+	from [dbo].[v_WGT_WaybillProperty] 
+) as WP
+pivot(max(Value) for [Description2] in ([WaybillNumber], [SenderShop])) as pvt
+inner join
+[dbo].[Documentations] D
+on pvt.[DocumentationsID] = D.ID and D.[EndTime] > dateadd(year, -1, getdate())
+
+GO
