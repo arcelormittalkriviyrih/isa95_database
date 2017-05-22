2f193de29c0c7cc08e832082a21f5910d2f249db
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-22 09:12:11 +0300

    create procedure [upd_Documentations]

---
+++ service_packs/DDL/2017_05_22_1_create_proc_upd_Documentations.sql
@@ -0,0 +1,28 @@
+﻿
+IF OBJECT_ID ('dbo.upd_Documentations',N'P') IS NOT NULL
+  DROP PROCEDURE dbo.upd_Documentations;
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+
+create PROCEDURE [dbo].[upd_Documentations]
+   @DocumentationsID int,
+   @Status nvarchar(20)
+AS
+BEGIN
+	IF @DocumentationsID IS NULL
+    THROW 60001, N'DocumentationsID param required', 1;
+
+	UPDATE [dbo].[Documentations]
+	SET  
+		[Status]	= @Status, 
+		[EndTime]	= getdate()
+	WHERE  ID = @DocumentationsID
+          
+END
+
+GO
