5147ab725ce9c6c96a95b97b1c7b4c25f17d65e2
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2017-05-26 12:13:19 +0300

    insert into EquipmentClassProperty; insert into EquipmentProperty;

---
+++ service_packs/DML/2017_05_26_1_ins_EquipmentClassProperty.sql
@@ -0,0 +1,23 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+/*add new class properties for ProcessCell*/
+insert into [dbo].[EquipmentClassProperty]
+(	 [Description]
+	,[Value]
+	,[EquipmentClassProperty]
+	,[EquipmentClassID])
+select
+	 N'ЖД станция по умолчанию' as [Description]
+	,N'DEFAULT RAILWAY STATION'	as [Value]
+	,null						as [EquipmentClassProperty]
+	,EC.ID						as [EquipmentClassID]
+from [dbo].[EquipmentClass] EC
+where EC.[EquipmentLevel] = N'Process Cell'
+
+GO
