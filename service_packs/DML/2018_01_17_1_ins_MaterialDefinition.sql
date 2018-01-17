0a044851e12a6b3990a4d2674f838abd2936fa28
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-01-17 12:07:50 +0200

    add new type of Scrap into MaterialDefinition

---
+++ service_packs/DML/2018_01_17_1_ins_MaterialDefinition.sql
@@ -0,0 +1,30 @@
+﻿
+SET QUOTED_IDENTIFIER ON
+GO
+/*insert [MaterialDefinition] new type of Scrap*/
+insert into [dbo].[MaterialDefinition](
+	 [ID]
+	,[Description]
+	,[Location]
+	,[HierarchyScope]
+	,[MaterialClassID]
+	,[MaterialDefinitionID])
+select
+	 T1.[ID]
+	,T1.[Description]
+	,null
+	,1
+	,MD.[MaterialClassID]
+	,MD.[ID]
+from [dbo].[MaterialDefinition] MD
+inner join 
+(values
+(830, N'Смесь шихтовая №4, Керамет')
+) as T1 ([ID],[Description])
+on 1=1
+where MD.[Description] = N'смешанный лом  '
+
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
