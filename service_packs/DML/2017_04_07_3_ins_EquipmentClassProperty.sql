6dd8d8538549c9c45436168066a13280f7bb74ce
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-01-18 14:08:26 +0200

    fix scripts failed during building

--- service_packs/DML/2017_04_07_3_ins_EquipmentClassProperty.sql
+++ service_packs/DML/2017_04_07_3_ins_EquipmentClassProperty.sql
@@ -6,9 +6,45 @@ GO
 SET NOCOUNT ON
 GO
 /*add new class properties*/
+
+/*
 insert into [dbo].[EquipmentClassProperty]
 ([Description],[Value],[EquipmentClassID])
 values
 (N'Грузоотправитель',N'CONSIGNER',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха')),
 (N'Грузополучатель' ,N'CONSIGNEE',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха'))
+*/
+
+
+-- merge because of checking duplicates
+merge [dbo].[EquipmentClassProperty] as trg
+using(
+	select [Description],[Value],[EquipmentClassID]
+	from (values
+		(N'Грузоотправитель',N'CONSIGNER',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха')),
+		(N'Грузополучатель' ,N'CONSIGNEE',(select top 1 ID from [dbo].[EquipmentClass] where [Description] = N'Цеха'))
+	)as T ([Description],[Value],[EquipmentClassID])
+) as src
+on trg.[Description] = src.[Description] and trg.[Value] = src.[Value]
+-- если есть сопоставление строки trg со строкой из источника src
+WHEN MATCHED THEN 
+	UPDATE SET	  
+	 trg.[EquipmentClassID] = src.[EquipmentClassID]
+-- если строка не найдена в trg, но есть в src
+WHEN NOT MATCHED BY TARGET THEN 
+    INSERT 
+	([Description]
+	,[Value]
+	,[EquipmentClassID])
+	VALUES  
+	(src.[Description]
+	,src.[Value]
+	,src.[EquipmentClassID])
+--OUTPUT 
+--    $ACTION, 
+--    ISNULL(INSERTED.[Description], DELETED.[Description]) AS [Description],
+--    ISNULL(INSERTED.[Value], DELETED.[Value]) AS [Value],
+--    ISNULL(INSERTED.[EquipmentClassID], DELETED.[EquipmentClassID]) AS [EquipmentClassID]
+;
+
 GO
--- service_packs/DML/2017_08_15_1_ins_EquipmentClassProperty.sql
+++ service_packs/DML/2017_08_15_1_ins_EquipmentClassProperty.sql
@@ -7,6 +7,7 @@ SET NOCOUNT ON
 GO
 
 /*add new class properties for ProcessCell*/
+/*
 insert into [dbo].[EquipmentClassProperty]
 (	 [Description]
 	,[Value]
@@ -19,5 +20,41 @@ select
 	,EC.ID						as [EquipmentClassID]
 from [dbo].[EquipmentClass] EC
 where EC.Code = N'SCALES'
+*/
+
+-- merge because of checking duplicates
+merge [dbo].[EquipmentClassProperty] as trg
+using(
+	select
+		 N'Платформы ЖД весов'		as [Description]
+		,N'WEIGHBRIDGES_PLATFORMS'	as [Value]
+		,null						as [EquipmentClassProperty]
+		,EC.ID						as [EquipmentClassID]
+	from [dbo].[EquipmentClass] EC
+	where EC.Code = N'SCALES'
+) as src
+on trg.[Description] = src.[Description] and trg.[Value] = src.[Value]
+-- если есть сопоставление строки trg со строкой из источника src
+WHEN MATCHED THEN 
+	UPDATE SET	  
+	 trg.[EquipmentClassID] = src.[EquipmentClassID]
+-- если строка не найдена в trg, но есть в src
+WHEN NOT MATCHED BY TARGET THEN 
+    INSERT 
+	([Description]
+	,[Value]
+	,[EquipmentClassProperty]
+	,[EquipmentClassID])
+	VALUES  
+	(src.[Description]
+	,src.[Value]
+	,src.[EquipmentClassProperty]
+	,src.[EquipmentClassID])
+--OUTPUT 
+--    $ACTION, 
+--    ISNULL(INSERTED.[Description], DELETED.[Description]) AS [Description],
+--    ISNULL(INSERTED.[Value], DELETED.[Value]) AS [Value],
+--    ISNULL(INSERTED.[EquipmentClassID], DELETED.[EquipmentClassID]) AS [EquipmentClassID]
+;
 
 GO
