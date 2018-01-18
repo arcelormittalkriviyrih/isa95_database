0f56113de17fb1e3045802231f6fb2972b696690
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-01-18 09:36:59 +0200

    fix PsExec localhost access denied error

--- jenkins_scripts/apply_service_packs.bat
+++ jenkins_scripts/apply_service_packs.bat
@@ -8,9 +8,9 @@ if ERRORLEVEL 1 exit 1
 
 SET CMD_ERROR_LEVEL=0;
 for /f "tokens=*" %%a in (%WORKSPACE%\jenkins_scripts\all_files_scripts_sort.log) do (
-	IF EXIST DDL\%%a (C:\Nikama\SysinternalsSuite\psexec.exe \\localhost -accepteula -f ^
+	IF EXIST DDL\%%a (C:\Nikama\SysinternalsSuite\psexec.exe -accepteula -f ^
 					  -u %DDL_USER% -p %DDL_PASSWORD% -c "%WORKSPACE%\jenkins_scripts\apply_service_packs_all.bat" %WORKSPACE% %1 ddl %%a || SET CMD_ERROR_LEVEL=1)
-	IF EXIST DML\%%a (C:\Nikama\SysinternalsSuite\psexec.exe \\localhost -accepteula -f ^
+	IF EXIST DML\%%a (C:\Nikama\SysinternalsSuite\psexec.exe -accepteula -f ^
 					  -u %DML_USER% -p %DML_PASSWORD% -c "%WORKSPACE%\jenkins_scripts\apply_service_packs_all.bat" %WORKSPACE% %1 dml %%a || SET CMD_ERROR_LEVEL=1)
 )
 IF %CMD_ERROR_LEVEL% EQU 1 exit /b 1
