c6c133430c405953f43e67bfa79136d968c1b105
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-02-12 16:04:37 +0200

    add views [v_KP4_KIS], [v_WGT_DocumentationsExistCheck], [v_WGT_DocumentsProperty]; add new types of Wagons; change some equipment properties;

---
+++ service_packs/DDL/2018_02_06_1_create_view_v_WGT_DocumentationsExistCheck.sql
@@ -0,0 +1,62 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID ('dbo.v_WGT_DocumentationsExistCheck',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_DocumentationsExistCheck];
+GO
+
+/*
+   View: v_WGT_DocumentationsExistCheck
+   Возвращает основные данные по документам за последний год
+*/
+CREATE view [dbo].[v_WGT_DocumentationsExistCheck]
+as
+
+select
+	 [DocumentationsID] as [ID]
+	,case 
+		when isnumeric([WeightsheetNumber]) = 1
+		then cast([WeightsheetNumber] as int)
+		else 0
+	 end								as [WeightsheetNumber]
+	,case 
+		when isnumeric([WaybillNumber]) = 1
+		then cast([WaybillNumber] as int)
+		else 0
+	 end								as [WaybillNumber]
+	,[Weightbridge]
+	,[SenderShop]
+	,[ReceiverShop]
+	,D.[StartTime]
+	,D.[EndTime]
+	,D.[Status]
+	,DC.[ID]			as [DocumentationsClassID]
+	,case 
+		when DC.ParentID is null
+		then DC.[Description]
+		else DC1.[Description]
+	 end				as [DocumentationsType]
+from (
+	select 
+		 [DocumentationsID]
+		,[Description2]
+		,[Value]
+	from [dbo].[v_WGT_DocumentsProperty] 
+) as WP
+pivot(max(Value) for [Description2] in ([WeightsheetNumber], [WaybillNumber], [Weightbridge], [SenderShop], [ReceiverShop])) as pvt
+inner join
+[dbo].[Documentations] D
+on pvt.[DocumentationsID] = D.ID --and isnull(D.[EndTime], getdate()) > dateadd(year, -1, getdate())
+inner join [DocumentationsClass] DC
+on D.DocumentationsClassID = DC.ID
+left join [DocumentationsClass] DC1
+on DC.ParentID = DC1.ID
+--where DC1.[Description] = N'Отвесная'
+
+
+GO
+
+
---
+++ service_packs/DML/2018_02_12_2_ins_EquipmentProperty.sql
@@ -0,0 +1,76 @@
+﻿
+SET QUOTED_IDENTIFIER ON
+GO
+/* merge [EquipmentProperty] new property SCALES_TYPE for scales 'Весы Копр.№4' */
+;with CTE_Platforms as (
+select N'{WagonClassID: ' + cast([ID] as nvarchar) + N', Weight: [' + [Platforms] + N']}' as [Platforms]
+from (
+	select 
+		 PC.ID
+		,PC.[Description]
+		,case PC.[Description]
+			when N'Лафет-короб' 
+			then N'''Weight_platform_1'''
+			else N'''Weight_platform_1'', ''Weight_platform_2'''
+		 end	as [Platforms]
+	from [dbo].[PackagingClass] PC
+	left join [dbo].[PackagingClass] PC1
+	on PC.[ParentID] = PC1.[ID]
+	where PC1.[Description] = N'ЖД вагоны') as T1
+)
+, CTE_Platforms_Concat as (
+SELECT distinct
+	STUFF((
+		SELECT ',' + T2.[Platforms] 
+		FROM CTE_Platforms AS T2
+		FOR XML PATH('')), 1, 1, '') as [PlatformsArray]
+FROM CTE_Platforms AS T1
+)
+
+
+merge [dbo].[EquipmentProperty] as trg
+using(
+select
+	 null						as [Description]
+	--,N'[{ WagonClassID : 2, Weight : [ ''Weight_platform_1'' ] },{ WagonClassID : 3, Weight : [ ''Weight_platform_1'', ''Weight_platform_2'' ] },{ WagonClassID : 4, Weight : [ ''Weight_platform_1'', ''Weight_platform_2'' ] },{ WagonClassID : 5, Weight : [ ''Weight_platform_1'', ''Weight_platform_2'' ] }]'
+	,N'['+[PlatformsArray]+N']'	as [Value]
+	,null						as [EquipmentProperty]
+	,E.ID						as [EquipmentID]
+	,ECP.ID						as [ClassPropertyID]
+from [dbo].[Equipment] E
+join [dbo].[EquipmentClass] EC
+on E.EquipmentClassID = EC.ID
+join [dbo].[EquipmentClassProperty] ECP
+on ECP.EquipmentClassID = EC.ID
+join CTE_Platforms_Concat
+on 1=1
+where	EC.Code = N'SCALES'
+	and E.Description in (N'Весы Копр.№4')
+	and ECP.Value = N'WEIGHBRIDGES_PLATFORMS_WAGON_CLASS'
+) as src
+on	trg.[EquipmentID] = src.[EquipmentID] 
+and trg.[ClassPropertyID] = src.[ClassPropertyID] 
+and trg.[EquipmentProperty] = src.[EquipmentProperty]
+-- если есть сопоставление строки trg со строкой из источника src
+WHEN MATCHED THEN 
+	UPDATE SET	  
+	 trg.[Value] = src.[Value]
+-- если строка не найдена в trg, но есть в src
+WHEN NOT MATCHED BY TARGET THEN 
+    INSERT 
+(	 [Description]
+	,[Value]
+	,[EquipmentProperty]
+	,[EquipmentID]
+	,[ClassPropertyID])
+	VALUES  
+(	 src.[Description]
+	,src.[Value]
+	,src.[EquipmentProperty]
+	,src.[EquipmentID]
+	,src.[ClassPropertyID]);
+
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
