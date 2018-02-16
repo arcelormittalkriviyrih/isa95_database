ea6df4c6f5f5b3fb012c8d48bf182f6b2a826b8d
Oleksandr Chekmez <chekmez@nikama.com>
2018-02-16 13:59:52 +0200

    #384 add view for parallel printing

---
+++ service_packs/DDL/2018_02_16_2_PrintJobOrders.sql
@@ -0,0 +1,26 @@
+﻿SET ANSI_NULLS ON;
+GO
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID('dbo.v_PrintJobOrders', N'V') IS NOT NULL
+    DROP VIEW [dbo].[v_PrintJobOrders];
+GO
+/*
+   View: v_PrintJobOrders
+   Возвращает список JobOrder for Printiig.
+*/
+CREATE VIEW [dbo].[v_PrintJobOrders]
+AS
+SELECT jo.ID,
+            DispatchStatus,		
+		  Command,
+		  CommandRule,		  
+		  ep.[Value] PrinterIP
+     FROM dbo.JobOrder jo, OpEquipmentRequirement oer, dbo.EquipmentProperty ep, dbo.EquipmentClassProperty ecp
+	 Where WorkType=N'Print'
+	 and jo.ID=oer.JobOrderID
+	 and ep.EquipmentID=oer.EquipmentID
+	 and ecp.ID = ep.ClassPropertyID
+	 and ecp.[Value]=N'PRINTER_IP';
+GO
