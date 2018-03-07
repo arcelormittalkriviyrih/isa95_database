e1a61b0f2c6d461c48d6d7c65566417fd91545e6
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-03-07 14:40:31 +0200

    insert into MaterialDefinition new types of Scrap;

---
+++ service_packs/DML/2018_03_07_1_ins_MaterialDefinition.sql
@@ -0,0 +1,35 @@
+﻿
+SET QUOTED_IDENTIFIER ON
+GO
+/*insert [MaterialDefinition] new type of Scrap*/
+insert into [dbo].[MaterialDefinition](
+	 [ID]
+	,[Description]
+	,[Location]
+	,[HierarchyScope]
+	,[MaterialClassID]
+	,[MaterialDefinitionID])
+select
+		 T.[ID]
+		,T.[Description]
+		,null
+		,1
+		,MD.[MaterialClassID]
+		,MD.[ID]
+from [dbo].[MaterialDefinition] MD
+inner join 
+(values
+	 (181, N'Обрезь габаритная (вид 2)', N'оборотный лом')
+	,(310, N'Брак заготовок (вид 500)', N'оборотный лом')
+	,(312, N'Брак стальной негабаритный (вид 500)', N'оборотный лом')
+	,(777, N'Лом стальной негабаритный (вид 500)', N'смешанный лом')
+) as T([ID],[Description], [ParentDescription])
+on MD.[Description] = T.[ParentDescription]
+left join [dbo].[MaterialDefinition] MD1
+on T.[ID] = MD1.[ID]
+where MD1.[ID] is null
+	
+GO
+
+SET QUOTED_IDENTIFIER OFF
+GO
