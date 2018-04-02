536d47d8a6d84973a8f64c8dcaad590393fbcef1
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-04-02 15:35:22 +0300

    add new Waybill property; change procedures upd_SaveWaybill, ins_SaveWaybill and view v_WGT_DocumentsProperty for new Waybill property;

---
+++ service_packs/DML/2018_04_02_1_ins_DocumentationsClassProperty.sql
@@ -0,0 +1,14 @@
+﻿/* Properties of Waybills */
+insert into [dbo].[DocumentationsClassProperty]
+		( [Description],	[DocumentationsClassID])
+select T1.[Description], T2.[DocumentationsClassID]
+from(
+select N'Примечание к роду груза'	[Description]
+) as T1
+join	
+(	select ID					[DocumentationsClassID]
+	from [dbo].[DocumentationsClass] 
+	where [Description] = N'Путевая') as T2
+on 1=1
+
+GO
