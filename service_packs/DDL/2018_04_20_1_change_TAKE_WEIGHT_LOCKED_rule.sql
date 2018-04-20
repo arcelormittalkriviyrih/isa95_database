bc4fa9646d730fcbf7a7bc2ee51980db5e376b07
Oleksandr Chekmez <chekmez@nikama.com>
2018-04-20 14:28:23 +0300

    set TAKE_WEIGHT_LOCKED=0 on WEIGHT_CURRENT=0 for PACK_SANDWICH=1

---
+++ service_packs/DDL/2018_04_20_1_change_TAKE_WEIGHT_LOCKED_rule.sql
@@ -0,0 +1,83 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER TRIGGER [dbo].[InsKeplogger] ON [dbo].[KEP_logger_table]
+AFTER INSERT, UPDATE
+AS
+BEGIN
+
+SET NOCOUNT ON;
+SET XACT_ABORT OFF;
+
+SAVE TRANSACTION updProperty;
+
+BEGIN TRY
+
+   DECLARE @NUMBER_POCKET    INT,
+           @WEIGHT_ZERO      BIT,
+		   @PACK_SANDWICH	 BIT,
+		   @WEIGHT_CURRENT   real,
+           @EquipmentID      INT,
+           @TakeWeightLocked [NVARCHAR](50),
+           @err_message      [NVARCHAR](255);
+
+   SELECT @NUMBER_POCKET=[NUMBER_POCKET],
+          @WEIGHT_ZERO=[WEIGHT_ZERO],
+		  @WEIGHT_CURRENT=[WEIGHT_CURRENT],
+		  @PACK_SANDWICH=[PACK_SANDWICH]
+   FROM INSERTED;
+
+   IF @WEIGHT_ZERO=1
+      BEGIN
+         SET @EquipmentID=dbo.get_EquipmentIDByScalesNo(@NUMBER_POCKET);
+         IF @EquipmentID IS NULL
+            BEGIN
+               SET @err_message = N'By SCALES_NO=[' + @NUMBER_POCKET + N'] EquipmentID not found';
+               THROW 60010, @err_message, 1;
+            END;
+
+         SET @TakeWeightLocked=dbo.get_EquipmentPropertyValue(@EquipmentID,N'TAKE_WEIGHT_LOCKED');
+         IF @TakeWeightLocked NOT IN (N'0')
+            EXEC [dbo].[upd_EquipmentProperty] @EquipmentID = @EquipmentID,
+                                               @EquipmentClassPropertyValue = N'TAKE_WEIGHT_LOCKED',
+                                               @EquipmentPropertyValue = N'0';
+      END;
+
+	IF @PACK_SANDWICH=1
+		IF @WEIGHT_CURRENT=0
+			BEGIN
+			SET @EquipmentID=dbo.get_EquipmentIDByScalesNo(@NUMBER_POCKET);
+         IF @EquipmentID IS NULL
+            BEGIN
+               SET @err_message = N'By SCALES_NO=[' + @NUMBER_POCKET + N'] EquipmentID not found';
+               THROW 60010, @err_message, 1;
+            END;
+
+         SET @TakeWeightLocked=dbo.get_EquipmentPropertyValue(@EquipmentID,N'TAKE_WEIGHT_LOCKED');
+         IF @TakeWeightLocked NOT IN (N'0')
+            EXEC [dbo].[upd_EquipmentProperty] @EquipmentID = @EquipmentID,
+                                               @EquipmentClassPropertyValue = N'TAKE_WEIGHT_LOCKED',
+                                               @EquipmentPropertyValue = N'0';
+      END;
+
+END TRY
+
+BEGIN CATCH
+   ROLLBACK TRANSACTION updProperty;
+   BEGIN TRY
+      SAVE TRANSACTION InsErr;
+      EXEC [dbo].[ins_ErrorLog];
+   END TRY
+   BEGIN CATCH
+      ROLLBACK TRANSACTION InsErr;
+   END CATCH
+END CATCH
+
+END
+
+
+GO
+EXEC sp_settriggerorder @triggername=N'[dbo].[InsKeplogger]', @order=N'Last', @stmttype=N'INSERT'
+GO
