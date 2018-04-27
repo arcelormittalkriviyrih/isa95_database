8db84df5e5acea42c20da2fe3e19c550368f876d
Oleksandr Chekmez <chekmez@nikama.com>
2018-04-27 09:01:58 +0300

    do not show OPC property missing for manual weights

--- service_packs/DDL/2018_04_27_1_no_opc_for_manual.sql
+++ service_packs/DDL/2018_04_27_1_no_opc_for_manual.sql
@@ -1,4 +1,114 @@
-﻿SET ANSI_NULLS ON;
+﻿SET NUMERIC_ROUNDABORT OFF;
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON;
+GO
+
+IF OBJECT_ID ('dbo.v_ScalesDetailInfo',N'V') IS NOT NULL
+   DROP VIEW dbo.[v_ScalesDetailInfo];
+GO
+/*
+   View: v_ScalesDetailInfo
+    Возвращает данные из контроллера а также расчитывает количество прудков для весов.
+	Используется на экране маркиорвщицы.
+*/
+CREATE VIEW [dbo].[v_ScalesDetailInfo]
+AS
+     WITH BarWeight
+          AS (SELECT CAST(par.[Value] AS FLOAT) PropertyValue,
+                     pr.[Value] PropertyType,
+                     ep.EquipmentId
+              FROM [PropertyTypes] pr,
+                   [dbo].[Parameter] par,
+                   dbo.[EquipmentProperty] ep
+              WHERE pr.[Value] IN(N'BAR_WEIGHT', N'MIN_WEIGHT', N'MAX_WEIGHT', N'BAR_QUANTITY')
+              AND pr.ID = par.PropertyType
+              AND ep.[ClassPropertyID] = dbo.get_EquipmentClassPropertyByValue(N'JOB_ORDER_ID')
+          AND cast(ep.[Value] as nvarchar) = par.JobOrder),
+          Kep_Data
+          AS (SELECT *
+              FROM
+              (
+                  SELECT ROW_NUMBER() OVER(PARTITION BY kl.[NUMBER_POCKET] ORDER BY kl.[TIMESTAMP] DESC) RowNumber,
+                         kl.[WEIGHT_CURRENT],
+                         kl.[WEIGHT_STAB],
+                         kl.[WEIGHT_ZERO],
+                         kl.[COUNT_BAR],
+                         kl.[REM_BAR],
+                         kl.[AUTO_MANU],
+                         kl.[POCKET_LOC],
+                         kl.[PACK_SANDWICH],
+                         kl.[NUMBER_POCKET],
+                         kl.[ALARM],
+                         kl.[KEY_MANU],
+						 kl.[EN_BUTTON_TARA],
+						 kl.[WEIGHT_OK],
+						 kl.[PEREBOR]
+                  FROM dbo.KEP_logger kl
+              ) ww
+              WHERE ww.RowNumber = 1)
+          SELECT eq.ID AS ID,
+		  kd.[NUMBER_POCKET],
+                 eq.[Description] AS ScalesName,
+                 dbo.get_RoundedWeightByEquipment(kd.WEIGHT_CURRENT, eq.ID) WEIGHT_CURRENT,
+                 kd.WEIGHT_STAB,
+                 kd.WEIGHT_ZERO,
+                 kd.AUTO_MANU,
+                 kd.POCKET_LOC,
+                 kd.PACK_SANDWICH,
+                 kd.ALARM,
+                 kd.KEY_MANU,
+				 kd.EN_BUTTON_TARA,
+                (select ps.Value
+                 from [dbo].[ParameterSpecification] ps
+                 where ps.WorkDefinitionID=dbo.get_EquipmentPropertyValue(eq.ID,N'WORK_DEFINITION_ID')
+                   and ps.PropertyType=dbo.[get_PropertyTypeIdByValue](N'BUNT_NO')) BUNT_NO,
+                 CAST(FLOOR(dbo.get_RoundedWeightByEquipment(kd.WEIGHT_CURRENT, eq.ID) / bw.PropertyValue) AS   INT) RodsQuantity,
+                 kd.REM_BAR RodsLeft,
+                 ISNULL(CAST(bminW.PropertyValue AS INT), 0) MinWeight,
+                 ISNULL(CAST(bmaxW.PropertyValue AS INT), 0) MaxWeight,
+                 ISNULL(dbo.get_RoundedWeight(bmaxW.PropertyValue * 1.2, 'UP', 100), dbo.get_EquipmentPropertyValue(eq.ID, N'MAX_WEIGHT')) MaxPossibleWeight,
+                 ISNULL(dbo.get_EquipmentPropertyValue(eq.ID, N'SCALES_TYPE'), N'POCKET') SCALES_TYPE,
+                 dbo.get_EquipmentPropertyValue(eq.ID, N'PACK_RULE') PACK_RULE,
+                 CAST(bQty.PropertyValue AS INT)		BAR_QUANTITY,
+                 ISNULL(dbo.get_EquipmentPropertyValue(eq.ID, N'TAKE_WEIGHT_LOCKED'), '0') TAKE_WEIGHT_LOCKED,
+				 kd.[WEIGHT_OK],
+				 kd.[PEREBOR],
+				 (SELECT 1 WHERE EXISTS (SELECT NULL FROM dbo.JobOrder jo,
+				 dbo.EquipmentProperty epv,
+				 dbo.EquipmentClassProperty epr
+				  WHERE epv.EquipmentID=eq.ID 
+				  AND ecp.ID = eqp.ClassPropertyID
+				  AND ecp.value = N'SCALES_NO'
+				  AND jo.WorkType = N'KEPCommands'
+																		   AND jo.DispatchStatus=N'ToSend'   
+																		   AND CAST(epv.Value as nvarchar)=  left(Command, len(Command)- charindex('.', reverse(Command)))
+																		   and DATEDIFF(second, jo.StartTime, CURRENT_TIMESTAMP) > 15 )
+						  OR EXISTS (SELECT * from dbo.kep_controller_diag diag,
+						  dbo.Equipment side,
+						  dbo.EquipmentProperty epv2,
+						  dbo.EquipmentClassProperty epr2
+						   where side.ID = eq.Equipment 
+						   AND epv2.EquipmentID=side.ID
+						   AND epv2.ClassPropertyID=epr2.ID and CAST(epr2.Value as nvarchar) = N'CONTROLLER_ID'
+						   AND DATEDIFF(second, diag.Controller_Last_Connect,CURRENT_TIMESTAMP)>15 
+																			AND (cast(diag.Controller_ID as nvarchar) = CAST(epv2.[Value] as nvarchar)))) Send_OPC_Error
+          FROM dbo.Equipment eq
+               INNER JOIN dbo.EquipmentProperty eqp ON(eqp.EquipmentID = eq.ID)
+               INNER JOIN dbo.EquipmentClassProperty ecp ON(ecp.ID = eqp.ClassPropertyID
+                                                            AND ecp.value = N'SCALES_NO')
+			  -- INNER JOIN dbo.EquipmentProperty epv ON (epv.EquipmentID=eq.ID)
+			   --INNER JOIN dbo.EquipmentClassProperty epr ON (epv.ClassPropertyID=epr.ID and CAST(epr.Value as nvarchar) = N'OPC_DEVICE_NAME')
+			   --INNER JOIN dbo.Equipment side ON (side.ID = eq.Equipment)
+			   --INNER JOIN dbo.EquipmentProperty epv2 ON (epv2.EquipmentID=side.ID)
+			   --INNER JOIN dbo.EquipmentClassProperty epr2 ON (epv2.ClassPropertyID=epr2.ID and CAST(epr2.Value as nvarchar) = N'CONTROLLER_ID')
+               LEFT OUTER JOIN Kep_Data kd ON(ISNUMERIC(eqp.value) = 1 AND kd.[NUMBER_POCKET] = CAST(eqp.value AS INT))
+               LEFT OUTER JOIN BarWeight bw ON bw.EquipmentId = eq.ID AND bw.PropertyType = N'BAR_WEIGHT'
+               LEFT OUTER JOIN BarWeight bminW ON bminW.EquipmentId = eq.ID AND bminW.PropertyType = N'MIN_WEIGHT'
+               LEFT OUTER JOIN BarWeight bmaxW ON bmaxW.EquipmentId = eq.ID AND bmaxW.PropertyType = N'MAX_WEIGHT'
+               LEFT OUTER JOIN BarWeight bQty ON bQty.EquipmentId = eq.ID AND bQty.PropertyType = N'BAR_QUANTITY';
+GO
+
+SET ANSI_NULLS ON;
 GO
 
 SET QUOTED_IDENTIFIER ON;
