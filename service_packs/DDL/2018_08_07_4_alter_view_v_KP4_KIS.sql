28f342c7987b1c5456fd24e96fb2994bdcdee3dd
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-08-07 10:39:50 +0300

    add SAPCodes into v_WGT_Consigners and v_KP4_KIS;

---
+++ service_packs/DDL/2018_08_07_4_alter_view_v_KP4_KIS.sql
@@ -0,0 +1,62 @@
+﻿
+IF OBJECT_ID ('dbo.v_KP4_KIS',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_KP4_KIS];
+GO
+
+/*
+   View: v_KP4_KIS
+   Возвращает отвесные для КИС
+*/
+create view [dbo].[v_KP4_KIS]
+as
+
+select
+	 WO.[ID]
+	,vDP1.[Value]								as [WeightSheetNumber]
+	,cast(D.[StartTime] as smalldatetime)		as [WeightSheetCreateTime]
+	,WO.[OperationType]							as [WeighingType]
+	,vDP2.[Value]								as [SenderID]
+	,vDP3.[Value]								as [ReceiverID]
+	,vDP2.[Value2]								as [Sender]
+	,vDP3.[Value2]								as [Receiver]
+	,WO.[Description]							as [WagonNumber]
+	,PUDP.[Value]								as [WaybillNumber]
+	,vST.[Description]							as [ScrapGroupName]
+	,vST.[ScrapClassID]							as [ScrapCode]
+	,WO.[Tara]									as [Tare]
+	,WO.[Brutto]								as [Brutto]
+	,WO.[Netto]									as [Netto]
+	,cast(WO.[OperationTime] as smalldatetime)	as [WeightingTime]
+	,vDP4.[Value2]								as [Weigher]
+	,(select top 1 [Value] from [dbo].[EquipmentProperty] EP where EP.[Description] = N'Код SAP' and EP.[EquipmentID] = vDP2.[Value]) as [SenderSAPCode]
+	,(select top 1 [Value] from [dbo].[EquipmentProperty] EP where EP.[Description] = N'Код SAP' and EP.[EquipmentID] = vDP3.[Value]) as [ReceiverSAPCode]
+	
+	--,WO.[EquipmentID]
+	--,WO.[PackagingUnitsDocsID]
+	--,WO.[DocumentationsID]
+	--,WO.[TaringTime]
+	--,WO.[Status]
+
+from [dbo].[WeightingOperations] WO
+join [dbo].[Documentations] D
+on WO.[DocumentationsID] = D.[ID]
+left join [dbo].[v_WGT_DocumentsProperty] vDP1
+on vDP1.[DocumentationsID] = WO.[DocumentationsID] and vDP1.[Description2] = N'WeightsheetNumber'
+left join [dbo].[v_WGT_DocumentsProperty] vDP2
+on vDP2.[DocumentationsID] = WO.[DocumentationsID] and vDP2.[Description2] = N'SenderShop'
+left join [dbo].[v_WGT_DocumentsProperty] vDP3
+on vDP3.[DocumentationsID] = WO.[DocumentationsID] and vDP3.[Description2] = N'ReceiverShop'
+left join [dbo].[v_WGT_DocumentsProperty] vDP4
+on vDP4.[DocumentationsID] = WO.[DocumentationsID] and vDP4.[Description2] = N'Weigher'
+left join [dbo].[PackagingUnitsDocsProperty] PUDP
+on PUDP.[PackagingUnitsDocsID] = WO.[PackagingUnitsDocsID] and PUDP.[Description] = N'Путевая'
+left join [dbo].[v_WGT_ScrapTypes] vST
+on vST.[ID] = WO.[MaterialDefinitionID]
+
+where	isnull(WO.Status, '') != N'reject'
+	and isnull(D.Status, '')   = N'closed'
+
+--order by WO.id desc
+
+  
+GO
