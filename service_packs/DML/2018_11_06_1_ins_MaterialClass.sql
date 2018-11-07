a1388452d29b4c91ccc5ea25ed65b7aa62a6a1a6
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-11-07 08:42:05 +0200

    insert new MaterialClass, new property SAP_CODE for them; insert new MaterialDefinition and their properties;

---
+++ service_packs/DML/2018_11_06_1_ins_MaterialClass.sql
@@ -0,0 +1,16 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+-- insert MaterialClass
+insert into [dbo].[MaterialClass] ([Description], [ParentID], [Code])
+values
+ (N'Готовая продукция', null, N'Finished Products')
+,(N'Сырье', null, N'Raw Material')
+
+
+GO
---
+++ service_packs/DML/2018_11_06_2_ins_MaterialClassProperty.sql
@@ -0,0 +1,30 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+SET NOCOUNT ON
+GO
+
+--insert MaterialClassProperty
+insert into [dbo].[MaterialClassProperty]
+	([Description]
+	,[Value]
+	,[MaterialClassID])
+select distinct
+	 N'Код SAP'				as [Description]
+	,N'SAP_CODE'			as [Value]
+	,isnull(MC.ID, MD.[MaterialClassID])	as [MaterialClassID]
+from (values 
+	 (N'Готовая продукция')
+	,(N'Сырье')
+	,(N'Оборотный лом')
+	,(N'Чугунный лом')
+) as T([MaterialClassDescription])
+left join [MaterialClass] MC
+on T.[MaterialClassDescription] = MC.[Description]
+left join [dbo].[MaterialDefinition] MD
+on MD.[Description] = T.[MaterialClassDescription]
+where isnull(MC.ID, MD.[MaterialClassID]) is not null
+
+GO
