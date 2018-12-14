ed6c65a955519b6d0997bb9c912e7d60a3476367
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-12-14 08:19:04 +0200

    create table [dbo].[WeightingOperationsProperty]; create view [dbo].[v_WGT_PackagingUnitsProperty];

---
+++ service_packs/DDL/2018_12_13_6_create_view_v_WGT_PackagingUnitsProperty.sql
@@ -0,0 +1,27 @@
+﻿
+IF OBJECT_ID ('dbo.v_WGT_PackagingUnitsProperty',N'V') IS NOT NULL
+  DROP VIEW dbo.v_WGT_PackagingUnitsProperty;
+GO
+
+/*
+   View: v_WGT_PackagingUnitsProperty
+   Возвращает свойства вагонов (тара, грузоподъемность и т.д.)
+*/
+
+create view [dbo].[v_WGT_PackagingUnitsProperty]
+as
+
+select
+	 PUP.[ID]
+	,PU.[ID]			as [PackagingUnitsID]
+	,PU.[Description]	as [Wagon]
+	,PUP.[Description]	as [Parameter]
+	,PUP.[Value]
+	,PUP.[ValueTime]
+from [dbo].[PackagingUnits] PU
+join [dbo].[PackagingUnitsProperty] PUP
+on PU.[ID] = PUP.[PackagingUnitsID]
+
+GO
+
+
