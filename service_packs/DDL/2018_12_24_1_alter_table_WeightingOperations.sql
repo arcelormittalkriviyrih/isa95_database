760ce43e2ce00bf9fcb64e1d4f928253ef135053
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-12-24 15:31:59 +0200

    add column [TaringPersonID] into table [dbo].[WeightingOperations]; alter procedure ins_TakeWeightLoading (add TaringPersonID); fix procedure upd_Documentations (real taring time); fix procedure ins_JobOrderOPCCommandZeroingScales (enable multiple CommandRule); alter view v_WGT_Weightsheet (fix [Carrying] column); alter view [dbo].[v_WGT_WeightsheetForSAP] (add [TareWeigher] column);

---
+++ service_packs/DDL/2018_12_24_1_alter_table_WeightingOperations.sql
@@ -0,0 +1,16 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- add new column
+IF OBJECT_ID ('dbo.WeightingOperations',N'U') IS NOT NULL and COLUMNPROPERTY(OBJECT_ID('WeightingOperations','U'),'TaringPersonID','ColumnId') IS NULL
+begin
+	ALTER TABLE [dbo].[WeightingOperations] 
+	ADD [TaringPersonID] int NULL
+
+	ALTER TABLE [dbo].[WeightingOperations]  WITH CHECK 
+	ADD CONSTRAINT [FK_WeightingOperations_TaringPerson] FOREIGN KEY([TaringPersonID]) REFERENCES [dbo].[Person] ([ID])
+end
+
+GO
---
+++ service_packs/DDL/2018_12_24_3_create_view_v_WGT_WeightsheetForSAP.sql
@@ -0,0 +1,70 @@
+﻿
+IF OBJECT_ID ('dbo.v_WGT_WeightsheetForSAP',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_WeightsheetForSAP];
+GO
+
+
+
+/*
+   View: v_WeightsheetForSAP
+   Возвращает отвесные для SAP
+*/
+create view [dbo].[v_WGT_WeightsheetForSAP]
+as
+
+select
+	 WO.[ID]
+	,vDP1.[Value]								as [WeightSheetNumber]
+	,cast(D.[StartTime] as datetime)			as [WeightSheetCreateTime]
+	,cast(D.[EndTime]	as datetime)			as [WeightSheetCloseTime]
+	--,WO.[OperationType]							as [WeighingType]
+	,WO.[EquipmentID]							as [ScalesID]
+	,(select top 1 [Description] from [Equipment] where ID = WO.EquipmentID) as [ScalesName]
+	,vDP2.[Value]								as [SenderID]
+	,vDP3.[Value]								as [ReceiverID]
+	,vDP2.[Value2]								as [Sender]
+	,vDP3.[Value2]								as [Receiver]
+	,(select top 1 [Value] from [dbo].[EquipmentProperty] EP where EP.[Description] = N'Код SAP' and EP.[EquipmentID] = vDP2.[Value]) as [SenderSAPCode]
+	,(select top 1 [Value] from [dbo].[EquipmentProperty] EP where EP.[Description] = N'Код SAP' and EP.[EquipmentID] = vDP3.[Value]) as [ReceiverSAPCode]
+	,WO.[Description]							as [WagonNumber]
+	,PUDP.[Value]								as [WaybillNumber]
+	,vST.[Description]							as [ScrapGroupName]
+	,vST.[ScrapClassID]							as [ScrapSAPCode]
+	,WO.[Tara]									as [Tare]
+	,WO.[Brutto]								as [Brutto]
+	,WO.[Netto]									as [Netto]
+	,cast(WO.[OperationTime] as datetime)		as [WeightingTime]
+	,cast(WO.[TaringTime] as datetime)			as [TaringTime]
+	,cast(WOP.[Value] as real)					as [Carrying]
+	,cast(WOP1.[Value] as real)					as [MarkedTare]
+	--,vDP4.[Value2]								as [Weigher]
+	,(select top 1 [PersonName] from [dbo].[Person] where [ID] = WO.[PersonID])			as [Weigher]
+	,(select top 1 [PersonName] from [dbo].[Person] where [ID] = WO.[TaringPersonID])	as [TareWeigher]
+
+from [dbo].[WeightingOperations] WO
+join [dbo].[Documentations] D
+on WO.[DocumentationsID] = D.[ID]
+left join [dbo].[v_WGT_DocumentsProperty] vDP1
+on vDP1.[DocumentationsID] = WO.[DocumentationsID] and vDP1.[Description2] = N'WeightsheetNumber'
+left join [dbo].[v_WGT_DocumentsProperty] vDP2
+on vDP2.[DocumentationsID] = WO.[DocumentationsID] and vDP2.[Description2] = N'SenderShop'
+left join [dbo].[v_WGT_DocumentsProperty] vDP3
+on vDP3.[DocumentationsID] = WO.[DocumentationsID] and vDP3.[Description2] = N'ReceiverShop'
+-- left join [dbo].[v_WGT_DocumentsProperty] vDP4
+-- on vDP4.[DocumentationsID] = WO.[DocumentationsID] and vDP4.[Description2] = N'Weigher'
+left join [dbo].[PackagingUnitsDocsProperty] PUDP
+on PUDP.[PackagingUnitsDocsID] = WO.[PackagingUnitsDocsID] and PUDP.[Description] = N'Путевая'
+left join [dbo].[v_WGT_ScrapTypes] vST
+on vST.[ID] = WO.[MaterialDefinitionID]
+left join [dbo].[WeightingOperationsProperty] WOP
+on WOP.[WeightingOperationsID] = WO.[ID] and WOP.[Description] = N'Грузоподъемность'
+left join [dbo].[WeightingOperationsProperty] WOP1
+on WOP1.[WeightingOperationsID] = WO.[ID] and WOP1.[Description] = N'Тара с бруса'
+
+where	isnull(WO.Status, '') != N'reject'
+	and isnull(D.Status, '')   = N'closed'
+	and WO.[OperationType] = N'Погрузка'
+	and D.[EndTime] is not null
+--order by WO.id desc
+
+GO
