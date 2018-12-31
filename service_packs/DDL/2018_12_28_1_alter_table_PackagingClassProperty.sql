02a638f045801c9b0ef7d4cdf6db355f8579175f
Timur.Kapalov <timur.kapalov@arcelormittal.com>
2018-12-31 14:02:52 +0200

    fix view v_WGT_WeightsheetForSAP; fix procedure upd_SaveWaybill; delete old and insert new wagon types and their properties in PackagingClass and PackagingClassProperty;

---
+++ service_packs/DDL/2018_12_28_1_alter_table_PackagingClassProperty.sql
@@ -0,0 +1,7 @@
+﻿
+IF COLUMNPROPERTY(OBJECT_ID('PackagingClassProperty','U'),'Value','ColumnId') IS NOT NULL
+BEGIN
+	ALTER TABLE [dbo].[PackagingClassProperty]
+	ALTER COLUMN [Value] [nvarchar](500) NULL
+END
+
