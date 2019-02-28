2bc64c800e9f452c5ce456ef6abe2904510a35d6
Oleksandr Chekmez <chekmez@nikama.com>
2019-02-28 13:42:55 +0200

    Разрешить спецфункции для перемаркированных бирок

---
+++ service_packs/DDL/2019_02_28_1_MaterialLotProperty.sql
@@ -0,0 +1,30 @@
+﻿SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+IF OBJECT_ID ('dbo.v_MaterialLotProperty', N'V') IS NOT NULL
+   DROP VIEW dbo.v_MaterialLotProperty;
+GO
+/*
+   View: v_MaterialLotProperty
+    Возвращает список свойств бирки.
+	Используется в спец режимах.
+*/
+CREATE VIEW [dbo].[v_MaterialLotProperty]
+AS
+SELECT mlp.[ID],
+       ml.[ID] MaterialLotID,
+       ml.[FactoryNumber],
+       ml.[Status],
+       ml.[Quantity],
+       mlp.[PropertyType] PropertyID,
+       pt.[Value] Property,
+       mlp.[Value]
+FROM [dbo].[MaterialLot] ml
+	 INNER JOIN [dbo].[MaterialLotLast] mll ON (mll.[MaterialLotID]=ml.[ID])   
+     INNER JOIN [dbo].[MaterialLotProperty] mlp ON (mlp.[MaterialLotID]=ml.[ID])
+     INNER JOIN [dbo].[PropertyTypes] pt ON (pt.[ID]=mlp.[PropertyType])
+WHERE ml.[Quantity]	> 0;
+GO
