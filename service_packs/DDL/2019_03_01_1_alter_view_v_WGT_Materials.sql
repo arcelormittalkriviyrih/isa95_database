cdbb54c9fb9aa7ff0a59522cbc4d49d7301e202e
D.S. Guk <dmitry.guk@arcelormittal.com>
2019-03-01 09:40:23 +0200

    add field IN_USE

---
+++ service_packs/DDL/2019_03_01_1_alter_view_v_WGT_Materials.sql
@@ -0,0 +1,56 @@
+﻿
+IF OBJECT_ID ('dbo.v_WGT_Materials',N'V') IS NOT NULL
+  DROP VIEW [dbo].[v_WGT_Materials];
+GO
+
+/*
+   View: v_WGT_Materials
+   Возвращает список материалов
+*/
+
+CREATE VIEW [dbo].[v_WGT_Materials]
+AS
+
+select
+	 MD.[ID]
+	,MD.[Description]
+	,MD.[ID]			as [ParentID]
+	,MD.[Description]	as [ParentDescription]
+	,MDP.[Value]		as [SAPCode]
+	-- 01.03.19 start
+	,MDP2.[Value]		as [InUse]
+	-- 01.03.19 end
+	,MC.[ID]			as [ClassID]
+	,MC.[Description]	as [ClassDescription]
+	,MC.[Code]			as [ClassCode]
+from [dbo].[MaterialDefinition] MD
+join [dbo].[MaterialClass] MC
+on MD.[MaterialClassID] = MC.[ID]
+left join [dbo].[MaterialDefinition] MD1
+on MD.[MaterialDefinitionID] = MD1.[ID]
+cross apply(
+	select top 1
+		 [MaterialDefinitionID]
+		,[Description]
+		,[Value]
+		,[ClassPropertyID]
+	from [dbo].[MaterialDefinitionProperty]
+	where [Description] = N'SAP_CODE' and [MaterialDefinitionID] = MD.[ID]
+	order by [ID] desc
+) as MDP
+-- 01.03.19 start
+cross apply(
+	select top 1
+		 [MaterialDefinitionID]
+		,[Description]
+		,[Value]
+		,[ClassPropertyID]
+	from [dbo].[MaterialDefinitionProperty]
+	where [Description] = N'IN_USE' and [MaterialDefinitionID] = MD.[ID]
+	order by [ID] desc
+) as MDP2
+-- 01.03.19 end
+where MC.[Code] in (N'Scrap', N'Finished Products', N'Raw Material') and MDP2.[Value] != 0 -- mod and MDP2.[Value] != 0
+
+
+GO
